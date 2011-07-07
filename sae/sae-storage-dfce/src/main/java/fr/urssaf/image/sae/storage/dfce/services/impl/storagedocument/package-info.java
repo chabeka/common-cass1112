fb0cafec6f29a386f8a5c95b074a2894185d02bc
samuel.carriere <samuel.carriere>
2011-07-07 09:13:37 +0000

    Tag dfce-samples-0.9.2.1
    svn path=/java/tags/dfce-samples-0.9.2.1/; revision=974

--- sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/services/impl/storagedocument/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Ce package contient les classes qui implémentent les interfaces:
- * <li>InsertionService</li>
- * <li>SearchingService </li>
- * <li>RetrievalService </li>
- * <li>SuppressionService</li>
- */
-package fr.urssaf.image.sae.storage.dfce.services.impl.storagedocument;
