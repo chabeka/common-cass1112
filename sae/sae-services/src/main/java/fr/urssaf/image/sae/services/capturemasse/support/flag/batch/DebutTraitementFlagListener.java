cdccc869d96f59981fe57727f24739197ba54cd3
florent.bonnet <florent.bonnet>
2012-03-26 15:22:34 +0000

    débranchement écriture debut_traitement.flag
    svn path=/java/trunk/; revision=2000

---
+++ sae/sae-services/src/main/java/fr/urssaf/image/sae/services/capturemasse/support/flag/batch/DebutTraitementFlagListener.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package fr.urssaf.image.sae.services.capturemasse.support.flag.batch;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.stereotype.Component;
+
+/**
+ * Listener pour l'écriture du fichier fin_traitement.flag
+ * 
+ */
+@Component
+public class DebutTraitementFlagListener {
+
+   private static final Logger LOGGER = LoggerFactory
+         .getLogger(DebutTraitementFlagListener.class);
+
+   /**
+    * Action réalisée après le step
+    * 
+    * @param stepExecution
+    *           le stepExecution
+    */
+   @AfterStep
+   public final ExitStatus end(StepExecution stepExecution) {
+
+      if (CollectionUtils.isNotEmpty(stepExecution.getFailureExceptions())) {
+
+         for (Throwable throwable : stepExecution.getFailureExceptions()) {
+            LOGGER
+                  .debug(
+                        "Erreur lors de l'étape d'écriture du fichier fin_traitement.flag",
+                        throwable);
+         }
+      }
+
+      return stepExecution.getExitStatus();
+
+   }
+
+}
