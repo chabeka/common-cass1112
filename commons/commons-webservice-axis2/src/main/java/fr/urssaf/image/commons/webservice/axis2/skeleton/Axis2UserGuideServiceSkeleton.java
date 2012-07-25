2b130e37dddcf59dc951228ece914fd66f431250
bertrand.barault <bertrand.barault>
2012-07-25 13:21:42 +0000

    projet exemple axis2 avec WAR
    svn path=/java/trunk/; revision=2412

---
+++ commons/commons-webservice-axis2/src/main/java/fr/urssaf/image/commons/webservice/axis2/skeleton/Axis2UserGuideServiceSkeleton.java
@@ -0,0 +1,83 @@
+
+/**
+ * Axis2UserGuideServiceSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.1  Built on : Aug 31, 2011 (12:22:40 CEST)
+ */
+    package fr.urssaf.image.commons.webservice.axis2.skeleton;
+    /**
+     *  Axis2UserGuideServiceSkeleton java skeleton for the axisService
+     */
+    public class Axis2UserGuideServiceSkeleton implements Axis2UserGuideServiceSkeletonInterface{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param doInOnlyRequest0 
+             * @return  
+         */
+        
+                 public void doInOnly
+                  (
+                  org.apache.axis2.axis2userguide.DoInOnlyRequest doInOnlyRequest0
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param twoWayOneParameterEchoRequest1 
+             * @return twoWayOneParameterEchoResponse2 
+         */
+        
+                 public org.apache.axis2.axis2userguide.TwoWayOneParameterEchoResponse twoWayOneParameterEcho
+                  (
+                  org.apache.axis2.axis2userguide.TwoWayOneParameterEchoRequest twoWayOneParameterEchoRequest1
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#twoWayOneParameterEcho");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param noParametersRequest3 
+             * @return noParametersResponse4 
+         */
+        
+                 public org.apache.axis2.axis2userguide.NoParametersResponse noParameters
+                  (
+                  org.apache.axis2.axis2userguide.NoParametersRequest noParametersRequest3
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#noParameters");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param multipleParametersAddItemRequest5 
+             * @return multipleParametersAddItemResponse6 
+         */
+        
+                 public org.apache.axis2.axis2userguide.MultipleParametersAddItemResponse multipleParametersAddItem
+                  (
+                  org.apache.axis2.axis2userguide.MultipleParametersAddItemRequest multipleParametersAddItemRequest5
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#multipleParametersAddItem");
+        }
+     
+    }
+    
