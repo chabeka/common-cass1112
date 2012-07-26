7890a30435bc1babaeefc3c0339140c7eb1ed007
bertrand.barault <bertrand.barault>
2012-07-26 09:18:42 +0000

    mise en place du contexte spring
    svn path=/java/trunk/; revision=2422

--- commons/commons-webservice-axis2/src/main/java/fr/urssaf/image/commons/webservice/axis2/skeleton/Axis2UserGuideServiceSkeleton.java
+++ commons/commons-webservice-axis2/src/main/java/fr/urssaf/image/commons/webservice/axis2/skeleton/Axis2UserGuideServiceSkeleton.java
@@ -1,83 +1,81 @@
-
 /**
  * Axis2UserGuideServiceSkeleton.java
  *
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.1  Built on : Aug 31, 2011 (12:22:40 CEST)
  */
-    package fr.urssaf.image.commons.webservice.axis2.skeleton;
-    /**
-     *  Axis2UserGuideServiceSkeleton java skeleton for the axisService
-     */
-    public class Axis2UserGuideServiceSkeleton implements Axis2UserGuideServiceSkeletonInterface{
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param doInOnlyRequest0 
-             * @return  
-         */
-        
-                 public void doInOnly
-                  (
-                  org.apache.axis2.axis2userguide.DoInOnlyRequest doInOnlyRequest0
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param twoWayOneParameterEchoRequest1 
-             * @return twoWayOneParameterEchoResponse2 
-         */
-        
-                 public org.apache.axis2.axis2userguide.TwoWayOneParameterEchoResponse twoWayOneParameterEcho
-                  (
-                  org.apache.axis2.axis2userguide.TwoWayOneParameterEchoRequest twoWayOneParameterEchoRequest1
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#twoWayOneParameterEcho");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param noParametersRequest3 
-             * @return noParametersResponse4 
-         */
-        
-                 public org.apache.axis2.axis2userguide.NoParametersResponse noParameters
-                  (
-                  org.apache.axis2.axis2userguide.NoParametersRequest noParametersRequest3
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#noParameters");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param multipleParametersAddItemRequest5 
-             * @return multipleParametersAddItemResponse6 
-         */
-        
-                 public org.apache.axis2.axis2userguide.MultipleParametersAddItemResponse multipleParametersAddItem
-                  (
-                  org.apache.axis2.axis2userguide.MultipleParametersAddItemRequest multipleParametersAddItemRequest5
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#multipleParametersAddItem");
-        }
-     
-    }
-    
+package fr.urssaf.image.commons.webservice.axis2.skeleton;
+
+import org.apache.axis2.axis2userguide.DoInOnlyRequest;
+import org.apache.axis2.axis2userguide.MultipleParametersAddItemRequest;
+import org.apache.axis2.axis2userguide.MultipleParametersAddItemResponse;
+import org.apache.axis2.axis2userguide.NoParametersRequest;
+import org.apache.axis2.axis2userguide.NoParametersResponse;
+import org.apache.axis2.axis2userguide.TwoWayOneParameterEchoRequest;
+import org.apache.axis2.axis2userguide.TwoWayOneParameterEchoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fr.urssaf.image.commons.webservice.axis2.service.UserGuideService;
+
+/**
+ * Axis2UserGuideServiceSkeleton java skeleton for the axisService
+ */
+@Component
+public class Axis2UserGuideServiceSkeleton implements
+      Axis2UserGuideServiceSkeletonInterface {
+
+   private final UserGuideService service;
+
+   @Autowired
+   public Axis2UserGuideServiceSkeleton(UserGuideService service) {
+      this.service = service;
+   }
+
+   /**
+    * Auto generated method signature
+    * 
+    * @param doInOnlyRequest
+    */
+   @Override
+   public void doInOnly(DoInOnlyRequest request) {
+
+      service.doInOnly(request);
+
+   }
+
+   /**
+    * Auto generated method signature
+    * 
+    * @param twoWayOneParameterEchoRequest
+    */
+   @Override
+   public TwoWayOneParameterEchoResponse twoWayOneParameterEcho(
+         TwoWayOneParameterEchoRequest request) {
+
+      return service.twoWayOneParameterEcho(request);
+   }
+
+   /**
+    * Auto generated method signature
+    * 
+    * @param noParametersRequest
+    */
+   @Override
+   public NoParametersResponse noParameters(NoParametersRequest request) {
+
+      return service.noParameters(request);
+   }
+
+   /**
+    * Auto generated method signature
+    * 
+    * @param multipleParametersAddItemRequest
+    */
+   @Override
+   public MultipleParametersAddItemResponse multipleParametersAddItem(
+         MultipleParametersAddItemRequest request) {
+
+      return service.multipleParametersAddItem(request);
+   }
+}
