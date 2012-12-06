4003f66d0ed3f37b2ff45eb2b3edede7ce639880
paul.mareche <paul.mareche>
2012-12-06 13:34:52 +0000

    Suppression d'un copié/collé entre une DAO et un service, qui a eu pour effet de bord, suite à une évolution de la DAO, d'une différence de résultats entre l'utilisation de la méthode du service et celle de la DAO (dao.createJobRequestFromResult par rapport à  service.getJobRequestFromResult)
    svn path=/java/trunk/; revision=2895

--- sae/sae-pile-travaux/src/main/java/fr/urssaf/image/sae/pile/travaux/service/impl/JobLectureImpl.java
+++ sae/sae-pile-travaux/src/main/java/fr/urssaf/image/sae/pile/travaux/service/impl/JobLectureImpl.java
@@ -14,7 +14,6 @@ import me.prettyprint.cassandra.service.template.ColumnFamilyResult;
 import me.prettyprint.cassandra.service.template.ColumnFamilyResultWrapper;
 import me.prettyprint.cassandra.utils.TimeUUIDUtils;
 import me.prettyprint.hector.api.Keyspace;
-import me.prettyprint.hector.api.Serializer;
 import me.prettyprint.hector.api.beans.OrderedRows;
 import me.prettyprint.hector.api.factory.HFactory;
 import me.prettyprint.hector.api.query.QueryResult;
@@ -26,19 +25,15 @@ import org.springframework.stereotype.Service;
 
 import fr.urssaf.image.commons.cassandra.helper.HectorIterator;
 import fr.urssaf.image.commons.cassandra.helper.QueryResultConverter;
-import fr.urssaf.image.commons.cassandra.serializer.NullableDateSerializer;
 import fr.urssaf.image.sae.pile.travaux.dao.JobHistoryDao;
 import fr.urssaf.image.sae.pile.travaux.dao.JobRequestDao;
 import fr.urssaf.image.sae.pile.travaux.dao.JobsQueueDao;
 import fr.urssaf.image.sae.pile.travaux.dao.iterator.JobQueueIterator;
 import fr.urssaf.image.sae.pile.travaux.dao.serializer.JobQueueSerializer;
-import fr.urssaf.image.sae.pile.travaux.dao.serializer.VISerializer;
 import fr.urssaf.image.sae.pile.travaux.model.JobHistory;
 import fr.urssaf.image.sae.pile.travaux.model.JobQueue;
 import fr.urssaf.image.sae.pile.travaux.model.JobRequest;
-import fr.urssaf.image.sae.pile.travaux.model.JobState;
 import fr.urssaf.image.sae.pile.travaux.service.JobLectureService;
-import fr.urssaf.image.sae.vi.modele.VIContenuExtrait;
 
 /**
  * Implémentation du service {@link JobLectureService}
@@ -55,25 +50,6 @@ public class JobLectureImpl implements JobLectureService {
    private final JobsQueueDao jobsQueueDao;
 
    private final JobHistoryDao jobHistoryDao;
-
-   // Column Family JobRequest
-   public static final String JR_VI = "vi";
-   private static final String JOBREQUEST_CFNAME = "JobRequest";
-   private static final String JR_TYPE_COLUMN = "type";
-   private static final String JR_PARAMETERS_COLUMN = "parameters";
-   private static final String JR_STATE_COLUMN = "state";
-   private static final String JR_RESERVED_BY_COLUMN = "reservedBy";
-   private static final String JR_CREATION_DATE_COLUMN = "creationDate";
-   private static final String JR_RESERVATION_DATE_COLUMN = "reservationDate";
-   private static final String JR_STARTING_DATE_COLUMN = "startingDate";
-   private static final String JR_ENDING_DATE_COLUMN = "endingDate";
-   private static final String JR_MESSAGE = "message";
-   private static final String JR_SAE_HOST = "saeHost";
-   private static final String JR_CLIENT_HOST = "clientHost";
-   private static final String JR_DOC_COUNT = "docCount";
-   private static final String JR_PID = "pid";
-   public static final String JR_TO_CHECK_FLAG = "toCheckFlag";
-   public static final String JR_TO_CHECK_FLAG_RAISON = "toCheckFlagRaison";
 
    private static final int MAX_JOB_ATTIBUTS = 100;
 
@@ -214,7 +190,7 @@ public class JobLectureImpl implements JobLectureService {
       RangeSlicesQuery<UUID, String, byte[]> rangeSlicesQuery = HFactory
             .createRangeSlicesQuery(keyspace, UUIDSerializer.get(),
                   StringSerializer.get(), bytesSerializer);
-      rangeSlicesQuery.setColumnFamily(JOBREQUEST_CFNAME);
+      rangeSlicesQuery.setColumnFamily(JobRequestDao.JOBREQUEST_CFNAME);
       rangeSlicesQuery.setRange("", "", false, MAX_JOB_ATTIBUTS);
       rangeSlicesQuery.setRowCount(maxKeysToRead);
       QueryResult<OrderedRows<UUID, String, byte[]>> queryResult = rangeSlicesQuery
@@ -232,88 +208,13 @@ public class JobLectureImpl implements JobLectureService {
             result);
       List<JobRequest> list = new ArrayList<JobRequest>();
       for (ColumnFamilyResult<UUID, String> row : resultIterator) {
-         JobRequest jobRequest = getJobRequestFromResult(row);
+         JobRequest jobRequest = jobRequestDao.createJobRequestFromResult(row);
          // On peut obtenir un jobRequest null dans le cas d'un jobRequest
          // effacé
          if (jobRequest != null)
             list.add(jobRequest);
       }
       return list;
-
-   }
-
-   /**
-    * Crée un objet JobRequest à partir de données lues de cassandra.
-    * 
-    * @param result
-    *           Données de cassandra
-    * @return le jobRequest
-    */
-   private JobRequest getJobRequestFromResult(
-         ColumnFamilyResult<UUID, String> result) {
-
-      if (result == null || !result.hasResults()) {
-         return null;
-      }
-      Serializer<Date> dSlz = NullableDateSerializer.get();
-
-      JobRequest jobRequest = new JobRequest();
-
-      jobRequest.setIdJob(result.getKey());
-
-      jobRequest.setType(result.getString(JR_TYPE_COLUMN));
-
-      jobRequest.setParameters(result.getString(JR_PARAMETERS_COLUMN));
-
-      String state = result.getString(JR_STATE_COLUMN);
-      jobRequest.setState(JobState.valueOf(state));
-
-      jobRequest.setReservedBy(result.getString(JR_RESERVED_BY_COLUMN));
-
-      Date creationDate = dSlz.fromBytes(result
-            .getByteArray(JR_CREATION_DATE_COLUMN));
-      jobRequest.setCreationDate(creationDate);
-
-      if (result.getByteArray(JR_RESERVATION_DATE_COLUMN) != null) {
-         Date reservationDate = dSlz.fromBytes(result
-               .getByteArray(JR_RESERVATION_DATE_COLUMN));
-         jobRequest.setReservationDate(reservationDate);
-      }
-
-      if (result.getByteArray(JR_STARTING_DATE_COLUMN) != null) {
-         Date startingDate = dSlz.fromBytes(result
-               .getByteArray(JR_STARTING_DATE_COLUMN));
-         jobRequest.setStartingDate(startingDate);
-      }
-
-      if (result.getByteArray(JR_ENDING_DATE_COLUMN) != null) {
-         Date endingDate = dSlz.fromBytes(result
-               .getByteArray(JR_ENDING_DATE_COLUMN));
-         jobRequest.setEndingDate(endingDate);
-      }
-
-      jobRequest.setMessage(result.getString(JR_MESSAGE));
-
-      jobRequest.setSaeHost(result.getString(JR_SAE_HOST));
-
-      jobRequest.setClientHost(result.getString(JR_CLIENT_HOST));
-
-      jobRequest.setPid(result.getInteger(JR_PID));
-
-      jobRequest.setDocCount(result.getInteger(JR_DOC_COUNT));
-
-      jobRequest.setToCheckFlag(result.getBoolean(JR_TO_CHECK_FLAG));
-
-      jobRequest
-            .setToCheckFlagRaison(result.getString(JR_TO_CHECK_FLAG_RAISON));
-
-      if (result.getByteArray(JR_VI) != null) {
-         VIContenuExtrait extrait = VISerializer.get().fromBytes(
-               result.getByteArray(JR_VI));
-         jobRequest.setVi(extrait);
-      }
-
-      return jobRequest;
 
    }
 
