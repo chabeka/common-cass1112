34659165bf781f4df706c0a0f3492c0308f2981c
paul.mareche <paul.mareche>
2013-03-04 16:17:02 +0000

    Ecran d'affichage des traces : - diminution de la taille de la police des id de traces (uuid) - prise en compte des valeurs d'infos à null - la date de début est positionné à minuit et la date de fin à 23h59, afin d'avoir les journées complètes
    svn path=/java/trunk/; revision=3160

--- sae-integration/sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/controller/TraceVisualisationController.java
+++ sae-integration/sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/controller/TraceVisualisationController.java
@@ -10,11 +10,13 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,17 +40,16 @@ import fr.urssaf.image.sae.trace.service.RegTechniqueService;
 public class TraceVisualisationController {
 
    private static final String NOM_VUE = "traceVisualisation";
-   
+
    @Autowired
    private RegTechniqueService regtechniqueService;
-   
+
    @Autowired
    private JournalEvtService journalEvtService;
-   
-   private TraceFormulaire formulaire = new TraceFormulaire();
-   
 
-   @RequestMapping(value="traceVisualisation", method = RequestMethod.GET)
+   private TraceFormulaire formulaire = new TraceFormulaire();
+
+   @RequestMapping(value = "traceVisualisation", method = RequestMethod.GET)
    public final String defaultView(HttpSession session, Model model) {
       formulaire.setAction("getTrace");
       formulaire.setUrl("traceVisualisation.do");
@@ -56,8 +57,8 @@ public class TraceVisualisationController {
       model.addAttribute("formulaire", formulaire);
       return NOM_VUE;
    }
-   
-   @RequestMapping(value="journalVisualisation", method = RequestMethod.GET)
+
+   @RequestMapping(value = "journalVisualisation", method = RequestMethod.GET)
    public final String defaultViewJournal(HttpSession session, Model model) {
       // on a le même écran que pour les traces ..
       formulaire.setAction("getJournal");
@@ -66,16 +67,14 @@ public class TraceVisualisationController {
       model.addAttribute("formulaire", formulaire);
       return NOM_VUE;
    }
-   
-   
+
    public final TraceFormulaire getFormulairePourGet() {
 
       TraceFormulaire formulaire = new TraceFormulaire();
-      
-      return formulaire;
-      }
 
-   
+      return formulaire;
+   }
+
    /**
     * Retourne la liste des traces pour la periode donnée
     * 
@@ -87,7 +86,7 @@ public class TraceVisualisationController {
     */
    @SuppressWarnings("unchecked")
    @ResponseBody
-   @RequestMapping(value="traceVisualisation", method = RequestMethod.GET, params = "action=getTrace")
+   @RequestMapping(value = "traceVisualisation", method = RequestMethod.GET, params = "action=getTrace")
    public final Map<String, Object> lectureTrace(Model model,
          HttpSession session,
          @RequestParam(value = "dateDebut", required = true) Date dateDebut,
@@ -96,87 +95,117 @@ public class TraceVisualisationController {
          @RequestParam(value = "nbTrace") int nbTrace) {
 
       List listeDetail = new ArrayList();
-      int compteur =0;
-      List<TraceRegTechniqueIndex> traces = regtechniqueService.lecture(dateDebut, dateFin,nbTrace , inverse);
-      List<String> tracContent = new ArrayList<String>();
-      Map<String, Object> traceInfos = new HashMap<String, Object>();
-      if(traces!=null){
+
+      int compteur = 0;
+
+      Date dateDebutOk = calculeDateDebutJournee(dateDebut);
+      Date dateFinOk = calculeDateFinJournee(dateFin);
+
+      List<TraceRegTechniqueIndex> traces = regtechniqueService.lecture(
+            dateDebutOk, dateFinOk, nbTrace, inverse);
+
+      if (CollectionUtils.isNotEmpty(traces)) {
+
          compteur = traces.size();
-         for (TraceRegTechniqueIndex i : traces) {
-            TraceRegTechnique trace = regtechniqueService.lecture(i
+
+         for (TraceRegTechniqueIndex indexTrace : traces) {
+
+            TraceRegTechnique trace = regtechniqueService.lecture(indexTrace
                   .getIdentifiant());
-            Map<String, Object> info = trace.getInfos();
-            if (info != null) {
-               Iterator iter = info.keySet().iterator();
-               while (iter.hasNext()) {
-                  String key = iter.next().toString();
-                  Object valeur = info.get(key);
-                  if (valeur.toString().length() > 10) {
-                     tracContent.add(key + ":"
-                           + valeur.toString().substring(0, 10));
-                  } else {
-                     tracContent.add(key + ":" + valeur.toString());
-                  }
-               }
-               traceInfos.put("info", tracContent);
-            } else {
-               traceInfos.put("info", null);
-            }
+
+            Map<String, Object> traceInfos = traiteInfos(trace.getInfos());
 
             traceInfos.put("trace", trace);
 
             listeDetail.add(traceInfos);
+
          }
       }
+
       // Renvoie du résultat en json
       HashMap<String, Object> map = new HashMap<String, Object>();
       map.put("traces", listeDetail);
       map.put("compteur", compteur);
       return map;
-      
+
    }
-    
-   
-   @SuppressWarnings("unchecked")
+
+   private Map<String, Object> traiteInfos(Map<String, Object> info) {
+
+      List<String> tracContent = new ArrayList<String>();
+      Map<String, Object> traceInfos = new HashMap<String, Object>();
+
+      if (MapUtils.isNotEmpty(info)) {
+
+         String key;
+         Object valeur;
+         for (Map.Entry<String, Object> entry : info.entrySet()) {
+            key = entry.getKey();
+            valeur = entry.getValue();
+            if (valeur == null) {
+               tracContent.add(key + ":" + "null");
+            } else {
+               if (valeur.toString().length() > 10) {
+                  tracContent.add(key + ":"
+                        + valeur.toString().substring(0, 10));
+               } else {
+                  tracContent.add(key + ":" + valeur.toString());
+               }
+            }
+         }
+
+         traceInfos.put("info", tracContent);
+
+      } else {
+         traceInfos.put("info", null);
+      }
+
+      return traceInfos;
+
+   }
+
    @ResponseBody
-   @RequestMapping(value="tracePopUp", method = RequestMethod.GET, params = "action=getTracePopUp")
+   @RequestMapping(value = "tracePopUp", method = RequestMethod.GET, params = "action=getTracePopUp")
    public final Map<String, Object> lectureTrace(Model model,
          HttpSession session,
          @RequestParam(value = "uuid", required = true) UUID uuid) {
 
-      //List<TraceRegTechnique> listeDetail = new ArrayList<TraceRegTechnique>();
-      
+      // List<TraceRegTechnique> listeDetail = new
+      // ArrayList<TraceRegTechnique>();
 
-         TraceRegTechnique trace = regtechniqueService.lecture(uuid);
-      //   listeDetail.add(trace);
-      
+      TraceRegTechnique trace = regtechniqueService.lecture(uuid);
+      // listeDetail.add(trace);
+
       // Renvoie du résultat en json
       HashMap<String, Object> map = new HashMap<String, Object>();
       map.put("trace", trace);
       return map;
-      
+
    }
-   
+
    @SuppressWarnings("unchecked")
    @ResponseBody
-   @RequestMapping(value="tracePopUpInfo", method = RequestMethod.GET, params = "action=getTracePopUpInfo")
-   public final List<String> afficheInfo(Model model,
-         HttpSession session,
+   @RequestMapping(value = "tracePopUpInfo", method = RequestMethod.GET, params = "action=getTracePopUpInfo")
+   public final List<String> afficheInfo(Model model, HttpSession session,
          @RequestParam(value = "uuid", required = true) UUID uuid) {
-      
+
       List<String> tracContent = new ArrayList<String>();
       TraceRegTechnique trace = regtechniqueService.lecture(uuid);
-      Map<String, Object>info = trace.getInfos();
-      Iterator iter =info.keySet().iterator(); 
-      while(iter.hasNext()){
+      Map<String, Object> info = trace.getInfos();
+      Iterator iter = info.keySet().iterator();
+      while (iter.hasNext()) {
          String key = iter.next().toString();
-          Object valeur = info.get(key);
-          tracContent.add(key+":"+StringEscapeUtils.escapeHtml(valeur.toString()));             
+         Object valeur = info.get(key);
+         if (valeur == null) {
+            tracContent.add(key + ":null");
+         } else {
+            tracContent.add(key + ":"
+                  + StringEscapeUtils.escapeHtml(valeur.toString()));
+         }
       }
-      return tracContent;      
+      return tracContent;
    }
-   
-   
+
    /**
     * Retourne la liste des Constrat service
     * 
@@ -188,7 +217,7 @@ public class TraceVisualisationController {
     */
    @SuppressWarnings("unchecked")
    @ResponseBody
-   @RequestMapping(value="journalVisualisation", method = RequestMethod.GET, params = "action=getJournal")
+   @RequestMapping(value = "journalVisualisation", method = RequestMethod.GET, params = "action=getJournal")
    public final Map<String, Object> lectureJournal(Model model,
          HttpSession session,
          @RequestParam(value = "dateDebut", required = true) Date dateDebut,
@@ -197,46 +226,63 @@ public class TraceVisualisationController {
          @RequestParam(value = "nbTrace") int nbTrace) {
 
       List listeDetail = new ArrayList();
+
       int compteur = 0;
-      List<TraceJournalEvtIndex> journaux = journalEvtService.lecture(dateDebut, dateFin,nbTrace , inverse);
-      List<String> journalContent = new ArrayList<String>();
-      Map<String, Object> journalInfos = new HashMap<String, Object>();
-      if (journaux != null) {
+
+      Date dateDebutOk = calculeDateDebutJournee(dateDebut);
+      Date dateFinOk = calculeDateFinJournee(dateFin);
+
+      List<TraceJournalEvtIndex> journaux = journalEvtService.lecture(
+            dateDebutOk, dateFinOk, nbTrace, inverse);
+
+      if (CollectionUtils.isNotEmpty(journaux)) {
+
          compteur = journaux.size();
-         for (TraceJournalEvtIndex i : journaux) {
-            TraceJournalEvt journal = journalEvtService.lecture(i
+
+         for (TraceJournalEvtIndex indexJournal : journaux) {
+
+            TraceJournalEvt journal = journalEvtService.lecture(indexJournal
                   .getIdentifiant());
-            Map<String, Object> info = journal.getInfos();
-            if (info != null) {
-               Iterator iter = info.keySet().iterator();
-               while (iter.hasNext()) {
-                  String key = iter.next().toString();
-                  Object valeur = info.get(key);
-                  if (valeur.toString().length() > 10) {
-                     journalContent.add(key + ":"
-                           + valeur.toString().substring(0, 10));
-                  } else {
-                     journalContent.add(key + ":" + valeur.toString());
-                  }
-               }
-               journalInfos.put("info", journalContent);
-            } else {
-               journalInfos.put("info", null);
-            }
+
+            Map<String, Object> journalInfos = traiteInfos(journal.getInfos());
 
             journalInfos.put("trace", journal);
 
             listeDetail.add(journalInfos);
+
          }
       }
-      
+
       // Renvoie du résultat en json
       HashMap<String, Object> map = new HashMap<String, Object>();
       map.put("traces", listeDetail);
       map.put("compteur", compteur);
       return map;
-      
-   }
-   
-}
 
+   }
+
+   private Date calculeDateDebutJournee(Date dateDebut) {
+
+      Date dateDebutOk = dateDebut;
+      dateDebutOk = DateUtils.setHours(dateDebutOk, 0);
+      dateDebutOk = DateUtils.setMinutes(dateDebutOk, 0);
+      dateDebutOk = DateUtils.setSeconds(dateDebutOk, 0);
+      dateDebutOk = DateUtils.setMilliseconds(dateDebutOk, 0);
+
+      return dateDebutOk;
+
+   }
+
+   private Date calculeDateFinJournee(Date dateFin) {
+
+      Date dateFinOk = dateFin;
+      dateFinOk = DateUtils.setHours(dateFinOk, 23);
+      dateFinOk = DateUtils.setMinutes(dateFinOk, 59);
+      dateFinOk = DateUtils.setSeconds(dateFinOk, 59);
+      dateFinOk = DateUtils.setMilliseconds(dateFinOk, 999);
+
+      return dateFinOk;
+
+   }
+
+}
