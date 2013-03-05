16ae375691e03153c6386bd6569e5155afd8208c
rahee.ghurbhum <rahee.ghurbhum>
2013-03-05 09:54:48 +0000

    Redmine 3096 test 1104 + listeCastest.xml
    svn path=/java/trunk/; revision=3168

---
+++ sae-integration/sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/controller/Test1104Controller.java
@@ -0,0 +1,177 @@
+package fr.urssaf.image.sae.integration.ihmweb.controller;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.collections.ListUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.urssaf.image.sae.integration.ihmweb.constantes.SaeIntegrationConstantes;
+import fr.urssaf.image.sae.integration.ihmweb.formulaire.RechercheFormulaire;
+import fr.urssaf.image.sae.integration.ihmweb.formulaire.TestWsRechercheFormulaire;
+import fr.urssaf.image.sae.integration.ihmweb.formulaire.ViFormulaire;
+import fr.urssaf.image.sae.integration.ihmweb.modele.CodeMetadonneeList;
+import fr.urssaf.image.sae.integration.ihmweb.modele.MetadonneeValeurList;
+import fr.urssaf.image.sae.integration.ihmweb.modele.PagmList;
+import fr.urssaf.image.sae.integration.ihmweb.modele.ResultatTest;
+import fr.urssaf.image.sae.integration.ihmweb.modele.TestStatusEnum;
+import fr.urssaf.image.sae.integration.ihmweb.saeservice.comparator.ResultatRechercheComparator;
+import fr.urssaf.image.sae.integration.ihmweb.saeservice.comparator.ResultatRechercheComparator.TypeComparaison;
+import fr.urssaf.image.sae.integration.ihmweb.saeservice.modele.SaeServiceStub.RechercheResponse;
+import fr.urssaf.image.sae.integration.ihmweb.saeservice.modele.SaeServiceStub.ResultatRechercheType;
+
+/**
+ * 1104-Droits-Conformite-Recherche-ATT-VIGI
+ */
+@Controller
+@RequestMapping(value = "test1104")
+@SuppressWarnings( { "PMD.AvoidDuplicateLiterals" })
+public class Test1104Controller extends
+      AbstractTestWsController<TestWsRechercheFormulaire> {
+
+   private static final int WAITED_COUNT = 8;
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   protected final String getNumeroTest() {
+      return "1104";
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   protected String getNomVue() {
+      return "testDrRe";
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   protected final TestWsRechercheFormulaire getFormulairePourGet() {
+
+      TestWsRechercheFormulaire formulaire = new TestWsRechercheFormulaire();
+      RechercheFormulaire formRecherche = formulaire.getRecherche();
+      formRecherche.getResultats().setStatus(TestStatusEnum.SansStatus);
+
+      // Requête de recherche correspondant au jeu de test inséré en base
+      // d'intégration
+      formRecherche.setRequeteLucene(getCasTest().getLuceneExemple());
+
+      // Pas de métadonnées spécifiques à récupérer
+      CodeMetadonneeList codesMeta = new CodeMetadonneeList();
+      codesMeta.add("ApplicationProductrice");
+      codesMeta.add("CodeRND");
+      codesMeta.add("DateCreation");
+      codesMeta.add("Denomination");
+      codesMeta.add("NumeroRecours");
+      codesMeta.add("Siren");
+      formRecherche.setCodeMetadonnees(codesMeta);
+
+      // Paramètres du VI
+      ViFormulaire viForm = formulaire.getViFormulaire();
+      viForm.setIssuer("INT_CS_ATT_VIGI");
+      viForm.setRecipient(SaeIntegrationConstantes.VI_DEFAULT_RECIPIENT);
+      viForm.setAudience(SaeIntegrationConstantes.VI_DEFAULT_AUDIENCE);
+      viForm.setIdCertif("2");
+      PagmList pagmList = new PagmList();
+      viForm.setPagms(pagmList);
+      pagmList.add("INT_PAGM_ATT_VIGI_RECH");
+
+      return formulaire;
+
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   protected final void doPost(TestWsRechercheFormulaire formulaire) {
+      recherche(formulaire.getUrlServiceWeb(), formulaire.getRecherche(),
+            formulaire.getViFormulaire());
+   }
+
+   private void recherche(String urlServiceWeb, RechercheFormulaire formulaire,
+         ViFormulaire viParams) {
+
+      // Initialise
+      ResultatTest resultatTest = formulaire.getResultats();
+
+      // Résultats attendus
+      int nbResultatsAttendus = WAITED_COUNT;
+      boolean flagResultatsTronquesAttendu = false;
+
+      // Appel de la méthode de test
+      RechercheResponse response = getRechercheTestService()
+            .appelWsOpRechercheReponseCorrecteAttendue(urlServiceWeb,
+                  formulaire, nbResultatsAttendus,
+                  flagResultatsTronquesAttendu, TypeComparaison.NumeroRecours,
+                  viParams);
+
+      // Vérifications en profondeur
+      if ((response != null)
+            && (!TestStatusEnum.Echec.equals(resultatTest.getStatus()))) {
+
+         // Tri les résultats par ordre croissant de DateCreation
+         List<ResultatRechercheType> resultatsTries = Arrays.asList(response
+               .getRechercheResponse().getResultats().getResultat());
+         Collections.sort(resultatsTries, new ResultatRechercheComparator(
+               TypeComparaison.NumeroRecours));
+
+         // Vérifie chaque résultat
+         verifieResultatN(1, resultatsTries.get(0), resultatTest, "1");
+         verifieResultatN(2, resultatsTries.get(1), resultatTest, "2");
+         verifieResultatN(3, resultatsTries.get(2), resultatTest, "3");
+         verifieResultatN(4, resultatsTries.get(3), resultatTest, "5");
+         verifieResultatN(5, resultatsTries.get(4), resultatTest, "6");
+         verifieResultatN(6, resultatsTries.get(5), resultatTest, "7");
+         verifieResultatN(7, resultatsTries.get(6), resultatTest, "9");
+         verifieResultatN(8, resultatsTries.get(7), resultatTest, "10");
+
+      }
+
+      // On passe le test à OK si tous les contrôles sont passées
+      if (!TestStatusEnum.Echec.equals(resultatTest.getStatus())) {
+         resultatTest.setStatus(TestStatusEnum.Succes);
+      }
+
+   }
+
+   private void verifieResultatN(int numeroResultatRecherche,
+         ResultatRechercheType resultatRecherche, ResultatTest resultatTest,
+         String numeroRecours) {
+
+      MetadonneeValeurList valeursAttendues = new MetadonneeValeurList();
+
+      
+      valeursAttendues.add("ApplicationProductrice", "ADELAIDE");      
+      valeursAttendues.add("DateCreation", "2007-04-01");
+      valeursAttendues.add("Denomination",
+            "Test 1104-Droits-Conformite-Recherche-ATT-VIGI");
+      valeursAttendues.add("NumeroRecours", numeroRecours);
+      valeursAttendues.add("Siren", "3090000001");
+      
+      if(ArrayUtils.contains(new Integer[]{1,5,9},numeroRecours)){
+         valeursAttendues.add("CodeRND", "2.3.1.1.12");
+      }
+      if(ArrayUtils.contains(new Integer[]{6,2,10},numeroRecours)){
+         valeursAttendues.add("CodeRND", "2.3.1.1.8");
+      }
+      if(ArrayUtils.contains(new Integer[]{3,7},numeroRecours)){
+         valeursAttendues.add("CodeRND", "2.3.1.1.3");
+      }
+
+      getRechercheTestService().verifieResultatRecherche(resultatRecherche,
+            Integer.toString(numeroResultatRecherche), resultatTest,
+            valeursAttendues);
+
+   }
+
+}
