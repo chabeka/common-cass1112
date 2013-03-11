1ab065e62ff2d4689006ae71d430b156c08d21f5
romain.hardouin <romain.hardouin>
2013-03-11 13:23:23 +0000

    redmine #3269 : Evolution du script check_dfce_jobs.sh pour le lot 130400SAE. Le script grep désormais sur 10 fichiers de logs au lieu d'un seul.
    svn path=/java/trunk/; revision=3193

--- sae/sae-livrable/src/scripts/check_dfce_jobs.sh
+++ sae/sae-livrable/src/scripts/check_dfce_jobs.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# Scripts de vérification des logs des agents DFCE du SAE.
+# Scripts de vérification des logs de l'agent SAE.
 #
-# Le script vérifie que tous les agents DFCE sont terminés.
+# Le script vérifie que tous les jobs sont terminés avec succès.
 # Exit code : Nombre de traitements non terminés. 
 
-# Répertoire des logs
+# Répertoire des logs SAE
 LOG_DIR="/hawai/logs/sae"
 
-# Fichier de log des agents DFCE
-LOG_FILE="$LOG_DIR/sae_dfce_admin_exploit-debug.log"
-
-# Historique des logs.
-#
-# Permet de tracer les précédentes exécutions. 
-# Utile pour une vérification humaine des logs.
-# Doit être géré via logrotate.
-HISTORY_FILE="$LOG_DIR/sae_dfce_admin_exploit.history.log"
-
-# Fichier temporaire pour faire le pseudo log rotate
-HISTORY_FILE_TMP="$HISTORY_FILE.tailed"
-
-# Cron qui lance les agents DFCE sur un seul serveur d'un seul CNP
+# Cron qui lance l'agent SAE sur un seul serveur d'un seul CNP
 CRON_AGENT_FILE="/etc/cron.d/agents_sae"
+
 
 # Exit code
 ERRNO=0
@@ -44,39 +32,62 @@ else
     fi
 fi
 
-if ! grep -q "Fin de la purge des événements de type documents" $LOG_FILE; then
+#
+# ============ Analyses des logs sae-dfce-admin-exploit.jar ============ 
+#
+
+if ! grep -q "Fin de la purge des événements de type documents" "$LOG_DIR/sae_dfce_admin_exploit-clearDocEvent.log"; then
     error "La purge des évènements de type documents n'est pas terminée"
     ERRNO+=1
 fi
 
-if ! grep -q "Fin de la purge des événements de type système" $LOG_FILE; then
+if ! grep -q "Fin de la purge des événements de type système" "$LOG_DIR/sae_dfce_admin_exploit-clearSystemEvent.log"; then
     error "La purge des évènements de type système n'est pas terminée"
     ERRNO+=1
 fi
 
-if ! grep -q "Journalisation de type système est terminée" $LOG_FILE; then
+if ! grep -q "Journalisation de type système est terminée" "$LOG_DIR/sae_dfce_admin_exploit-creatSystemEvent.log"; then
     error "La journalisation de type système n'est pas terminée"
     ERRNO+=1
 fi
 
-if ! grep -q "Journalisation de type documents est terminée" $LOG_FILE; then
+if ! grep -q "Journalisation de type documents est terminée" "$LOG_DIR/sae_dfce_admin_exploit-createDocEvent.log"; then
     error "La journalisation de type documents n'est pas terminée"
     ERRNO+=1
 fi
 
-if ! grep -q "Réindexation DFCE terminée" $LOG_FILE; then
+if ! grep -q "Réindexation DFCE terminée" "$LOG_DIR/sae_dfce_admin_exploit-reindex.log"; then
     error "La réindexation de la base DFCE n'est pas terminée"
     ERRNO+=1
 fi
 
-# Sauvegarde des logs
-cat $LOG_FILE >> $HISTORY_FILE
+#
+# ============ Analyses des logs sae-trace-executable.jar ============ 
+#
 
-# Pseudo logrotate : on garde les 1000 dernières lignes.
-tail -1000 $HISTORY_FILE > $HISTORY_FILE_TMP
-mv -f $HISTORY_FILE_TMP $HISTORY_FILE
+if ! grep -q "fin du traitement de la journalisation pour le type JOURNALISATION_EVT" "$LOG_DIR/sae_trace_executable-JOURNALISATION_EVT.log"; then
+    error "La journalisation JOURNALISATION_EVT n'est pas terminée"
+    ERRNO+=1
+fi
 
-# RAZ des logs pour la prochaine exécution
-echo "" > $LOG_FILE
+if ! grep -q "fin du traitement de la purge pour le type PURGE_EVT" "$LOG_DIR/sae_trace_executable-PURGE_EVT.log"; then
+    error "La purge de type PURGE_EVT n'est pas terminée"
+    ERRNO+=1
+fi
+
+if ! grep -q "fin du traitement de la purge pour le type PURGE_TECHNIQUE" "$LOG_DIR/sae_trace_executable-PURGE_TECHNIQUE.log"; then
+    error "La purge de type PURGE_TECHNIQUE n'est pas terminée"
+    ERRNO+=1
+fi
+
+if ! grep -q "fin du traitement de la purge pour le type PURGE_EXPLOITATION" "$LOG_DIR/sae_trace_executable-PURGE_EXPLOITATION.log"; then
+    error "La purge de type PURGE_EXPLOITATION n'est pas terminée"
+    ERRNO+=1
+fi
+
+if ! grep -q "fin du traitement de la purge pour le type PURGE_SECURITE" "$LOG_DIR/sae_trace_executable-PURGE_SECURITE.log"; then
+    error "La purge de type PURGE_SECURITE n'est pas terminée"
+    ERRNO+=1
+fi
 
 exit $ERRNO
