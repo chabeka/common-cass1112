67289524f7d8c2331b85c27a5ec413274648f182
rahee.ghurbhum <rahee.ghurbhum>
2013-05-13 15:42:04 +0000

    Ajout de deux test pour vérifier que les capture sont rejetées si : - la valeur de la métadonnées ne figure pas dans le dictionnaire - Le dictionnaire associé est inexistant
    svn path=/java/trunk/; revision=3347

--- saedemos/sae-webservices-test/src/test/java/fr/urssaf/image/sae/webservices/service/ArchivageUnitaireTest.java
+++ saedemos/sae-webservices-test/src/test/java/fr/urssaf/image/sae/webservices/service/ArchivageUnitaireTest.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.axis2.AxisFault;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.io.FileUtils;
@@ -28,16 +29,17 @@ import fr.urssaf.image.sae.webservices.configuration.EcdeManager;
 import fr.urssaf.image.sae.webservices.modele.SaeServiceStub.ArchivageUnitaireResponseType;
 import fr.urssaf.image.sae.webservices.service.factory.ObjectModelFactory;
 import fr.urssaf.image.sae.webservices.service.model.Metadata;
-
+import fr.urssaf.image.sae.webservices.util.SoapTestUtils;
 
 /**
  * Tests de l'opération "archivageUnitaire" pour lesquels on attend une réponse
  * correcte (pas de SoapFault)<br>
  * <br>
- * Il faut penser à configurer son SAE local pour établir une configuration ECDE entre :<br>
- *  - DNS : ecde.cer69.recouv
- *  - Point de montage : REPERTOIRE_TEMP_UTILISATEUR\ecde
- *                       Exemple : C:\DOCUME~1\CER699~1\LOCALS~1\Temp\ecde 
+ * Il faut penser à configurer son SAE local pour établir une configuration ECDE
+ * entre :<br>
+ * - DNS : ecde.cer69.recouv - Point de montage :
+ * REPERTOIRE_TEMP_UTILISATEUR\ecde Exemple :
+ * C:\DOCUME~1\CER699~1\LOCALS~1\Temp\ecde
  * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -101,7 +103,8 @@ public class ArchivageUnitaireTest {
             "1999-11-25"));
       metadatas.add(ObjectModelFactory.createMetadata("DateDebutConservation",
             "2011-09-02"));
-      metadatas.add(ObjectModelFactory.createMetadata("ReferenceDocumentaire", "213039953275"));
+      metadatas.add(ObjectModelFactory.createMetadata("ReferenceDocumentaire",
+            "213039953275"));
 
       URI urlEcde = URI
             .create("ecde://ecde.cer69.recouv/DCL001/19991231/3/documents/attestation.pdf");
@@ -137,8 +140,122 @@ public class ArchivageUnitaireTest {
 
       ConsultationUtilsTest consultationTest = new ConsultationUtilsTest();
 
-      consultationTest.assertConsultationResponse(consultation.consultation(idArchive),
-            expectedMetadatas, srcFile);
+      consultationTest.assertConsultationResponse(consultation
+            .consultation(idArchive), expectedMetadatas, srcFile);
 
+   }
+
+   /**
+    * Test permettant de vérifier qu'une capture échoue si une métadonnée avec
+    * dictionnaire présente une valeur qui n'est référencée
+    * 
+    * @throws URISyntaxException
+    * @throws FileNotFoundException
+    * @throws IOException
+    */
+
+   @Test
+   public void archivageUnitaire_failure_metadataValueNotInDict()
+         throws URISyntaxException, FileNotFoundException, IOException {
+
+      // appel du service archivage unitaire
+
+      List<Metadata> metadatas = new ArrayList<Metadata>();
+
+      metadatas.add(ObjectModelFactory.createMetadata(
+            "CodeOrganismeProprietaire", "CER69"));
+      metadatas.add(ObjectModelFactory.createMetadata(
+            "CodeOrganismeGestionnaire", "UR750"));
+      metadatas.add(ObjectModelFactory.createMetadata("CodeRND", "2.3.1.1.12"));
+      metadatas.add(ObjectModelFactory.createMetadata("VersionRND", "11.1"));
+      metadatas.add(ObjectModelFactory.createMetadata("NbPages", "2"));
+      metadatas.add(ObjectModelFactory.createMetadata("FormatFichier",
+            "fmt/354"));
+      metadatas.add(ObjectModelFactory.createMetadata("DateCreation",
+            "2012-01-01"));
+      metadatas.add(ObjectModelFactory.createMetadata("Titre",
+            "Attestation de vigilance"));
+      metadatas.add(ObjectModelFactory.createMetadata("TypeHash", "SHA-1"));
+      metadatas.add(ObjectModelFactory.createMetadata("Hash", "128775498884"));
+      metadatas.add(ObjectModelFactory.createMetadata("DateReception",
+            "1999-11-25"));
+      metadatas.add(ObjectModelFactory.createMetadata("DateDebutConservation",
+            "2011-09-02"));
+      // cette metadonnées nes pas référencée dans le dictionnaire des données
+      // dictMeta2
+      metadatas.add(ObjectModelFactory.createMetadata("metadonne2", "3"));
+
+      URI urlEcde = URI
+            .create(" ecde.cer69.recouv/DCL001/19991231/3/documents/attestation.pdf");
+      // URI urlEcde = URI
+      // .create("ecde://CER69-TEC24251/SAE_INTEGRATION/20110822/CaptureUnitaire-101-CaptureUnitaire-OK-Standard/documents/doc1.PDF");
+      try {
+         ArchivageUnitaireResponseType archivageResponse = archivage
+               .archivageUnitaire(urlEcde, metadatas);
+      } catch (Exception e) {
+         Assert
+               .assertEquals(
+                     "La valeur de la métadonnée metadonne2 est incorrecte: elle n'est pas comprise dans le dictionnaire de données associé",
+                     e.getMessage());
+      }
+
+   }
+   
+   /**
+    * test vérifiant l'échec d'une capture si le dictionnaire est inexistant. la MetadataRuntimeException est transformée en erreur interne de capture
+    * @throws URISyntaxException
+    * @throws FileNotFoundException
+    * @throws IOException
+    */
+   @Test
+   public void archivageUnitaireFailureNoDict()
+         throws URISyntaxException, FileNotFoundException, IOException {
+
+      // appel du service archivage unitaire
+
+      List<Metadata> metadatas = new ArrayList<Metadata>();
+
+      metadatas.add(ObjectModelFactory.createMetadata(
+            "CodeOrganismeProprietaire", "CER69"));
+      metadatas.add(ObjectModelFactory.createMetadata(
+            "CodeOrganismeGestionnaire", "UR750"));
+      metadatas.add(ObjectModelFactory.createMetadata("CodeRND", "2.3.1.1.12"));
+      metadatas.add(ObjectModelFactory.createMetadata("VersionRND", "11.1"));
+      metadatas.add(ObjectModelFactory.createMetadata("NbPages", "2"));
+      metadatas.add(ObjectModelFactory.createMetadata("FormatFichier",
+            "fmt/354"));
+      metadatas.add(ObjectModelFactory.createMetadata("DateCreation",
+            "2012-01-01"));
+      metadatas.add(ObjectModelFactory.createMetadata("Titre",
+            "Attestation de vigilance"));
+      metadatas.add(ObjectModelFactory.createMetadata("TypeHash", "SHA-1"));
+      metadatas.add(ObjectModelFactory.createMetadata("Hash", "128775498884"));
+      metadatas.add(ObjectModelFactory.createMetadata("DateReception",
+            "1999-11-25"));
+      metadatas.add(ObjectModelFactory.createMetadata("DateDebutConservation",
+            "2011-09-02"));
+      // cette metadonnées nes pas référencée dans le dictionnaire des données
+      // dictMeta2
+      metadatas.add(ObjectModelFactory.createMetadata("metadonne3", "3"));
+
+//      URI urlEcde = URI
+//            .create(" ecde.cer69.recouv/DCL001/19991231/3/documents/attestation.pdf");
+       URI urlEcde = URI
+       .create("ecde://CER69-TEC24251/SAE_INTEGRATION/20110822/CaptureUnitaire-101-CaptureUnitaire-OK-Standard/documents/doc1.PDF");
+      try {
+         ArchivageUnitaireResponseType archivageResponse = archivage
+               .archivageUnitaire(urlEcde, metadatas);
+      } catch (AxisFault fault) {
+         // on vérifie que la soapfault a été causé par le MetadataRuntimeexception 
+         Assert.assertTrue(fault.getDetail().toString().contains("MetadataRuntimeException: Le dictionnaire dictExistePas n'a pas été trouvée"));
+         // on vérifie que l'exception levée est une erreur interne de capture.
+         SoapTestUtils
+               .assertAxisFault(
+                     fault,
+                     "Une erreur interne à l'application est survenue lors de la capture.",
+                     "ErreurInterneCapture", SoapTestUtils.SAE_NAMESPACE,
+                     SoapTestUtils.SAE_PREFIX);
+
+      }      
    }
 }
