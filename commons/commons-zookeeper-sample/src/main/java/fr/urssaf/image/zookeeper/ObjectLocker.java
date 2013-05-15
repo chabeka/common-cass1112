ef705066006eb51520d2802729768ee0e8c789e7
florent.bonnet <florent.bonnet>
2013-05-15 10:23:28 +0000

    [maven-release-plugin]  copy for tag commons-dfce-1.2.4-CRTL-1
    svn path=/java/tags/commons-dfce-1.2.4-CRTL-1/; revision=3361

--- commons/commons-zookeeper-sample/src/main/java/fr/urssaf/image/zookeeper/ObjectLocker.java
+++
@@ -1,9 +0,0 @@
-package fr.urssaf.image.zookeeper;
-
-import fr.urssaf.image.zookeeper.exception.ZookeeperEx;
-
-public interface ObjectLocker {
-   public boolean lockObject(String noeud) throws ZookeeperEx;
-
-   public boolean unlockObject(String noeud);
-}
