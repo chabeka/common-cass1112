97cf64ae4ee2d951c4b38fa3e60fe28bf4194818
aurelie.garnier <aurelie.garnier>
2013-05-21 13:01:07 +0000

    Redmine 2974 : Automatisation de la mise à jour du RND Ajout de contrôle Aspect d'arguments
    svn path=/java/trunk/; revision=3381

--- sae/sae-rnd/src/main/java/fr/urssaf/image/sae/rnd/aspect/ServiceValidation.java
+++ sae/sae-rnd/src/main/java/fr/urssaf/image/sae/rnd/aspect/ServiceValidation.java
@@ -3,8 +3,10 @@ package fr.urssaf.image.sae.rnd.aspect;
 import org.apache.commons.lang.StringUtils;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-
 import fr.urssaf.image.sae.rnd.modele.Correspondance;
+import fr.urssaf.image.sae.rnd.modele.TypeCode;
+import fr.urssaf.image.sae.rnd.modele.TypeDocument;
+import fr.urssaf.image.sae.rnd.modele.VersionRnd;
 
 /**
  * Classe de validation
@@ -17,19 +19,102 @@ public class ServiceValidation {
    private static final String AJOUTER_CORRES = "execution(void fr.urssaf.image.sae.rnd.dao.support.CorrespondancesRndSupport.ajouterCorrespondance(*,*))"
          + "&& args(correspondance, clock)";
 
+   private static final String AJOUTER_RND = "execution(void fr.urssaf.image.sae.rnd.dao.support.RndSupport.ajouterRnd(*, *))"
+         + "&& args(typeDoc, clock)";
+
+   private static final String UPDATE_VERSION = "execution(void fr.urssaf.image.sae.rnd.dao.support.SaeBddSupport.updateVersionRnd(*))"
+         + "&& args(versionRnd)";
+
+   private static final String RND_SERVICE = "execution(java.lang.String fr.urssaf.image.sae.rnd.service.RndService.get*(*))"
+         + "&& args(codeRnd)";
+
+   /**
+    * Vérification des arguments des méthodes get de RndService
+    * 
+    * @param codeRnd
+    *           le code RND
+    */
+   @Before(RND_SERVICE)
+   public final void validationRndService(String codeRnd) {
+
+      if (StringUtils.isBlank(codeRnd)) {
+         throw new IllegalArgumentException(
+               "l'argument codeRnd n'est pas valide : codeRnd null ou vide");
+      }
+
+   }
+
+   
+   @Before(UPDATE_VERSION)
+   public final void validationArgUpdateVersionRnd(VersionRnd versionRnd) {
+      if (versionRnd == null) {
+         throw new IllegalArgumentException("l'argument versionRnd n'est pas valide");
+      } else {
+         if (versionRnd.getDateMiseAJour() == null) {
+            throw new IllegalArgumentException("l'argument versionRnd n'est pas valide : date de mise à jour null");
+         }
+         if (StringUtils.isBlank(versionRnd.getVersionEnCours())) {
+            throw new IllegalArgumentException("l'argument versionRnd n'est pas valide : numéro version null ou vide");
+         }
+      }
+   }
+   
+   /**
+    * Validation des arguments de la méthode d'ajout d'un type de document
+    * 
+    * @param typeDoc
+    *           le type de doc à ajouter
+    * @param clock
+    *           Horloge de la création
+    */
+   @Before(AJOUTER_RND)
+   public final void validationArgAjouterRnd(TypeDocument typeDoc, long clock) {
+
+      if (clock == 0) {
+         throw new IllegalArgumentException("l'argument clock n'est pas valide");
+      }
+
+      if (typeDoc == null) {
+         throw new IllegalArgumentException(
+               "l'argument typeDoc n'est pas valide");
+      } else {
+         // Le code RND ne peut pas être null
+         if (StringUtils.isBlank(typeDoc.getCode())) {
+            throw new IllegalArgumentException(
+                  "l'argument correspondance n'est pas valide : code Rnd null ou vide");
+         }
+
+         // Le code activité peut être null
+
+         // Le code fonction peut être null uniquement pour les types de
+         // document temporaire
+         if (!TypeCode.TEMPORAIRE.equals(typeDoc.getType())
+               && StringUtils.isBlank(typeDoc.getCodeFonction())) {
+            throw new IllegalArgumentException(
+                  "l'argument codeFonction n'est pas valide : codeFonction null ou vide alors que le type de document n'est pas TEMPORAIRE");
+         }
+
+         // La durée de conservation doit être renseignée
+         if (typeDoc.getDureeConservation() <= 0) {
+            throw new IllegalArgumentException(
+                  "l'argument dureeConservation n'est pas valide : dureeConservation négatif");
+         }
+      }
+   }
+
    /**
     * Vérification des arguments de la méthode ajouterCorrespondance
     * 
     * @param correspondance
     *           la correspondance
-    * @param clock 
+    * @param clock
     *           Horloge de la création
     */
    @Before(AJOUTER_CORRES)
-   public final void validationArgRechercherDoc(Correspondance correspondance,
-         Long clock) {
+   public final void validationArgAjouterCorrespondance(
+         Correspondance correspondance, long clock) {
 
-      if (clock == null) {
+      if (clock == 0) {
          throw new IllegalArgumentException("l'argument clock n'est pas valide");
       }
 
