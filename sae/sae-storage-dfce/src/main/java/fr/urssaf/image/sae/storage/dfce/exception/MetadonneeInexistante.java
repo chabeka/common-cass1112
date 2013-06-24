9548bb25ef6456213a56f18249683272aaffcbf6
paul.mareche <paul.mareche>
2013-06-24 06:53:40 +0000

    javadoc
    svn path=/java/trunk/; revision=3540

--- sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/exception/MetadonneeInexistante.java
+++ sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/exception/MetadonneeInexistante.java
@@ -1,8 +1,11 @@
 package fr.urssaf.image.sae.storage.dfce.exception;
 
+/**
+ * Exception levée lorsqu'une métadonnée n'est pas trouvée dans DFCE alors qu'on
+ * l'y attendait
+ */
 public class MetadonneeInexistante extends Exception {
    private static final long serialVersionUID = 1L;
-
 
    /**
     * Constructeur
