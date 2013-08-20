0e33e8c5aa5c9ae681b7f7209ea9b3a71dc05f6d
paul.mareche <paul.mareche>
2013-08-20 09:39:28 +0000

    Régio vague 3 : mise à jour du TU permettant de traiter les fichiers V2 pour la régionalisation. On ne fait plus de tri de données dans les fichiers CSV, les traitements sont fait dans Postgresql.
    svn path=/java/trunk/; revision=3571

--- sae-regionalisation/src/test/java/fr/urssaf/image/sae/regionalisation/util/ConversionFileTest.java
+++ sae-regionalisation/src/test/java/fr/urssaf/image/sae/regionalisation/util/ConversionFileTest.java
@@ -1,201 +1,27 @@
-/**
- * 
- */
 package fr.urssaf.image.sae.regionalisation.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
-import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.core.io.ClassPathResource;
-
-import au.com.bytecode.opencsv.CSVReader;
 
 public class ConversionFileTest {
 
-   private static final String COTI_CLE = "COTI_CLE";
-   private static final String CPTE_CLE = "CPTE_CLE";
-   private static final String PERS_CLE = "PERS_CLE";
-
    @Test
    @Ignore
-   public void generateFile() {
-      File parentDirectory = new File("c:/datas");
+   public void generateFile() throws IOException {
+
+      File parentDirectory = new File(
+            "S:/produits/Qualite/Projet_ae/Documentation refonte/Refonte/Régionalisation/Vague 3 (fin 2013)/Fichiers de fusion à blanc (FAB)/FAB1");
 
       regroupFiles(parentDirectory);
-      // sortFiles(tempDirectory);
-      // createDatasFile(parentDirectory, tempDirectory);
-
-   }
-
-   /**
-    * @param parentDirectory
-    * @param tempDirectory
-    */
-   @Test
-   @Ignore
-   public void createDatasFile() {
-
-      ClassPathResource resource = new ClassPathResource(
-            "datas/correspondances.properties");
-      Properties properties = new Properties();
-
-      try {
-         properties.load(resource.getInputStream());
-      } catch (IOException e1) {
-         throw new RuntimeException(
-               "impossible de charger le fichier de resource");
-      }
-
-      File parentDirectory = new File("c:/datas");
-      File tempDirectory = new File(parentDirectory, "temp");
-      Reader reader = null;
-      CSVReader csvReader = null;
-      Writer fileWriter = null;
-      File destFile;
-
-      for (File file : tempDirectory.listFiles()) {
-         try {
-            destFile = new File(parentDirectory, file.getName());
-            fileWriter = new FileWriter(destFile);
-            reader = new FileReader(file);
-            csvReader = new CSVReader(reader, '$');
-            String[] tabLine;
-            String lucene = null;
-
-            while ((tabLine = csvReader.readNext()) != null) {
-               if (COTI_CLE.equals(tabLine[0].trim())) {
-                  lucene = "nce:" + tabLine[2].trim();
-                  fileWriter.write(lucene);
-                  fileWriter.write(";nce;");
-                  fileWriter.write(tabLine[2].trim());
-                  fileWriter.write(">");
-                  fileWriter.write(tabLine[3].trim());
-                  fileWriter.write(";cog;UR");
-
-                  fileWriter
-                        .write(properties.containsKey(tabLine[1].trim()) ? properties
-                              .getProperty(tabLine[1].trim())
-                              : tabLine[1].trim());
-                  fileWriter.write(">UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[4].trim()) ? properties
-                              .getProperty(tabLine[4].trim())
-                              : tabLine[4].trim());
-                  fileWriter.write(";cop;UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[1].trim()) ? properties
-                              .getProperty(tabLine[1].trim())
-                              : tabLine[1].trim());
-                  fileWriter.write(">UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[4].trim()) ? properties
-                              .getProperty(tabLine[4].trim())
-                              : tabLine[4].trim());
-                  fileWriter.write("\n");
-
-               } else if (CPTE_CLE.equals(tabLine[0])) {
-                  lucene = "nci:" + tabLine[2].trim();
-                  fileWriter.write(lucene);
-                  fileWriter.write(";nci;");
-                  fileWriter.write(tabLine[2].trim());
-                  fileWriter.write(">");
-                  fileWriter.write(tabLine[3].trim());
-                  fileWriter.write(";cog;UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[1].trim()) ? properties
-                              .getProperty(tabLine[1].trim())
-                              : tabLine[1].trim());
-                  fileWriter.write(">UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[4].trim()) ? properties
-                              .getProperty(tabLine[4].trim())
-                              : tabLine[4].trim());
-                  fileWriter.write(";cop;UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[1].trim()) ? properties
-                              .getProperty(tabLine[1].trim())
-                              : tabLine[1].trim());
-                  fileWriter.write(">UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[4].trim()) ? properties
-                              .getProperty(tabLine[4].trim())
-                              : tabLine[4].trim());
-                  fileWriter.write("\n");
-
-               } else if (PERS_CLE.equals(tabLine[0].trim())) {
-                  lucene = "npe:" + tabLine[2].trim();
-                  fileWriter.write(lucene);
-                  fileWriter.write(";npe;");
-                  fileWriter.write(tabLine[2].trim());
-                  fileWriter.write(">");
-                  fileWriter.write(tabLine[3].trim());
-                  fileWriter.write(";cog;UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[1].trim()) ? properties
-                              .getProperty(tabLine[1].trim())
-                              : tabLine[1].trim());
-                  fileWriter.write(">UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[4].trim()) ? properties
-                              .getProperty(tabLine[4].trim())
-                              : tabLine[4].trim());
-                  fileWriter.write(";cop;UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[1].trim()) ? properties
-                              .getProperty(tabLine[1].trim())
-                              : tabLine[1].trim());
-                  fileWriter.write(">UR");
-                  fileWriter
-                        .write(properties.containsKey(tabLine[4].trim()) ? properties
-                              .getProperty(tabLine[4].trim())
-                              : tabLine[4].trim());
-                  fileWriter.write("\n");
-
-               }
-            }
-
-         } catch (IOException e) {
-            e.printStackTrace();
-
-         } finally {
-
-            if (csvReader != null) {
-               try {
-                  csvReader.close();
-               } catch (IOException e) {
-                  e.printStackTrace();
-               }
-            }
-
-            if (reader != null) {
-
-               try {
-                  reader.close();
-               } catch (Exception e) {
-                  e.printStackTrace();
-               }
-            }
-
-            if (fileWriter != null) {
-               try {
-                  fileWriter.close();
-               } catch (IOException ex) {
-                  ex.printStackTrace();
-               }
-            }
-         }
-      }
 
    }
 
@@ -281,110 +107,6 @@ public class ConversionFileTest {
       }
 
       return tempDirectory;
-   }
-
-   @Test
-   @Ignore
-   public void supprimeZeros() {
-      File directory = new File("c:/datas");
-      File file = new File(directory, "regionalisation_pers.csv");
-
-      Reader reader = null;
-      BufferedReader bReader = null;
-      Writer writer = null;
-
-      String line, requete, value;
-      Long iValue;
-
-      try {
-         reader = new FileReader(file);
-         bReader = new BufferedReader(reader);
-         writer = new FileWriter(new File(directory, "zeros_less_"
-               + file.getName()));
-         String[] tabLine, tabRequete, tabLink;
-
-         while (StringUtils.isNotBlank((line = bReader.readLine()))) {
-            tabLine = line.split(";");
-            requete = tabLine[0];
-            tabRequete = requete.split(":");
-            value = tabRequete[1];
-
-            if (value.startsWith("0")) {
-               iValue = Long.valueOf(value);
-               value = String.valueOf(iValue);
-               writer.write(tabRequete[0] + ":" + value + ";");
-            } else {
-               writer.write(tabLine[0] + ";");
-            }
-
-            writer.write(tabLine[1] + ";");
-
-            String corresp = tabLine[2];
-            tabLink = corresp.split(">");
-            if (tabLink[0].startsWith("0")) {
-               value = tabLink[0];
-               iValue = Long.valueOf(value);
-               value = String.valueOf(iValue);
-               writer.write(value);
-
-            } else {
-               writer.write(tabLink[0]);
-            }
-            
-            writer.write(">");
-
-            if (tabLink[1].startsWith("0")) {
-               value = tabLink[1];
-               iValue = Long.valueOf(value);
-               value = String.valueOf(iValue);
-
-               writer.write(value);
-            } else {
-               writer.write(tabLink[1]);
-            }
-
-            writer.write(";");
-
-            writer.write(tabLine[3] + ";");
-            writer.write(tabLine[4] + ";");
-            writer.write(tabLine[5] + ";");
-            writer.write(tabLine[6] + "\n");
-
-         }
-
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-
-      } catch (IOException e) {
-         e.printStackTrace();
-      } finally {
-
-         if (bReader != null) {
-
-            try {
-               bReader.close();
-            } catch (Exception e) {
-               e.printStackTrace();
-            }
-         }
-
-         if (reader != null) {
-
-            try {
-               reader.close();
-            } catch (Exception e) {
-               e.printStackTrace();
-            }
-         }
-
-         if (writer != null) {
-            try {
-               writer.close();
-            } catch (IOException ex) {
-               ex.printStackTrace();
-            }
-         }
-      }
 
    }
 
