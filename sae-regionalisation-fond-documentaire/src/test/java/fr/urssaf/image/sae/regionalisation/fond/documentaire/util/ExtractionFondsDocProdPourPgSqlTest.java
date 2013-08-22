26062fe9358c5c6b6ea2fd2e69e0ee87f4295f86
paul.mareche <paul.mareche>
2013-08-22 12:05:47 +0000

    Régio vague 3: Tests unitaires permettant de sortir les données du SAE pour les traitements Postgresql
    svn path=/java/trunk/; revision=3573

---
+++ sae-regionalisation-fond-documentaire/src/test/java/fr/urssaf/image/sae/regionalisation/fond/documentaire/util/ExtractionFondsDocProdPourPgSqlTest.java
@@ -0,0 +1,218 @@
+package fr.urssaf.image.sae.regionalisation.fond.documentaire.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.netflix.astyanax.query.AllRowsQuery;
+
+import fr.urssaf.image.sae.regionalisation.fond.documentaire.dao.DocInfoDao;
+import fr.urssaf.image.sae.regionalisation.fond.documentaire.dao.cf.DocInfoKey;
+import fr.urssaf.image.sae.regionalisation.fond.documentaire.iterator.CassandraIterator;
+import fr.urssaf.image.sae.regionalisation.fond.documentaire.support.CassandraSupport;
+
+/**
+ * Cette classe n'est pas une "vraie" classe de TU.
+ * 
+ * Elle contient des méthodes pour extraire le fonds 
+ * documentaire du SAE de PRODUCTION, afin de réaliser
+ * les traitements de régionalisation
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/applicationContext-sae-regionalisation-cassandra-test.xml" })
+@Ignore
+public class ExtractionFondsDocProdPourPgSqlTest {
+
+   @Autowired
+   private CassandraSupport cassandraSupport;
+   
+   @Autowired
+   private DocInfoDao infoDao;
+   
+   @Autowired
+   private Properties cassandraConf;
+   
+   
+   // TODO: ATTENTION: VALEURS A CHANGER
+   private static String CHEMINREP = "c:/divers";
+   private int MIN_MOIS = 201001;
+   private int MAX_MOIS = 201307;
+   
+   
+
+   @Test
+   public void extraitFondsDoc() throws IOException {
+
+      // Liste des métadonnées que l'on va lire
+      List<String> reqMetas = new ArrayList<String>();
+      reqMetas.add("SM_BASE_ID");
+      reqMetas.add("SM_UUID");
+      reqMetas.add("cog");
+      reqMetas.add("cop");
+      reqMetas.add("nce");
+      reqMetas.add("nci");
+      reqMetas.add("npe");
+      reqMetas.add("SM_ARCHIVAGE_DATE");
+      
+      // Création du répertoire de sortie s'il n'existe pas déjà
+      File rep = new File(CHEMINREP);
+      if (!rep.exists()) {
+         rep.mkdir();
+      }
+      
+      // Récupère le nom de la base DFCE sur laquelle travailler
+      String nomBaseDfceAttendue = cassandraConf.getProperty("db.baseName");
+      
+
+      Map<String, Writer> writers = new HashMap<String, Writer>();
+      try {
+
+         cassandraSupport.connect();
+
+         AllRowsQuery<DocInfoKey, String> query = infoDao.getQuery(reqMetas
+               .toArray(new String[0]));
+         CassandraIterator<DocInfoKey> iterator = new CassandraIterator<DocInfoKey>(
+               query);
+         
+         Map<String, String> map;
+         
+         int nbDocsTraites = 0;
+         int nbDocsSortis = 0;
+         
+         String idDoc;
+         String cog;
+         String nomBaseDfce;
+         String dateArchivage;
+         int anneeMoisArchivage;
+         String nce;
+         String nci;
+         String npe;
+         
+         Writer writer;
+         while (iterator.hasNext()) {
+            map = iterator.next();
+            
+            idDoc = map.get("SM_UUID");
+            cog = map.get("cog");
+            nomBaseDfce = map.get("SM_BASE_ID");
+            dateArchivage = StringUtils.trimToEmpty(map.get("SM_ARCHIVAGE_DATE"));
+            
+            if ( 
+                  StringUtils.equals(nomBaseDfce, nomBaseDfceAttendue) && 
+                  StringUtils.isNotBlank(idDoc) && 
+                  StringUtils.isNotBlank(cog) && 
+                  StringUtils.isNotBlank(dateArchivage))  { 
+               
+               anneeMoisArchivage = Integer.parseInt(StringUtils.left(dateArchivage, 6));
+               
+               if (
+                     (anneeMoisArchivage>=MIN_MOIS) &&
+                     (anneeMoisArchivage<=MAX_MOIS)) {
+
+                  nce = StringUtils.trimToEmpty(map.get("nce"));
+                  nci = StringUtils.trimToEmpty(map.get("nci"));
+                  npe = StringUtils.trimToEmpty(map.get("npe"));
+                  
+                  if (
+                        StringUtils.isNotBlank(nce) || 
+                        StringUtils.isNotBlank(nci) || 
+                        StringUtils.isNotBlank(npe)) {
+                  
+                     writer = getWriter(dateArchivage, writers); 
+                     
+                     writer.write(idDoc);
+                     writer.write(";");
+                     writer.write(cog);
+                     writer.write(";");
+                     writer.write(map.get("cop"));
+                     writer.write(";");
+                     writer.write(nce);
+                     writer.write(";");
+                     writer.write(nci);
+                     writer.write(";");
+                     writer.write(npe);
+                     
+                     writer.write("\n");
+                     
+                     nbDocsSortis++;
+                  
+                  }
+               
+               }
+               
+            }
+            
+            nbDocsTraites++;
+            if ((nbDocsTraites%1000)==0) {
+               System.out.println("Nombre de docs traités : " + nbDocsTraites);
+            }
+            
+         }
+         
+         System.out.println("Nombre total de docs traités : " + (nbDocsTraites-1));
+         System.out.println("Nombre total de docs sortis dans le fichier : " + nbDocsSortis);
+
+      } catch (IOException exception) {
+         System.err.println(exception);
+
+      } finally {
+         closeWriters(writers);
+         cassandraSupport.disconnect();
+      }
+
+   }
+   
+   
+   private void closeWriters(Map<String, Writer> writers) {
+      for (Map.Entry<String, Writer> entry : writers.entrySet()) {
+         closeWriter(entry.getValue());
+      }
+   }
+   
+   private void closeWriter(Writer writer) {
+      try {
+         if (writer != null) {
+            writer.close();
+         }
+      } catch (IOException exception) {
+         System.err.println("impossible de fermer le flux");
+      }
+   }
+   
+   
+   private Writer getWriter(String date, Map<String, Writer> writers) {
+      
+      String anneeMois = StringUtils.left(date, 6);
+      
+      Writer writer = writers.get(anneeMois);
+      if (writer==null) {
+         File fichier = new File(CHEMINREP, String.format("fonds_doc_%s.csv", anneeMois));
+         try {
+            writer = new FileWriter(fichier);
+         } catch (IOException e) {
+            throw new RuntimeException(e);
+         }
+         writers.put(anneeMois, writer);
+         
+      }
+      
+      return writer;
+      
+   }
+   
+   
+}
