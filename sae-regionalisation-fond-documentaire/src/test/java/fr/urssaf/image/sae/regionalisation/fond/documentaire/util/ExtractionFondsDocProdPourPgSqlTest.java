7d57428760bbd36cfa2e2b8d7772584f1507def7
paul.mareche <paul.mareche>
2013-10-16 14:32:56 +0000

    - rassemblement des 2 TU en 1 seul pour éviter le copier/coller - correction de l'algo: certains documents étaient filtrés à tort (ceux dont seuls le code organisme doit être modifiés) - ajout d'un filtre sur les journaux SAE
    svn path=/java/trunk/; revision=3642

--- sae-regionalisation-fond-documentaire/src/test/java/fr/urssaf/image/sae/regionalisation/fond/documentaire/util/ExtractionFondsDocProdPourPgSqlTest.java
+++ sae-regionalisation-fond-documentaire/src/test/java/fr/urssaf/image/sae/regionalisation/fond/documentaire/util/ExtractionFondsDocProdPourPgSqlTest.java
@@ -50,223 +50,18 @@ public class ExtractionFondsDocProdPourPgSqlTest {
    @Autowired
    private Properties cassandraConf;
    
-   /**
-    * Chemin du répertoire de sortie des fichiers CVS
-    * TODO: Adapter la valeur à son poste
-    */
-   private static String CHEMINREP = "c:/divers";
-   
-   /**
-    * Premier mois d'archivage à sortir en CSV (format AAAAMM)
-    * TODO: Adapter la valeur selon les données que l'on veut sortir
-    */
-   private int MIN_MOIS = 201201;
-   
-   /**
-    * Dernier mois d'archivage à sortir en CSV (format AAAAMM)
-    * TODO: Adapter la valeur selon les données que l'on veut sortir
-    */
-   private int MAX_MOIS = 201309;
-
-   /**
-    * Date de début pour la sortie en CSV (format AAAAMMJJ)
-    * TODO: Adapter la valeur selon les données que l'on veut sortir
-    */
-   private int MIN_DATE = 20120101;
-   
-   /**
-    * Date de fin de la sortir en CSV (format AAAAMMJJ)
-    * TODO: Adapter la valeur selon les données que l'on veut sortir
-    */
-   private int MAX_DATE = 20130731;
-   
-   private static PeriodFormatter PERIOD_FORMATTER = new PeriodFormatterBuilder()
-      .appendHours()
-      .appendSuffix("h", "h")
-      .appendSeparator(" ")
-      .appendMinutes()
-      .appendSuffix("mn", "mn")
-      .appendSeparator(" ")
-      .appendSeconds()
-      .appendSuffix("s", "s")
-      .appendSeparator(" ")
-      .appendMillis()
-      .appendSuffix("ms", "ms")
-      .toFormatter();
-  
-   @Test
-   public void extraitFondsDocUnFichierParMois() throws IOException {
-
-      // Timestamp courant pour le calcul du temps d'exécution
-      DateTime dateDebut = new DateTime();
-      System.out.println("Début du traitement: " + dateDebut.toString("dd/MM/yyyy hh'h'mm ss's' SSS'ms'"));
-      
-      // Liste des métadonnées que l'on va lire
-      List<String> reqMetas = new ArrayList<String>();
-      reqMetas.add("SM_BASE_ID");
-      reqMetas.add("SM_UUID");
-      reqMetas.add("cog");
-      reqMetas.add("cop");
-      reqMetas.add("nce");
-      reqMetas.add("nci");
-      reqMetas.add("npe");
-      reqMetas.add("SM_ARCHIVAGE_DATE");
-      
-      // Création du répertoire de sortie s'il n'existe pas déjà
-      File rep = new File(CHEMINREP);
-      if (!rep.exists()) {
-         rep.mkdir();
-      }
-      
-      // Récupère le nom de la base DFCE sur laquelle travailler
-      String nomBaseDfceAttendue = cassandraConf.getProperty("db.baseName");
-      
-
-      Map<String, Writer> writers = new HashMap<String, Writer>();
-      try {
-
-         cassandraSupport.connect();
-
-         AllRowsQuery<DocInfoKey, String> query = infoDao.getQuery(reqMetas
-               .toArray(new String[0]));
-         CassandraIterator<DocInfoKey> iterator = new CassandraIterator<DocInfoKey>(
-               query);
-         
-         Map<String, String> map;
-         
-         int nbDocsTraites = 0;
-         int nbDocsSortis = 0;
-         
-         String idDoc;
-         String cog;
-         String nomBaseDfce;
-         String dateArchivage;
-         int anneeMoisArchivage;
-         String nce;
-         String nci;
-         String npe;
-         
-         Writer writer;
-         while (iterator.hasNext()) {
-            map = iterator.next();
-            
-            idDoc = map.get("SM_UUID");
-            cog = map.get("cog");
-            nomBaseDfce = map.get("SM_BASE_ID");
-            dateArchivage = StringUtils.trimToEmpty(map.get("SM_ARCHIVAGE_DATE"));
-            
-            if ( 
-                  StringUtils.equals(nomBaseDfce, nomBaseDfceAttendue) && 
-                  StringUtils.isNotBlank(idDoc) && 
-                  StringUtils.isNotBlank(cog) && 
-                  StringUtils.isNotBlank(dateArchivage))  { 
-               
-               anneeMoisArchivage = Integer.parseInt(StringUtils.left(dateArchivage, 6));
-               
-               if (
-                     (anneeMoisArchivage>=MIN_MOIS) &&
-                     (anneeMoisArchivage<=MAX_MOIS)) {
-
-                  nce = StringUtils.trimToEmpty(map.get("nce"));
-                  nci = StringUtils.trimToEmpty(map.get("nci"));
-                  npe = StringUtils.trimToEmpty(map.get("npe"));
-                  
-                  if (
-                        StringUtils.isNotBlank(nce) || 
-                        StringUtils.isNotBlank(nci) || 
-                        StringUtils.isNotBlank(npe)) {
-                  
-                     writer = getWriter(dateArchivage, writers); 
-                     
-                     writer.write(idDoc);
-                     writer.write(";");
-                     writer.write(cog);
-                     writer.write(";;0;");
-                     writer.write(map.get("cop"));
-                     writer.write(";;0;");
-                     writer.write(nce);
-                     writer.write(";");
-                     writer.write(nci);
-                     writer.write(";");
-                     writer.write(npe);
-                     
-                     writer.write("\n");
-                     
-                     nbDocsSortis++;
-                  
-                  }
-               
-               }
-               
-            }
-            
-            nbDocsTraites++;
-            if ((nbDocsTraites%1000)==0) {
-               System.out.println("Nombre de docs traités : " + nbDocsTraites);
-            }
-            
-         }
-         
-         System.out.println("Nombre total de docs traités : " + (nbDocsTraites-1));
-         System.out.println("Nombre total de docs sortis dans le fichier : " + nbDocsSortis);
-
-      } catch (IOException exception) {
-         System.err.println(exception);
-
-      } finally {
-         closeWriters(writers);
-         cassandraSupport.disconnect();
-      }
-      
-      // Timestamp courant pour le calcul du temps d'exécution
-      DateTime dateFin = new DateTime();
-      System.out.println("Fin du traitement: " + dateFin.toString("dd/MM/yyyy hh'h'mm ss's' SSS'ms'"));
-      Period tempsExec = new Period(dateDebut, dateFin);
-      System.out.println("Temps d'exécution: " + tempsExec.toString(PERIOD_FORMATTER));
-
-   }
-   
-   
-   private void closeWriters(Map<String, Writer> writers) {
-      for (Map.Entry<String, Writer> entry : writers.entrySet()) {
-         closeWriter(entry.getValue());
-      }
-   }
-   
-   private void closeWriter(Writer writer) {
-      try {
-         if (writer != null) {
-            writer.close();
-         }
-      } catch (IOException exception) {
-         System.err.println("impossible de fermer le flux");
-      }
-   }
-   
-   
-   private Writer getWriter(String date, Map<String, Writer> writers) {
-      
-      String anneeMois = StringUtils.left(date, 6);
-      
-      Writer writer = writers.get(anneeMois);
-      if (writer==null) {
-         File fichier = new File(CHEMINREP, String.format("fonds_doc_%s.csv", anneeMois));
-         try {
-            writer = new FileWriter(fichier);
-         } catch (IOException e) {
-            throw new RuntimeException(e);
-         }
-         writers.put(anneeMois, writer);
-         
-      }
-      
-      return writer;
-      
-   }
    
    @Test
    public void extraitFondsDoc() throws IOException {
 
+      // Paramétrage du TU
+      // TODO: à adapter selon le besoin
+      String repertoireSortie = "c:/divers";
+      int dateArchivageDebut = 20120101;
+      int dateArchivageFin = 20130931;
+      boolean unFichierParMois = true;
+      
+      
       // Timestamp courant pour le calcul du temps d'exécution
       DateTime dateDebut = new DateTime();
       System.out.println("Début du traitement: " + dateDebut.toString("dd/MM/yyyy hh'h'mm ss's' SSS'ms'"));
@@ -281,9 +76,10 @@ public class ExtractionFondsDocProdPourPgSqlTest {
       reqMetas.add("nci");
       reqMetas.add("npe");
       reqMetas.add("SM_ARCHIVAGE_DATE");
+      reqMetas.add("SM_DOCUMENT_TYPE");
       
       // Création du répertoire de sortie s'il n'existe pas déjà
-      File rep = new File(CHEMINREP);
+      File rep = new File(repertoireSortie);
       if (!rep.exists()) {
          rep.mkdir();
       }
@@ -306,6 +102,7 @@ public class ExtractionFondsDocProdPourPgSqlTest {
          
          int nbDocsTraites = 0;
          int nbDocsSortis = 0;
+         int nbJournauxSae = 0;
          
          String idDoc;
          String cog;
@@ -315,10 +112,14 @@ public class ExtractionFondsDocProdPourPgSqlTest {
          String nce;
          String nci;
          String npe;
+         String codeRnd;
+         Writer writer;
          
-         File fichier = new File(CHEMINREP, "fonds_doc.csv");
-         Writer writer = new FileWriter(fichier);
-         writers.put("all", writer);
+         // Si un gros fichier, on créé le writer global
+         if (!unFichierParMois) {
+            File fichier = new File(repertoireSortie, "fonds_doc.csv");
+            writers.put("all", new FileWriter(fichier));
+         }
          
          while (iterator.hasNext()) {
             map = iterator.next();
@@ -337,18 +138,21 @@ public class ExtractionFondsDocProdPourPgSqlTest {
                anneeMoisJourArchivage = Integer.parseInt(StringUtils.left(dateArchivage, 8));
                
                if (
-                     (anneeMoisJourArchivage>=MIN_DATE) &&
-                     (anneeMoisJourArchivage<=MAX_DATE)) {
+                     (anneeMoisJourArchivage>=dateArchivageDebut) &&
+                     (anneeMoisJourArchivage<=dateArchivageFin)) {
 
                   nce = StringUtils.trimToEmpty(map.get("nce"));
                   nci = StringUtils.trimToEmpty(map.get("nci"));
                   npe = StringUtils.trimToEmpty(map.get("npe"));
+                  codeRnd = StringUtils.trimToEmpty(map.get("SM_DOCUMENT_TYPE"));
                   
                   if (
-                        StringUtils.isNotBlank(nce) || 
-                        StringUtils.isNotBlank(nci) || 
-                        StringUtils.isNotBlank(npe)) {
+                        // Exclusion des journaux SAE.
+                        !StringUtils.equals(codeRnd,"7.7.8.8.1")) 
+                  {
                   
+                     writer = getWriter(unFichierParMois, repertoireSortie, dateArchivage, writers);
+                     
                      writer.write(idDoc);
                      writer.write(";");
                      writer.write(cog);
@@ -365,6 +169,8 @@ public class ExtractionFondsDocProdPourPgSqlTest {
                      
                      nbDocsSortis++;
                   
+                  } else {
+                     nbJournauxSae++;
                   }
                
                }
@@ -379,7 +185,9 @@ public class ExtractionFondsDocProdPourPgSqlTest {
          }
          
          System.out.println("Nombre total de docs traités : " + (nbDocsTraites-1));
+         System.out.println("Nombre de journaux SAE : " + nbJournauxSae);
          System.out.println("Nombre total de docs sortis dans le fichier : " + nbDocsSortis);
+         
 
       } catch (IOException exception) {
          System.err.println(exception);
@@ -393,7 +201,69 @@ public class ExtractionFondsDocProdPourPgSqlTest {
       DateTime dateFin = new DateTime();
       System.out.println("Fin du traitement: " + dateFin.toString("dd/MM/yyyy hh'h'mm ss's' SSS'ms'"));
       Period tempsExec = new Period(dateDebut, dateFin);
-      System.out.println("Temps d'exécution: " + tempsExec.toString(PERIOD_FORMATTER));
+      PeriodFormatter periodFormatter = new PeriodFormatterBuilder()
+         .appendHours()
+         .appendSuffix("h", "h")
+         .appendSeparator(" ")
+         .appendMinutes()
+         .appendSuffix("mn", "mn")
+         .appendSeparator(" ")
+         .appendSeconds()
+         .appendSuffix("s", "s")
+         .appendSeparator(" ")
+         .appendMillis()
+         .appendSuffix("ms", "ms")
+         .toFormatter();
+      System.out.println("Temps d'exécution: " + tempsExec.toString(periodFormatter));
 
-   }   
+   }
+   
+   private void closeWriters(Map<String, Writer> writers) {
+      for (Map.Entry<String, Writer> entry : writers.entrySet()) {
+         closeWriter(entry.getValue());
+      }
+   }
+   
+   private void closeWriter(Writer writer) {
+      try {
+         if (writer != null) {
+            writer.close();
+         }
+      } catch (IOException exception) {
+         System.err.println("impossible de fermer le flux");
+      }
+   }
+   
+   
+   private Writer getWriter(boolean unFichierParMois, String repertoireSortie, String date, Map<String, Writer> writers) {
+      
+      if (unFichierParMois) {
+         return getWriter(repertoireSortie, date, writers);
+      } else {
+         return writers.get("all");
+      }
+      
+   }
+   
+   
+   private Writer getWriter(String repertoireSortie, String date, Map<String, Writer> writers) {
+      
+      String anneeMois = StringUtils.left(date, 6);
+      
+      Writer writer = writers.get(anneeMois);
+      if (writer==null) {
+         File fichier = new File(repertoireSortie, String.format("fonds_doc_%s.csv", anneeMois));
+         try {
+            writer = new FileWriter(fichier);
+         } catch (IOException e) {
+            throw new RuntimeException(e);
+         }
+         writers.put(anneeMois, writer);
+         
+      }
+      
+      return writer;
+      
+   }
+   
 }
