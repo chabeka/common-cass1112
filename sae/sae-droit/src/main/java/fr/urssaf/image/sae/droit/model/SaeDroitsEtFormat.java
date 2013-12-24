41490cc52109b44369f1a593985dfccc0fc73fd6
lokmen.baadj <lokmen.baadj>
2013-12-24 15:33:05 +0000

    SAE - Gestion des Formats - Lot 00015
    svn path=/java/trunk/; revision=3766

---
+++ sae/sae-droit/src/main/java/fr/urssaf/image/sae/droit/model/SaeDroitsEtFormat.java
@@ -0,0 +1,38 @@
+package fr.urssaf.image.sae.droit.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.urssaf.image.sae.droit.dao.model.FormatControlProfil;
+
+public class SaeDroitsEtFormat {
+
+   private SaeDroits saeDroits;
+   private List<FormatControlProfil> listFormatControlProfil = new ArrayList<FormatControlProfil>();
+   /**
+    * @return the saeDroits
+    */
+   public SaeDroits getSaeDroits() {
+      return saeDroits;
+   }
+   /**
+    * @param saeDroits the saeDroits to set
+    */
+   public void setSaeDroits(SaeDroits saeDroits) {
+      this.saeDroits = saeDroits;
+   }
+   /**
+    * @return the listFormatControlProfil
+    */
+   public List<FormatControlProfil> getListFormatControlProfil() {
+      return listFormatControlProfil;
+   }
+   /**
+    * @param listFormatControlProfil the listFormatControlProfil to set
+    */
+   public void setListFormatControlProfil(
+         List<FormatControlProfil> listFormatControlProfil) {
+      this.listFormatControlProfil = listFormatControlProfil;
+   }
+   
+}
