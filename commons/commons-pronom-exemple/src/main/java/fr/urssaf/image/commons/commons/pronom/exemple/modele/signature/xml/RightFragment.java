10d632974f3d1501402320923b19e471f30eb1ae
aurelie.garnier <aurelie.garnier>
2014-12-02 09:14:07 +0000

    [maven-release-plugin]  copy for tag sae-anais-portail-1.0.5
    svn path=/java/tags/sae-anais-portail-1.0.5/; revision=4162

--- commons/commons-pronom-exemple/src/main/java/fr/urssaf/image/commons/commons/pronom/exemple/modele/signature/xml/RightFragment.java
+++
@@ -1,7 +0,0 @@
-
-package fr.urssaf.image.commons.commons.pronom.exemple.modele.signature.xml;
-
-
-public class RightFragment extends SideFragment {
-
-}
--- pdfa-validator/src/main/java/fr/urssaf/image/pdfa/exception/NotAFolderException.java
+++
@@ -1,9 +0,0 @@
-package fr.urssaf.image.pdfa.exception;
-
-import java.text.MessageFormat;
-
-public class NotAFolderException extends Exception {
-   public NotAFolderException(String cheminPDF) {
-      super(MessageFormat.format("Le chemin vers le répertoire {0} est incorrect.",cheminPDF));
-   }
-}
