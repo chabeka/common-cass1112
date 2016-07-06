406612f73183f55c3370a26bfd2e76f7acfb925c
aurelie.garnier <aurelie.garnier>
2016-07-06 09:51:57 +0000

    Coorectif bug répertoire dans analyse des logs de la purge de la corbeille
    svn path=/java/trunk/; revision=4812

--- sae/sae-livrable/src/scripts/check_dfce_jobs.sh
+++ sae/sae-livrable/src/scripts/check_dfce_jobs.sh
@@ -120,7 +120,7 @@ fi
 # ============ Analyses des logs sae-documents-executable.jar ============ 
 #
 
-if ! grep -q "fin du traitement de la purge de la corbeille" "$LOG_DIR/sae_documents_executable-PURGE_CORBEILLE.log"; then
+if ! grep -q "fin du traitement de la purge de la corbeille" "$LOG_DIR/sae-documents-executable/sae_documents_executable-PURGE_CORBEILLE.log"; then
     error "Erreur dans la purge de la corbeille"
     ERRNO+=1
 fi
