257c06075d035e79d669448c6bda41149f6823f0
'AURELIE GARNIER' <aurelie.garnier@urssaf.fr>
2017-03-13 13:43:28 +0000

    [maven-release-plugin]  copy for tag sae-batch-docs-executable-17.6.0.1
    svn path=/java/tags/sae-batch-docs-executable-17.6.0.1/; revision=5086

--- sae-integration/sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/modele/somres/sommaire/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.5-2 
-// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2016.11.17 à 03:49:54 PM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.cirtil.fr/sae/sommaireXml", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fr.urssaf.image.sae.integration.ihmweb.modele.somres.sommaire;
