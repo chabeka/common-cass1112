14c5ee63abe3b3fdc62c2b98d9bf51c8b0b023b8
'NICOLAS VANGOUT' <nicolas.vangout@urssaf.fr>
2017-04-28 07:17:07 +0000

    Correction bugs webservice modification_masse.
    svn path=/java/branches/sae-17.9.0.1/; revision=5235

--- sae-services-batch/src/main/java/fr/urssaf/image/sae/services/batch/modification/support/sommaire/batch/ConvertMetaDocumentModificationProcessor.java
+++ sae-services-batch/src/main/java/fr/urssaf/image/sae/services/batch/modification/support/sommaire/batch/ConvertMetaDocumentModificationProcessor.java
@@ -6,11 +6,15 @@ package fr.urssaf.image.sae.services.batch.modification.support.sommaire.batch;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.xml.bind.JAXBElement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +22,7 @@ import fr.urssaf.image.sae.bo.model.untyped.UntypedDocument;
 import fr.urssaf.image.sae.bo.model.untyped.UntypedMetadata;
 import fr.urssaf.image.sae.services.batch.capturemasse.modele.commun_sommaire_et_resultat.DocumentType;
 import fr.urssaf.image.sae.services.batch.capturemasse.modele.commun_sommaire_et_resultat.MetadonneeType;
+import fr.urssaf.image.sae.services.batch.common.Constantes;
 
 /**
  * Item processor pour convertir un document sommaire d'un modèle objet XML vers
@@ -33,6 +38,23 @@ public class ConvertMetaDocumentModificationProcessor implements
 
    private static final String PREFIXE_TRC = "ConvertMetaDocumentModificationProcessor.process()";
 
+   private StepExecution stepExecution;
+   private String batchMode;
+
+   /**
+    * initialisation avant le début du Step
+    * 
+    * @param stepExecution
+    *           le stepExecution
+    */
+   @BeforeStep
+   public final void init(StepExecution stepExecution) {
+      this.stepExecution = stepExecution;
+      this.batchMode = (String) stepExecution.getJobExecution()
+            .getExecutionContext().get(Constantes.BATCH_MODE_NOM_REDIRECT);
+
+   }
+
    /**
     * {@inheritDoc}
     */
@@ -46,25 +68,94 @@ public class ConvertMetaDocumentModificationProcessor implements
 
       final UntypedDocument untypedDoc = new UntypedDocument();
       
-      final List<MetadonneeType> metaDataType = item.getValue()
-            .getMetadonnees().getMetadonnee();
-      final List<UntypedMetadata> listUM = new ArrayList<UntypedMetadata>();
+      try {
 
-      UntypedMetadata untypedMetadata;
-      for (MetadonneeType metadonneeType : metaDataType) {
-         untypedMetadata = new UntypedMetadata();
-         untypedMetadata.setLongCode(metadonneeType.getCode());
-         untypedMetadata.setValue(metadonneeType.getValeur());
-         listUM.add(untypedMetadata);
+         final List<MetadonneeType> metaDataType = item.getValue()
+               .getMetadonnees().getMetadonnee();
+         final List<UntypedMetadata> listUM = new ArrayList<UntypedMetadata>();
+
+         UntypedMetadata untypedMetadata;
+         for (MetadonneeType metadonneeType : metaDataType) {
+            untypedMetadata = new UntypedMetadata();
+            untypedMetadata.setLongCode(metadonneeType.getCode());
+            untypedMetadata.setValue(metadonneeType.getValeur());
+            listUM.add(untypedMetadata);
+         }
+         untypedDoc.setUMetadatas(listUM);
+
+         untypedDoc.setUuid(UUID.fromString(item.getValue().getObjetNumerique()
+               .getUUID()));
+
+      } catch (Exception e) {
+         if (isModePartielBatch()) {
+            getCodesErreurListe().add(Constantes.ERR_BUL002);
+            getIndexErreurListe().add(
+                  stepExecution.getExecutionContext().getInt(
+                        Constantes.CTRL_INDEX));
+            getExceptionErreurListe().add(new Exception(e.getMessage()));
+         } else {
+            throw e;
+         }
+
       }
-      untypedDoc.setUMetadatas(listUM);
-
-      untypedDoc.setUuid(UUID.fromString(item.getValue().getObjetNumerique().getUUID()));
 
       LOGGER
             .debug(
                   "{} - Fin du mapping de l'objet Jaxb représentant le sommaire.xml vers un objet métier Sommaire",
                   PREFIXE_TRC);
       return untypedDoc;
+   }
+
+   /**
+    * Methode permettant de savoir si l'on est en mode partiel.
+    * 
+    * @return True si mode partiel, false sinon
+    */
+   protected boolean isModePartielBatch() {
+      return batchMode != null
+            && Constantes.BATCH_MODE.PARTIEL.getModeNomCourt()
+                  .equals(batchMode);
+   }
+
+   /**
+    * Getter code erreur liste
+    * 
+    * @return la liste des codes erreurs stockée dans le contexte d'execution du
+    *         job
+    */
+   @SuppressWarnings("unchecked")
+   protected final ConcurrentLinkedQueue<String> getCodesErreurListe() {
+      ExecutionContext jobExecution = stepExecution.getJobExecution()
+            .getExecutionContext();
+      return (ConcurrentLinkedQueue<String>) jobExecution
+            .get(Constantes.CODE_EXCEPTION);
+   }
+
+   /**
+    * Getter code erreur liste
+    * 
+    * @return la liste des index des erreurs stockée dans le contexte
+    *         d'execution du job
+    */
+   @SuppressWarnings("unchecked")
+   protected final ConcurrentLinkedQueue<Integer> getIndexErreurListe() {
+      ExecutionContext jobExecution = stepExecution.getJobExecution()
+            .getExecutionContext();
+      return (ConcurrentLinkedQueue<Integer>) jobExecution
+            .get(Constantes.INDEX_EXCEPTION);
+   }
+
+   /**
+    * Getter code erreur liste
+    * 
+    * @return la liste des exceptions des erreurs stockée dans le contexte
+    *         d'execution du job
+    */
+   @SuppressWarnings("unchecked")
+   protected final ConcurrentLinkedQueue<Exception> getExceptionErreurListe() {
+      ExecutionContext jobExecution = stepExecution.getJobExecution()
+            .getExecutionContext();
+      return (ConcurrentLinkedQueue<Exception>) jobExecution
+            .get(Constantes.DOC_EXCEPTION);
    }
 }
