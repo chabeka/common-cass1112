2f08c7c4a6cb17591c79963bb62feb0a3a31b680
'NICOLAS VANGOUT' <nicolas.vangout@urssaf.fr>
2017-05-05 14:25:40 +0000

    Modification du test sur la modification de masse.
    svn path=/java/trunk/; revision=5244

--- saedemos/sae-client-demo/src/test/java/sae/client/demo/webservice/ModificationMasseTest.java
+++ saedemos/sae-client-demo/src/test/java/sae/client/demo/webservice/ModificationMasseTest.java
@@ -15,7 +15,7 @@ import sae.client.demo.webservice.modele.SaeServiceStub.ModificationMasseRespons
 
 public class ModificationMasseTest {
 
-   
+
    /**
     * Exemple de consommation de l'opération modificationMasse du service web
     * SaeService<br>
@@ -26,7 +26,7 @@ public class ModificationMasseTest {
     */
    @Test
    public void modificationMasse_success() throws RemoteException {
-      
+
       // Pré-requis pour la capture de masse :
       //  - Un répertoire de traitement a été créé dans l'ECDE dans la bonne arborescence
       //    par l'application cliente.
@@ -46,26 +46,26 @@ public class ModificationMasseTest {
       //  => ecde://cnp69intgnsecde.gidn.recouv/CS_DEV_TOUTES_ACTIONS/20120120/Traitement002_ArchivageMasse/sommaire.xml
       // Le hash SHA-1 du sommaire.xml est :
       //  => bbf4df5e743c1dace7f50034c4f3863d9a9f0d43
-      
+
       // URL ECDE du fichier sommaire.xml
       // String urlEcdeSommaire =
       // "ecde://cnp69intgnsecde.gidn.recouv/CS_DEV_TOUTES_ACTIONS/20120120/Traitement002_ArchivageMasse/sommaire.xml";
-      String urlEcdeSommaire = "ecde://cnp.nicolas/doc_5000_AUG/CS_DEV_TOUTES_ACTIONS/20170316/Traitement002_ModificationMasse_non_passant/sommaire.xml";
-      
+      String urlEcdeSommaire = "ecde://cnp69devecde.cer69.recouv/doc_5000_AUG/CS_DEV_TOUTES_ACTIONS/20170426/Traitement001_ModificationMasse_passant/sommaire.xml";
+
       // Hash SHA-1 du fichier sommaire.xml
       String typeHash = "SHA-1";
-      String hash = "c1e07795e8430026c8e0ae1033b9408c6f100f7b";
-      String codeTraitement = "847";
-      
+      String hash = "29ff24a0ec2474463f1c904ddf1e8a3c671198e9";
+      String codeTraitement = "UR827";
+
       // Construction du Stub
       SaeServiceStub saeService = StubFactory.createStubAvecAuthentification();
-      
+
       // Construction du paramètre d'entrée de l'opération archivageMasseAvecHash, 
       //  avec les objets modèle générés par Axis2.
       ModificationMasse paramsEntree = Axis2ObjectFactory
             .contruitParamsEntreeModificationMasse(urlEcdeSommaire, typeHash,
                   hash, codeTraitement);
-      
+
       // Appel de l'opération archivageMasseAvecHash
       // => en attendu, l'identifiant unique de traitement de masse affecté par le SAE
       ModificationMasseResponse reponse = saeService
@@ -77,10 +77,10 @@ public class ModificationMasseTest {
       System.out.println("URL ECDE du sommaire.xml : " + urlEcdeSommaire);
       System.out.println("Hash SHA-1 du sommaire.xml : " + hash);
       System.out.println("Identifiant unique du traitement de masse affecté par le SAE : " + idTraitementSae);
-      
+
    }
-   
-   
+
+
    /**
     * Exemple de consommation de l'opération archivageMasse du service web SaeService<br>
     * <br>
@@ -94,55 +94,55 @@ public class ModificationMasseTest {
     */
    @Test
    public void modificationMasse_failure() {
-      
+
       // URL ECDE du fichier sommaire.xml
       // String urlEcdeSommaire =
       // "ecde://cnp69intgnsecde.gidn.recouv/CS_DEV_TOUTES_ACTIONS/20120120/Traitement002_ArchivageMasse/sommaire.xml";
-      String urlEcdeSommaire = "ecde://cnp.nicolas/doc_5000_AUG/CS_DEV_TOUTES_ACTIONS/20170316/Traitement002_ModificationMasse_non_passant/sommaire.xml";
-      
+      String urlEcdeSommaire = "ecde://cnp69devecde.cer69.recouv/doc_5000_AUG/CS_DEV_TOUTES_ACTIONS/20170426/Traitement001_ModificationMasse_passant/sommaire.xml";
+
       // Hash SHA-1 du fichier sommaire.xml.
       // Le hash est faux
       String typeHash = "SHA-1";
       String hash = "HASHPASBON";
-      String codeTraitement = "847";
-      
+      String codeTraitement = "UR827";
+
       // Construction du Stub
       SaeServiceStub saeService = StubFactory.createStubAvecAuthentification();
-      
+
       // Construction du paramètre d'entrée de l'opération archivageMasseAvecHash, 
       //  avec les objets modèle générés par Axis2.
       ModificationMasse paramsEntree = Axis2ObjectFactory
             .contruitParamsEntreeModificationMasse(urlEcdeSommaire, typeHash,
                   hash, codeTraitement);
-      
+
       // Appel de l'opération modificationMasse
       try {
-      
+
          // Appel de l'opération modificationMasse
          saeService.modificationMasse(paramsEntree);
-         
+
          // Si on a passé l'appel, le test est en échec
          fail("La SoapFault attendue n'a pas été renvoyée");
-         
+
       } catch (AxisFault fault) {
-      
+
          // sysout
          TestUtils.sysoutAxisFault(fault);
-         
+
          // Vérification de la SoapFault
          TestUtils.assertSoapFault(
                fault,
                "urn:sae:faultcodes",
                "sae",
                "HashSommaireIncorrect",
-               "Le hash du fichier sommaire.xml attendu : HASHPASBON est différent de celui obtenu : c1e07795e8430026c8e0ae1033b9408c6f100f7b (type de hash : SHA-1)");
-         
+               "Le hash du fichier sommaire.xml attendu : HASHPASBON est différent de celui obtenu : 29ff24a0ec2474463f1c904ddf1e8a3c671198e9 (type de hash : SHA-1)");
+
       } catch (RemoteException exception) {
-         
+
          fail("Une RemoteException a été levée, alors qu'on attendait une AxisFault\r\n" + exception);
-         
+
       }
-      
+
    }
-   
+
 }
