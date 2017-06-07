9476adc48607e507a2dc1704a5498935cf5db8e3
'MATHIEU SNABRE' <mathieu.snabre@urssaf.fr>
2017-06-07 12:04:17 +0000

    REDMINE 7102 ajout uuid vide dans service de deblocage
    svn path=/java/trunk/; revision=5295

--- sae-integration/sae-integration-ihmweb-v2/IHM-Integration-V2/src/main/java/fr/urssaf/image/sae/integration/ihmweb/service/tests/DeblocageTestService.java
+++ sae-integration/sae-integration-ihmweb-v2/IHM-Integration-V2/src/main/java/fr/urssaf/image/sae/integration/ihmweb/service/tests/DeblocageTestService.java
@@ -23,89 +23,94 @@ import fr.urssaf.image.sae.integration.ihmweb.service.tests.listeners.impl.WsTes
 
 @Service
 public class DeblocageTestService {
-   
+
    @Autowired
    private SaeServiceStubUtils saeServiceStubUtils;
-   
+
    private DeblocageResultat appelWsOpDeblocage(String urlServiceWeb,
          ViStyle viStyle, ViFormulaire viParams,
          DeblocageFormulaire formulaire, WsTestListener wsListener) {
-      
-      DeblocageResponse  response = null;
+
+      DeblocageResponse response = null;
       DeblocageResultat res = null;
-      
-   // Vide le résultat du test précédent
+
+      // Vide le résultat du test précédent
       ResultatTest resultatTest = formulaire.getResultats();
       resultatTest.clear();
       ResultatTestLog log = resultatTest.getLog();
       wsListener.onSetStatusInitialResultatTest(resultatTest);
-      
-   // Ajout d'un log de résultat
+
+      // Ajout d'un log de résultat
       SaeServiceLogUtils.logAppelDeblocage(log, formulaire);
 
       // Récupération du stub du service web
       SaeServiceStub service = saeServiceStubUtils.getServiceStub(
             urlServiceWeb, viStyle, viParams);
-      
+
       try {
-         
-         Deblocage paramsService = SaeServiceObjectFactory.buildDeblocageRequest(formulaire.getIdJob().get(0));
-         
+
+         Deblocage paramsService = new Deblocage();
+         if (formulaire.getIdJob().size() != 0)
+            paramsService = SaeServiceObjectFactory
+                  .buildDeblocageRequest(formulaire.getIdJob().get(0));
+         else
+            paramsService = SaeServiceObjectFactory.buildDeblocageRequest("");
+
          response = service.deblocage(paramsService);
-         
+
          res = fromDeblocage(response);
-         
-      // Appel du listener
+
+         // Appel du listener
          wsListener.onRetourWsSansErreur(resultatTest, service
                ._getServiceClient().getServiceContext()
                .getConfigurationContext(), formulaire.getParent());
-         
-      // Log de la réponse obtenue
+
+         // Log de la réponse obtenue
          log.appendLogLn("Détails de la réponse obtenue de l'opération \"deblocage\" :");
          SaeServiceLogUtils.logResultatDeblocage(resultatTest, res);
 
-         
-      }catch (AxisFault fault) {
+      } catch (AxisFault fault) {
          // Appel du listener
          wsListener.onSoapFault(resultatTest, fault, service
                ._getServiceClient().getServiceContext()
                .getConfigurationContext(), formulaire.getParent());
-    } 
-         catch (RemoteException e) {
+      } catch (RemoteException e) {
          // Appel du listener
          wsListener.onRemoteException(resultatTest, e, service
                ._getServiceClient().getServiceContext()
                .getConfigurationContext(), formulaire.getParent());
-     }
+      }
 
       // Ajoute le timestamp en 1ère ligne du log
       log.insertTimestamp();
 
       // Renvoi du résultat
       return res;
-      
+
    }
-   
-   public final DeblocageResultat appelWsOpDeblocageTestLibre(String urlServiceWeb,
-         DeblocageFormulaire formulaire, ViFormulaire viParams) {
-      
+
+   public final DeblocageResultat appelWsOpDeblocageTestLibre(
+         String urlServiceWeb, DeblocageFormulaire formulaire,
+         ViFormulaire viParams) {
+
       // Création de l'objet qui implémente l'interface WsTestListener
       // et qui ne s'attend pas à un quelconque résultat (test libre)
       WsTestListener testLibre = new WsTestListenerImplLibre();
 
       // Appel de la méthode "générique" de test
-      return appelWsOpDeblocage(urlServiceWeb, ViStyle.VI_OK, viParams, formulaire,testLibre);
-      
+      return appelWsOpDeblocage(urlServiceWeb, ViStyle.VI_OK, viParams,
+            formulaire, testLibre);
+
    }
-   
-   private DeblocageResultat fromDeblocage(DeblocageResponse response){
-      
+
+   private DeblocageResultat fromDeblocage(DeblocageResponse response) {
+
       DeblocageResultat result = new DeblocageResultat();
       result.setEtat(response.getDeblocageResponse().getEtat());
       result.setIdTraitement(response.getDeblocageResponse().getUuid());
-     
+
       return result;
-    
+
    }
 
 }
