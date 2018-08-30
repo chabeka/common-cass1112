a5a607802591a8066ced33c1dc08350c5f7cadce
'SAMUEL CARRIERE' <samuel.carriere@acoss.fr>
2018-08-30 13:55:50 +0000

    [maven-release-plugin]  copy for tag commons-dfce-1.9.6
    svn path=/java/tags/commons-dfce-1.9.6/; revision=6174

--- sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/services/impl/storagedocument/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Ce package contient les classes qui implémentent les interfaces:
- * <li>{@link fr.urssaf.image.sae.storage.services.storagedocument.InsertionService}</li>
- * <li>{@link fr.urssaf.image.sae.storage.services.storagedocument.SearchingService}</li>
- * <li>{@link fr.urssaf.image.sae.storage.services.storagedocument.RetrievalService}</li>
- * <li>{@link fr.urssaf.image.sae.storage.services.storagedocument.DeletionService}</li>
- */
-package fr.urssaf.image.sae.storage.dfce.services.impl.storagedocument;
