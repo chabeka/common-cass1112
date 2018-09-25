ecf6f1e08623240870136570e2b03e16aa6d9d49
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2018-09-25 12:51:37 +0000

    Ajout d'un type generic pour l'extraction des données dans les tables thrift
    svn path=/java/branches/migrationThriftCQL/; revision=6409

--- sae/sae-trace/src/main/java/fr/urssaf/image/sae/trace/daocql/IGenericTypeDao.java
+++ sae/sae-trace/src/main/java/fr/urssaf/image/sae/trace/daocql/IGenericTypeDao.java
@@ -1,21 +1,17 @@
 /**
  *  TODO (AC75095028) Description du fichier
  */
-package fr.urssaf.image.sae.trace.daocql.impl;
+package fr.urssaf.image.sae.trace.daocql;
 
 import java.util.UUID;
 
-import org.springframework.stereotype.Service;
-
-import fr.urssaf.image.sae.commons.dao.impl.GenericDAOImpl;
+import fr.urssaf.image.sae.commons.dao.IGenericDAO;
 import fr.urssaf.image.sae.trace.dao.model.GenericType;
-import fr.urssaf.image.sae.trace.daocql.IGenericTypeDao;
 
 /**
  * Cette classe est utilisée juste pour l'extraction des données dans les tables thrift avec une requete cql.
  * La classe est utilisée que pour la migration des données des tables <b>Trace</b>.<br>
  */
-@Service
-public class GenericTypeImpl extends GenericDAOImpl<GenericType, UUID> implements IGenericTypeDao {
+public interface IGenericTypeDao extends IGenericDAO<GenericType, UUID> {
 
 }
--- sae/sae-trace/src/test/java/fr/urssaf/image/sae/trace/daocql/TraceDestinataireDaoCqlTest.java
+++ sae/sae-trace/src/test/java/fr/urssaf/image/sae/trace/daocql/TraceDestinataireDaoCqlTest.java
@@ -28,128 +28,128 @@ import fr.urssaf.image.sae.trace.utils.DateRegUtils;
 import junit.framework.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/applicationContext-sae-trace-test.xml"})
+@ContextConfiguration(locations = { "/applicationContext-sae-trace-test.xml" })
 public class TraceDestinataireDaoCqlTest {
 
-  @Autowired
-  private TraceDestinataireCqlSupport tracesupport;
+   @Autowired
+   private TraceDestinataireCqlSupport tracesupport;
 
-  @Autowired
-  private CassandraServerBeanCql server;
+   @Autowired
+   private CassandraServerBeanCql server;
 
-  private final List<String> list = Arrays.asList("date", "contrat");
+   private final List<String> list = Arrays.asList("date", "contrat");
 
-  @Autowired
-  TimeUUIDEtTimestampSupport timeuuid;
+   @Autowired
+   TimeUUIDEtTimestampSupport timeuuid;
 
-  @After
-  public void after() throws Exception {
-    server.resetData();
-  }
+   @After
+   public void after() throws Exception {
+      server.resetData();
+   }
 
-  @Test(expected = TraceRuntimeException.class)
-  public void createTestFailureCodeInexistant() {
-    createTrace("TEST|CREATE", "colonneNonRepertoriee");
-  }
+   @Test(expected = TraceRuntimeException.class)
+   public void createTestFailureCodeInexistant() {
+      createTrace("TEST|CREATE", "colonneNonRepertoriee");
+   }
 
-  @Test(expected = TraceRuntimeException.class)
-  public void create_new_trace_with_bad_code() {
-    final List<TraceDestinataire> allTraces = tracesupport.findAll();
-    final TraceDestinataire trace = new TraceDestinataire();
-    trace.setCodeEvt("WS_REPRISE_MASSE|KO");
+   @Test(expected = TraceRuntimeException.class)
+   public void create_new_trace_with_bad_code() {
+      final List<TraceDestinataire> allTraces = tracesupport.findAll();
+      final TraceDestinataire trace = new TraceDestinataire();
+      trace.setCodeEvt("WS_REPRISE_MASSE|KO");
 
-    final Map<String, List<String>> dest = new HashMap<String, List<String>>();
-    dest.put("BAD_CODE|KO", list);
-    trace.setDestinataires(dest);
+      final Map<String, List<String>> dest = new HashMap<String, List<String>>();
+      dest.put("BAD_CODE|KO", list);
+      trace.setDestinataires(dest);
 
-    tracesupport.create(trace, new Date().getTime());
+      tracesupport.create(trace, new Date().getTime());
 
-  }
+   }
 
-  @Test
-  public void create_new_trace_and_find_it_with_succes() {
+   @Test
+   public void create_new_trace_and_find_it_with_succes() {
 
-    final List<TraceDestinataire> traces = tracesupport.findAll();
-    final String code = "TEST|CREATE";
-    final TraceDestinataire trace = new TraceDestinataire();
-    trace.setCodeEvt(code);
+      final List<TraceDestinataire> traces = tracesupport.findAll();
+      final String code = "TEST|CREATE";
+      final TraceDestinataire trace = new TraceDestinataire();
+      trace.setCodeEvt(code);
 
-    final Map<String, List<String>> dest = new HashMap<String, List<String>>();
-    dest.put(TraceDestinataireEnum.HIST_ARCHIVE.name(), list);
-    trace.setDestinataires(dest);
+      final Map<String, List<String>> dest = new HashMap<String, List<String>>();
+      dest.put(TraceDestinataireEnum.HIST_ARCHIVE.name(), list);
+      trace.setDestinataires(dest);
 
-    tracesupport.create(trace, new Date().getTime());
-    final TraceDestinataire traceFromDB = tracesupport.findById(code);
+      tracesupport.create(trace, new Date().getTime());
+      final TraceDestinataire traceFromDB = tracesupport.findById(code);
 
-    Assert.assertEquals("le code événement doit etre correct",
-                        code,
-                        traceFromDB.getCodeEvt());
-    Assert.assertNotNull("la liste des destinataires doit etre non nulle",
-                         traceFromDB.getDestinataires());
-    Assert
-          .assertEquals(
-                        "il doit y avoir un et un seul élément dans la map des destinataires",
-                        1,
-                        traceFromDB.getDestinataires().size());
-    Assert.assertEquals("la clé présente doit etre valide",
-                        TraceDestinataireEnum.HIST_ARCHIVE.name(),
-                        traceFromDB
-                                   .getDestinataires().keySet().toArray(new String[0])[0]);
+      Assert.assertEquals("le code événement doit etre correct",
+                          code,
+                          traceFromDB.getCodeEvt());
+      Assert.assertNotNull("la liste des destinataires doit etre non nulle",
+                           traceFromDB.getDestinataires());
+      Assert
+            .assertEquals(
+                          "il doit y avoir un et un seul élément dans la map des destinataires",
+                          1,
+                          traceFromDB.getDestinataires().size());
+      Assert.assertEquals("la clé présente doit etre valide",
+                          TraceDestinataireEnum.HIST_ARCHIVE.name(),
+                          traceFromDB
+                                     .getDestinataires().keySet().toArray(new String[0])[0]);
 
-    final List<String> values = traceFromDB.getDestinataires().get(
-                                                                   TraceDestinataireEnum.HIST_ARCHIVE.name());
-    Assert.assertEquals(
-                        "le nombre d'éléments de la liste doit etre correcte", 2, values
-                                                                                        .size());
-    Assert.assertTrue("tous les éléments présents doivent etre corrects",
-                      list.containsAll(values));
-  }
+      final List<String> values = traceFromDB.getDestinataires().get(
+                                                                     TraceDestinataireEnum.HIST_ARCHIVE.name());
+      Assert.assertEquals(
+                          "le nombre d'éléments de la liste doit etre correcte", 2, values
+                                                                                          .size());
+      Assert.assertTrue("tous les éléments présents doivent etre corrects",
+                        list.containsAll(values));
+   }
 
-  @Test
-  public void create_delete_with_success() {
-    final Date date = DateRegUtils.getDateWithoutTime();
-    final UUID id = timeuuid.buildUUIDFromDate(date);
-    final String code = "TEST|DELETE";
-    final String colName = TraceDestinataireEnum.HIST_ARCHIVE.name();
+   @Test
+   public void create_delete_with_success() {
+      final Date date = DateRegUtils.getDateWithoutTime();
+      final UUID id = timeuuid.buildUUIDFromDate(date);
+      final String code = "TEST|DELETE";
+      final String colName = TraceDestinataireEnum.HIST_ARCHIVE.name();
 
-    final TraceDestinataire trace = new TraceDestinataire();
-    trace.setCodeEvt(code);
+      final TraceDestinataire trace = new TraceDestinataire();
+      trace.setCodeEvt(code);
 
-    final Map<String, List<String>> dest = new HashMap<String, List<String>>();
-    dest.put(TraceDestinataireEnum.HIST_ARCHIVE.name(), list);
-    trace.setDestinataires(dest);
-    tracesupport.create(trace, new Date().getTime());
+      final Map<String, List<String>> dest = new HashMap<String, List<String>>();
+      dest.put(TraceDestinataireEnum.HIST_ARCHIVE.name(), list);
+      trace.setDestinataires(dest);
+      tracesupport.create(trace, new Date().getTime());
 
-    tracesupport.delete(code, new Date().getTime());
+      tracesupport.delete(code, new Date().getTime());
 
-    final TraceDestinataire TraceDest = tracesupport.findById(code);
-    Assert.assertNull("L'objet doit etre null", TraceDest);
+      final TraceDestinataire TraceDest = tracesupport.findById(code);
+      Assert.assertNull("L'objet doit etre null", TraceDest);
 
-  }
+   }
 
-  private void createTrace(final String code, final String colName) {
-    final TraceDestinataire trace = new TraceDestinataire();
-    trace.setCodeEvt(code);
+   private void createTrace(final String code, final String colName) {
+      final TraceDestinataire trace = new TraceDestinataire();
+      trace.setCodeEvt(code);
 
-    final Map<String, List<String>> dest = new HashMap<String, List<String>>();
-    dest.put(colName, list);
-    trace.setDestinataires(dest);
+      final Map<String, List<String>> dest = new HashMap<String, List<String>>();
+      dest.put(colName, list);
+      trace.setDestinataires(dest);
 
-    tracesupport.create(trace, new Date().getTime());
-  }
+      tracesupport.create(trace, new Date().getTime());
+   }
 
-  @Test
-  public void findAll() {
-    final TraceDestinataire trace = new TraceDestinataire();
-    trace.setCodeEvt("TEST|CREATE");
-    final Map<String, List<String>> map = new HashMap<String, List<String>>();
-    final List<String> listString = new ArrayList<>();
-    listString.add("TEST LIST");
-    map.put("HIST_ARCHIVE", listString);
-    trace.setDestinataires(map);
-    tracesupport.create(trace, new Date().getTime());
-    final List<TraceDestinataire> traces = tracesupport.findAll();
-    Assert.assertNotNull(traces);
-  }
+   @Test
+   public void findAll() {
+      final TraceDestinataire trace = new TraceDestinataire();
+      trace.setCodeEvt("TEST|CREATE");
+      final Map<String, List<String>> map = new HashMap<String, List<String>>();
+      final List<String> listString = new ArrayList<>();
+      listString.add("TEST LIST");
+      map.put("HIST_ARCHIVE", listString);
+      trace.setDestinataires(map);
+      tracesupport.create(trace, new Date().getTime());
+      final List<TraceDestinataire> traces = tracesupport.findAll();
+      Assert.assertNotNull(traces);
+   }
 
 }
