35d7640278b0aef4d28b66846e8cfe5dccb81153
'SAMUEL CARRIERE' <samuel.carriere@acoss.fr>
2018-10-01 14:10:00 +0000

    Pour le redmine #315577 : reprise transfert avec suppression
    svn path=/java/branches/merge-hawaiv6/; revision=6418

--- sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/services/impl/storagedocument/crud/UpdateServiceImpl.java
+++ sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/services/impl/storagedocument/crud/UpdateServiceImpl.java
@@ -33,14 +33,13 @@ import net.docubase.toolkit.model.document.Document;
  * Classe d'implémentation de l'interface {@link UpdateService}. Cette classe
  * est un singleton et peut être accessible via le mécanisme d'injection IOC et
  * l'annotation @Autowired
- *
  */
 @Service
 public class UpdateServiceImpl extends AbstractServices implements
-UpdateService {
+                               UpdateService {
 
    private static final Logger LOGGER = LoggerFactory
-         .getLogger(UpdateServiceImpl.class);
+                                                     .getLogger(UpdateServiceImpl.class);
 
    @Autowired
    private TracesDfceSupport tracesDfceSupport;
@@ -52,13 +51,14 @@ UpdateService {
    @ServiceChecked
    public void updateStorageDocument(final UUID uuidJob, final UUID uuid,
                                      final List<StorageMetadata> modifiedMetadatas,
-                                     final List<StorageMetadata> deletedMetadatas) throws UpdateServiceEx {
+                                     final List<StorageMetadata> deletedMetadatas)
+         throws UpdateServiceEx {
 
       final String trcPrefix = "updateStorageDocument()";
       LOGGER.debug("{} - début", trcPrefix);
       LOGGER.debug("{} - Récupération des informations du document", trcPrefix);
-      final List<String> modifMetas = new ArrayList<String>();
-      final List<String> delMetas = new ArrayList<String>();
+      final List<String> modifMetas = new ArrayList<>();
+      final List<String> delMetas = new ArrayList<>();
       final Document storedDocument = getDfceServices().getDocumentByUUID(uuid);
 
       LOGGER.debug("{} - Modification des critères", trcPrefix);
@@ -69,7 +69,7 @@ UpdateService {
       LOGGER.debug("{} - Suppression des critères", trcPrefix);
       for (final StorageMetadata metadata : Utils.nullSafeIterable(deletedMetadatas)) {
          storedDocument.deleteCriterion(storedDocument
-                                        .getSingleCriterion(metadata.getShortCode()));
+                                                      .getSingleCriterion(metadata.getShortCode()));
          delMetas.add(metadata.getShortCode());
       }
       LOGGER.debug("{} - Mise à jour dans DFCE", trcPrefix);
@@ -80,40 +80,25 @@ UpdateService {
          referenceDate = referenceDateUpdate(modifiedMetadatas, storedDocument);
          getDfceServices().updateDocument(storedDocument);
 
-      } catch (final TagControlException exception) {
+      }
+      catch (final TagControlException exception) {
          rollback(rndCode, referenceDate, storedDocument);
          throw new UpdateServiceEx(exception);
 
-      } catch (final FrozenDocumentException exception) {
+      }
+      catch (final FrozenDocumentException exception) {
          rollback(rndCode, referenceDate, storedDocument);
          throw new UpdateServiceEx(exception);
 
       }
       LOGGER.debug("{} - Ajout d'une trace", trcPrefix);
-      tracesDfceSupport.traceModifDocumentDansDFCE(uuid, modifMetas, delMetas,
+      tracesDfceSupport.traceModifDocumentDansDFCE(uuid,
+                                                   modifMetas,
+                                                   delMetas,
                                                    new Date());
 
       LOGGER.debug("{} - fin", trcPrefix);
 
-   }
-
-   /**
-    * Retourne l'objet StorageMetadata de code passé en paramètre à partir de la
-    * liste listMetadatas
-    *
-    * @param listMetadatas
-    * @param shortCode
-    * @return
-    */
-   private StorageMetadata getStorageMetadataByCode(
-                                                    final List<StorageMetadata> listMetadatas, final String shortCode) {
-      StorageMetadata metaData = null;
-      for (final StorageMetadata storageMetadata : listMetadatas) {
-         if (shortCode.equals(storageMetadata.getShortCode())) {
-            metaData = storageMetadata;
-         }
-      }
-      return metaData;
    }
 
    private void rollback(final String rndCode, final Date referenceDate,
@@ -124,23 +109,27 @@ UpdateService {
       if (StringUtils.isNotBlank(rndCode)) {
          try {
             updateRnd(storedDocument, rndCode);
-         } catch (final FrozenDocumentException exception) {
+         }
+         catch (final FrozenDocumentException exception) {
             LOGGER.error("{} - Impossible de réinitialiser à l'état "
                   + "d'origine le code RND pour le document {}. "
-                  + "Code d'origine : {}", new Object[] { trcPrefix,
-                                                          storedDocument.getUuid().toString(), rndCode });
+                  + "Code d'origine : {}",
+                         new Object[] {trcPrefix,
+                                       storedDocument.getUuid().toString(), rndCode});
          }
       }
 
       if (referenceDate != null) {
          try {
             updateReferenceDate(referenceDate, storedDocument);
-         } catch (final FrozenDocumentException exception) {
+         }
+         catch (final FrozenDocumentException exception) {
             LOGGER.error("{} - Impossible de réinitialiser à l'état "
                   + "d'origine la date de débit de conservation "
-                  + "pour le document {}. Date d'origine : {}", new Object[] {
-                                                                              trcPrefix, storedDocument.getUuid().toString(),
-                                                                              DateFormatUtils.ISO_DATE_FORMAT.format(referenceDate) });
+                  + "pour le document {}. Date d'origine : {}",
+                         new Object[] {
+                                       trcPrefix, storedDocument.getUuid().toString(),
+                                       DateFormatUtils.ISO_DATE_FORMAT.format(referenceDate)});
          }
       }
 
@@ -158,7 +147,8 @@ UpdateService {
    }
 
    private Date referenceDateUpdate(final List<StorageMetadata> modifiedMetadatas,
-                                    final Document storedDocument) throws FrozenDocumentException {
+                                    final Document storedDocument)
+         throws FrozenDocumentException {
       final String trcPrefix = "referenceDateUpdate()";
       LOGGER.debug("{} - début", trcPrefix);
 
@@ -187,14 +177,16 @@ UpdateService {
    }
 
    private String rndCodeUpdate(final List<StorageMetadata> modifiedMetadatas,
-                                final Document storedDocument) throws FrozenDocumentException {
+                                final Document storedDocument)
+         throws FrozenDocumentException {
       final String trcPrefix = "rndCodeUpdate()";
       LOGGER.debug("{} - début", trcPrefix);
 
       String oldRnd = null;
 
       final StorageMetadata metadata = find(
-                                            StorageTechnicalMetadatas.TYPE.getShortCode(), modifiedMetadatas);
+                                            StorageTechnicalMetadatas.TYPE.getShortCode(),
+                                            modifiedMetadatas);
       if (metadata != null) {
          oldRnd = storedDocument.getType();
          updateRnd(storedDocument, (String) metadata.getValue());
@@ -209,7 +201,9 @@ UpdateService {
 
       final String trcPrefix = "find";
       LOGGER.debug("{} - début", trcPrefix);
-
+      if (modifiedMetadatas == null) {
+         return null;
+      }
       int index = 0;
       StorageMetadata metadata = null;
       while (metadata == null && index < modifiedMetadatas.size()) {
@@ -228,44 +222,52 @@ UpdateService {
       final String trcPrefix = "manageMetadata";
       LOGGER.debug("{} - début", trcPrefix);
 
-      if (StorageTechnicalMetadatas.TITRE.getShortCode().equals(
-                                                                metadata.getShortCode())) {
+      if (StorageTechnicalMetadatas.TITRE.getShortCode()
+                                         .equals(
+                                                 metadata.getShortCode())) {
          storedDocument.setTitle((String) metadata.getValue());
 
-      } else if (StorageTechnicalMetadatas.DATE_CREATION.getShortCode().equals(
-                                                                               metadata.getShortCode())) {
+      } else if (StorageTechnicalMetadatas.DATE_CREATION.getShortCode()
+                                                        .equals(
+                                                                metadata.getShortCode())) {
          storedDocument.setCreationDate((Date) metadata.getValue());
 
       } else if (StorageTechnicalMetadatas.ID_MODIFICATION_MASSE_INTERNE
-            .getShortCode().equals(metadata.getShortCode())) {
+                                                                        .getShortCode()
+                                                                        .equals(metadata.getShortCode())) {
          if (storedDocument.getSingleCriterion(metadata.getShortCode()) == null) {
             storedDocument.addCriterion(metadata.getShortCode(),
                                         uuidJob.toString());
          } else {
-            storedDocument.getSingleCriterion(metadata.getShortCode()).setWord(
-                                                                               uuidJob.toString());
+            storedDocument.getSingleCriterion(metadata.getShortCode())
+                          .setWord(
+                                   uuidJob.toString());
          }
       } // MAJ de la métadonnée pour tracer le traitement de transfert dans le doc
       else if (StorageTechnicalMetadatas.ID_TRANSFERT_MASSE_INTERNE
-            .getShortCode().equals(metadata.getShortCode())) {
+                                                                   .getShortCode()
+                                                                   .equals(metadata.getShortCode())) {
          if (storedDocument.getSingleCriterion(metadata.getShortCode()) == null) {
             storedDocument.addCriterion(metadata.getShortCode(),
                                         uuidJob.toString());
          } else {
-            storedDocument.getSingleCriterion(metadata.getShortCode()).setWord(
-                                                                               uuidJob.toString());
+            storedDocument.getSingleCriterion(metadata.getShortCode())
+                          .setWord(
+                                   uuidJob.toString());
          }
-      }
-      else if (!StorageTechnicalMetadatas.DATE_DEBUT_CONSERVATION
-            .getShortCode().equals(metadata.getShortCode())
-            && !StorageTechnicalMetadatas.TYPE.getShortCode().equals(
-                                                                     metadata.getShortCode())) {
+      } else if (!StorageTechnicalMetadatas.DATE_DEBUT_CONSERVATION
+                                                                   .getShortCode()
+                                                                   .equals(metadata.getShortCode())
+            && !StorageTechnicalMetadatas.TYPE.getShortCode()
+                                              .equals(
+                                                      metadata.getShortCode())) {
          if (storedDocument.getSingleCriterion(metadata.getShortCode()) == null) {
             storedDocument.addCriterion(metadata.getShortCode(),
                                         (Serializable) metadata.getValue());
          } else {
-            storedDocument.getSingleCriterion(metadata.getShortCode()).setWord(
-                                                                               (Serializable) metadata.getValue());
+            storedDocument.getSingleCriterion(metadata.getShortCode())
+                          .setWord(
+                                   (Serializable) metadata.getValue());
          }
       }
       LOGGER.debug("{} - fin", trcPrefix);
