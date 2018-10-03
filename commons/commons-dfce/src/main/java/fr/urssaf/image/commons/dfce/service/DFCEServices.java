ca16b3b4e65bfd1560baa7e3832d9efdeb592a9b
'SAMUEL CARRIERE' <samuel.carriere@acoss.fr>
2018-10-03 14:22:59 +0000

    Ajouts des services DFCE utilisés par sae-dfce-admin-exploit
    svn path=/java/branches/merge-hawaiv6/; revision=6420

--- commons/commons-dfce/src/main/java/fr/urssaf/image/commons/dfce/service/DFCEServices.java
+++ commons/commons-dfce/src/main/java/fr/urssaf/image/commons/dfce/service/DFCEServices.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -14,6 +15,15 @@ import com.docubase.dfce.exception.NoSuchAttachmentException;
 import com.docubase.dfce.exception.ObjectAlreadyExistsException;
 import com.docubase.dfce.exception.SearchQueryParseException;
 import com.docubase.dfce.exception.TagControlException;
+import com.docubase.dfce.exception.batch.DfceJobParametersInvalidException;
+import com.docubase.dfce.exception.batch.UnexpectedDfceJobExecutionException;
+import com.docubase.dfce.exception.batch.launch.DfceJobInstanceAlreadyExistsException;
+import com.docubase.dfce.exception.batch.launch.DfceJobParametersNotFoundException;
+import com.docubase.dfce.exception.batch.launch.NoSuchDfceJobException;
+import com.docubase.dfce.exception.batch.launch.NoSuchDfceJobExecutionException;
+import com.docubase.dfce.exception.batch.repository.DfceJobExecutionAlreadyRunningException;
+import com.docubase.dfce.exception.batch.repository.DfceJobInstanceAlreadyCompleteException;
+import com.docubase.dfce.exception.batch.repository.DfceJobRestartException;
 
 import fr.urssaf.image.commons.dfce.exception.DFCEConnectionServiceException;
 import fr.urssaf.image.commons.dfce.model.DFCEConnection;
@@ -21,6 +31,7 @@ import net.docubase.toolkit.model.base.Base;
 import net.docubase.toolkit.model.base.CategoryDataType;
 import net.docubase.toolkit.model.document.Attachment;
 import net.docubase.toolkit.model.document.Document;
+import net.docubase.toolkit.model.index.IndexInformation;
 import net.docubase.toolkit.model.note.Note;
 import net.docubase.toolkit.model.recordmanager.RMDocEvent;
 import net.docubase.toolkit.model.recordmanager.RMLogArchiveReport;
@@ -38,9 +49,8 @@ import net.docubase.toolkit.model.search.SearchResult;
  * Une instance de DFCEServices est thread-safe : elle peut être partagée par plusieurs threads. La reconnexion
  * est gérée de manière à ne pas perturber les autres threads.
  * Dans l'application SAE : sur les plateformes GNT, on gère deux instance de cette classe :
- *   - une instance vers le serveur DFCE GNT local
- *   - une instance vers un serveur DFCE de la plateforme GNS associée, pour le transfert
- *
+ * - une instance vers le serveur DFCE GNT local
+ * - une instance vers un serveur DFCE de la plateforme GNS associée, pour le transfert
  */
 public interface DFCEServices {
    /**
@@ -53,208 +63,323 @@ public interface DFCEServices {
     * pas appeler directement cette méthode, sauf pour des tests.
     */
    void reconnect() throws DFCEConnectionServiceException;
+
    /**
     * Permet de fermer la connexion vers DFCE. Ne pas utiliser si la connexion est partagée par plusieurs threads !
     */
    void closeConnexion();
+
    /**
     * Renvoie les paramètres de connexions vers DFCE
     */
    DFCEConnection getCnxParams();
+
    /**
     * Renvoie la base DFCE courante (spécifiée dans les paramètres de connexion)
     */
 
    Base getBase();
+
    /**
     * Récupère un document par son uuid sur la base courante
+    * 
     * @see net.docubase.toolkit.service.ged.SearchService#getDocumentByUUID
     */
    Document getDocumentByUUID(final UUID uuid);
+
    /**
     * @see net.docubase.toolkit.service.ged.SearchService#getDocumentByUUID(Base, UUID)
     */
    Document getDocumentByUUID(final Base base, final UUID uuid);
+
    /**
     * Récupère un document de la corbeille par son uuid sur la base courante
+    * 
     * @see net.docubase.toolkit.service.ged.RecycleBinService#getDocumentByUUID(Base, UUID)
     */
    Document getDocumentByUUIDFromRecycleBin(final UUID uuid);
+
    /**
     * @see net.docubase.toolkit.service.ged.SearchService#search(SearchQuery)
     */
    SearchResult search(SearchQuery searchQuery) throws ExceededSearchLimitException, SearchQueryParseException;
+
    /**
     * @see net.docubase.toolkit.service.administration.BaseAdministrationService#updateBase(Base)
     */
    Base updateBase(Base base);
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#findOrCreateCategory(String, CategoryDataType)
     */
    Category findOrCreateCategory(String metadataCode, CategoryDataType metadataType);
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#addAttachment(UUID, String, String, boolean, String, InputStream)
     */
    Document addAttachment(UUID paramUUID, String paramString1, String paramString2, boolean paramBoolean, String paramString3, InputStream paramInputStream)
          throws FrozenDocumentException, TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#getAllLifeCycleRules()
     */
    Set<LifeCycleRule> getAllLifeCycleRules();
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#getAttachmentFile(Document, Attachment)
     */
    InputStream getAttachmentFile(Document paramDocument, Attachment paramAttachment) throws NoSuchAttachmentException;
+
    /**
     * @see net.docubase.toolkit.service.ged.RecycleBinService#throwAwayDocument(UUID)
     */
    Document throwAwayDocument(UUID paramUUID) throws FrozenDocumentException;
+
    /**
     * @see net.docubase.toolkit.service.ged.RecycleBinService#restoreDocument(UUID)
     */
    Document restoreDocument(UUID paramUUID) throws TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.ged.NoteService#getNotes(UUID)
     */
    List<Note> getNotes(UUID paramUUID);
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#storeDocument(Document, InputStream)
     */
    Document storeDocument(Document paramDocument, InputStream paramInputStream) throws TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#storeDocument(Document, StoreOptions, byte[], InputStream)
     */
-   Document storeDocument(Document paramDocument, StoreOptions paramStoreOptions, byte[] paramArrayOfByte, InputStream paramInputStream) throws TagControlException;
+   Document storeDocument(Document paramDocument, StoreOptions paramStoreOptions, byte[] paramArrayOfByte, InputStream paramInputStream)
+         throws TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#storeDocument(Document, String, String, InputStream)
     */
    Document storeDocument(Document paramDocument, String paramString1, String paramString2, InputStream paramInputStream) throws TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#getLifeCycleRule(String)
     */
    LifeCycleRule getLifeCycleRule(String paramString);
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#getDocumentFile(Document)
     */
    InputStream getDocumentFile(Document paramDocument);
+
    /**
     * Récupère le fichier d'un document dans la corbeille, par son uuid sur la base courante
+    * 
     * @see net.docubase.toolkit.service.ged.RecycleBinService#getDocumentByUUID(Base, UUID)
     */
    InputStream getDocumentFileFromRecycleBin(Document paramDocument);
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#isFrozen(Document)
     */
    boolean isFrozen(Document paramDocument);
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#deleteDocument(UUID)
     */
    void deleteDocument(UUID paramUUID) throws FrozenDocumentException;
+
    /**
     * Supprime un document se trouvant dans la corbeille, par son uuid
+    * 
     * @see net.docubase.toolkit.service.ged.RecycleBinService#deleteDocument(UUID)
     */
    void deleteDocumentFromRecycleBin(UUID paramUUID) throws FrozenDocumentException;
+
    /**
     * @see net.docubase.toolkit.service.ged.NoteService#storeNote(Note)
     */
    Note storeNote(Note paramNote) throws FrozenDocumentException, TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#createFileReference(String, String, InputStream)
     */
    FileReference createFileReference(String paramString1, String paramString2, InputStream paramInputStream);
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#storeVirtualDocument(Document, FileReference, int, int)
     */
    Document storeVirtualDocument(Document paramDocument, FileReference paramFileReference, int paramInt1, int paramInt2)
          throws TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.ged.SearchService#createDocumentIterator(SearchQuery)
     */
    Iterator<Document> createDocumentIterator(SearchQuery paramSearchQuery)
          throws SearchQueryParseException;
+
    /**
     * @see net.docubase.toolkit.service.ged.RecycleBinService#createDocumentIterator(SearchQuery)
     */
    Iterator<Document> createDocumentIteratorFromRecycleBin(SearchQuery paramSearchQuery)
          throws SearchQueryParseException;
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#updateDocument(Document)
     */
    Document updateDocument(Document paramDocument)
          throws TagControlException, FrozenDocumentException;
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#updateDocumentType(Document, String)
     */
    void updateDocumentType(Document paramDocument, String paramString)
          throws FrozenDocumentException;
+
    /**
     * @see net.docubase.toolkit.service.ged.StoreService#updateDocumentLifeCycleReferenceDate(Document, Date)
     */
    Document updateDocumentLifeCycleReferenceDate(Document paramDocument, Date paramDate)
          throws FrozenDocumentException;
+
    /**
     * @see net.docubase.toolkit.service.ged.RecordManagerService#createCustomSystemEventLog(RMSystemEvent)
     */
    RMSystemEvent createCustomSystemEventLog(RMSystemEvent paramRMSystemEvent);
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#fetchAllCompositeIndex()
     */
    Set<CompositeIndex> fetchAllCompositeIndex();
+
    /**
     * @deprecated
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#getCategory(String)
     */
    @Deprecated
    Category getCategory(String paramString);
+
    /**
     * @deprecated
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#findOrCreateCompositeIndex(Category...)
     */
    @Deprecated
    CompositeIndex findOrCreateCompositeIndex(Category... paramVarArgs);
+
    /**
     * @see net.docubase.toolkit.service.ged.RecordManagerService#createCustomDocumentEventLog(RMDocEvent)
     */
    RMDocEvent createCustomDocumentEventLog(RMDocEvent paramRMDocEvent);
+
    /**
     * @see net.docubase.toolkit.service.ged.RecordManagerService#getDocumentEventLogsByDates(Date, Date)
     */
    List<RMDocEvent> getDocumentEventLogsByDates(Date paramDate1, Date paramDate2);
+
    /**
     * @see net.docubase.toolkit.service.ged.RecordManagerService#getDocumentEventLogsByUUID(UUID)
     */
    List<RMDocEvent> getDocumentEventLogsByUUID(UUID paramUUID);
+
    /**
     * @see net.docubase.toolkit.service.ged.RecordManagerService#getSystemEventLogsByDates(Date, Date)
     */
    List<RMSystemEvent> getSystemEventLogsByDates(Date paramDate1, Date paramDate2);
+
    /**
     * @see net.docubase.toolkit.service.ged.ArchiveService#getLogsArchiveBase()
     */
    Base getLogsArchiveBase();
+
    /**
     * @see net.docubase.toolkit.service.ged.ArchiveService#createSystemLogArchiveChainingReport(Date, Date)
     */
    List<RMLogArchiveReport> createSystemLogArchiveChainingReport(Date paramDate1, Date paramDate2);
+
    /**
     * @see net.docubase.toolkit.service.ged.ArchiveService#createDocumentLogArchiveChainingReport(Date, Date)
     */
    List<RMLogArchiveReport> createDocumentLogArchiveChainingReport(Date paramDate1, Date paramDate2);
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#createNewLifeCycleRule(LifeCycleRule)
     */
    LifeCycleRule createNewLifeCycleRule(LifeCycleRule paramLifeCycleRule) throws ObjectAlreadyExistsException;
+
    /**
     * @see net.docubase.toolkit.service.administration.StorageAdministrationService#updateLifeCycleRule(LifeCycleRule)
     */
    LifeCycleRule updateLifeCycleRule(LifeCycleRule paramLifeCycleRule);
+
    /**
     * @see net.docubase.toolkit.service.ged.NoteService#addNote(UUID, String)
     */
    Note addNote(UUID paramUUID, String paramString) throws FrozenDocumentException, TagControlException;
+
    /**
     * @see net.docubase.toolkit.service.ServiceProvider#isServerUp()
     */
    boolean isServerUp();
+
+   /**
+    * @see net.docubase.toolkit.service.administration.JobAdministrationService#startNextInstance(String)
+    */
+   Long startNextInstance(String jobName)
+         throws NoSuchDfceJobException, DfceJobParametersNotFoundException, DfceJobRestartException, DfceJobExecutionAlreadyRunningException,
+         DfceJobInstanceAlreadyCompleteException, UnexpectedDfceJobExecutionException, DfceJobParametersInvalidException;
+
+   /**
+    * @see net.docubase.toolkit.service.administration.JobAdministrationService#start(String, String)
+    */
+   Long start(String jobName, String parameters) throws NoSuchDfceJobException, DfceJobInstanceAlreadyExistsException, DfceJobParametersInvalidException;
+
+   /**
+    * @see net.docubase.toolkit.service.administration.IndexAdministrationService#getIndexesOverLimitInBase(Integer, UUID)
+    */
+   List<IndexInformation> getIndexesOverLimitInBase(Integer limit, UUID baseUUID);
+
+   /**
+    * @see net.docubase.toolkit.service.administration.IndexAdministrationService#getIndexesInBase(UUID)
+    */
+   List<IndexInformation> getIndexesInBase(UUID baseUUID);
+
+   /**
+    * @see net.docubase.toolkit.service.administration.JobAdministrationService#getSummaryAsMap(long)
+    */
+   Map<String, String> getSummaryAsMap(long executionId) throws NoSuchDfceJobExecutionException;
+
+   /**
+    * @see net.docubase.toolkit.service.administration.JobAdministrationService#restart(long)
+    */
+   Long restart(long executionId) throws DfceJobInstanceAlreadyCompleteException, NoSuchDfceJobExecutionException, NoSuchDfceJobException,
+         DfceJobRestartException, DfceJobParametersInvalidException;
+
+   /**
+    * @see net.docubase.toolkit.service.ged.ArchiveService#isDocumentLogsArchiveRunning()
+    */
+   boolean isDocumentLogsArchiveRunning();
+
+   /**
+    * @see net.docubase.toolkit.service.ged.ArchiveService#getLastDocumentLogsArchiveUUID()
+    */
+   UUID getLastDocumentLogsArchiveUUID();
+
+   /**
+    * @see net.docubase.toolkit.service.ged.ArchiveService#getLastSucessfulDocumentLogsArchiveRunDate()
+    */
+   Date getLastSucessfulDocumentLogsArchiveRunDate();
+
+   /**
+    * @see net.docubase.toolkit.service.ged.ArchiveService#getLastSystemLogsArchiveUUID()
+    */
+   UUID getLastSystemLogsArchiveUUID();
+
+   /**
+    * @see net.docubase.toolkit.service.ged.ArchiveService#getLastSucessfulSystemLogsArchiveRunDate()
+    */
+   Date getLastSucessfulSystemLogsArchiveRunDate();
+
+   /**
+    * @see net.docubase.toolkit.service.ged.ArchiveService#isSystemLogsArchiveRunning()
+    */
+   boolean isSystemLogsArchiveRunning();
 }
