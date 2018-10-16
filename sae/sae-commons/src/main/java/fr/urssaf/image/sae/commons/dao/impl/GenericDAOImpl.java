5c8017c993fa85e111ba83752eb7f51fb6f4fac7
'NICOLAS VANGOUT' <nicolas.vangout-externe@acoss.fr>
2018-10-16 12:06:11 +0000

    Changement de version librairie java driver de datastax qui entraine la modification des signatures de methodes.
    svn path=/java/branches/merge-migrationCQL-hawaiv6/; revision=6475

--- sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/dao/impl/GenericDAOImpl.java
+++ sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/dao/impl/GenericDAOImpl.java
@@ -24,7 +24,6 @@ import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
 
 import fr.urssaf.image.commons.cassandra.helper.CassandraCQLClientFactory;
-import fr.urssaf.image.commons.cassandra.helper.CassandraServerBeanCql;
 import fr.urssaf.image.sae.commons.dao.IGenericDAO;
 import fr.urssaf.image.sae.commons.utils.ColumnUtil;
 import fr.urssaf.image.sae.commons.utils.QueryUtils;
@@ -35,170 +34,166 @@ import fr.urssaf.image.sae.commons.utils.QueryUtils;
 
 public class GenericDAOImpl<T, ID> implements IGenericDAO<T, ID> {
 
-   private static final Logger LOGGER = LoggerFactory.getLogger(GenericDAOImpl.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(GenericDAOImpl.class);
 
-   @Autowired
-   protected CassandraCQLClientFactory ccf;
+  @Autowired
+  protected CassandraCQLClientFactory ccf;
 
-   protected Class<? extends T> daoType;
+  protected Class<? extends T> daoType;
 
-   private MappingManager manager;
+  private MappingManager manager;
 
-   protected Mapper<T> mapper;
+  protected Mapper<T> mapper;
 
-   public GenericDAOImpl() {
-      final Type t = getClass().getGenericSuperclass();
-      final ParameterizedType pt = (ParameterizedType) t;
-      daoType = (Class) pt.getActualTypeArguments()[0];
-   }
+  public GenericDAOImpl() {
+    final Type t = getClass().getGenericSuperclass();
+    final ParameterizedType pt = (ParameterizedType) t;
+    daoType = (Class) pt.getActualTypeArguments()[0];
+  }
 
-   /**
-    * @return the daoType
-    */
-   @Override
-   public Class<? extends T> getDaoType() {
-      return daoType;
-   }
+  /**
+   * @return the daoType
+   */
+  @Override
+  public Class<? extends T> getDaoType() {
+    return daoType;
+  }
 
-   /**
-    * @return the ccf
-    */
-   @Override
-   public CassandraCQLClientFactory getCcf() {
-      return ccf;
-   }
+  /**
+   * @return the ccf
+   */
+  @Override
+  public CassandraCQLClientFactory getCcf() {
+    return ccf;
+  }
 
-   /**
-    * Mapper le type T à la table cassandra
-    *
-    * @return
-    */
-   @Override
-   @SuppressWarnings("unchecked")
-   public Mapper<T> getMapper() {
-      if (mapper == null) {
-         manager = new MappingManager(ccf.getSession());
-         if (ccf.getStartLocal()) {
-            mapper = (Mapper<T>) manager.mapper(daoType, CassandraServerBeanCql.KEYSPACE_TU);
-         } else {
-            mapper = (Mapper<T>) manager.mapper(daoType);
-         }
-      }
-      return mapper;
-   }
+  /**
+   * Mapper le type T à la table cassandra
+   *
+   * @return
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public Mapper<T> getMapper() {
+    if (mapper == null) {
+      manager = new MappingManager(ccf.getSession());
+      mapper = (Mapper<T>) manager.mapper(daoType);
+    }
+    return mapper;
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public Optional<T> findWithMapperById(final ID id) {
-      Assert.notNull(id, "L'identifiant ne peut être null");
-      final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
-      final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
-      Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Optional<T> findWithMapperById(final ID id) {
+    Assert.notNull(id, "L'identifiant ne peut être null");
+    final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
+    final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
+    Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
 
-      final String keyName = keyField.getName();
-      select.where(eq(keyName.toLowerCase(), id));
-      final ResultSet result = getSession().execute(select);
-      return Optional.ofNullable(getMapper().map(result).one());
-   }
+    final String keyName = keyField.getName();
+    select.where(eq(keyName.toLowerCase(), id));
+    final ResultSet result = getSession().execute(select);
+    return Optional.ofNullable(getMapper().map(result).one());
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public Iterator<T> IterableFindById(final ID id) {
-      Assert.notNull(id, "L'identifiant ne peut être null");
-      final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
-      final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
-      Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Iterator<T> IterableFindById(final ID id) {
+    Assert.notNull(id, "L'identifiant ne peut être null");
+    final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
+    final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
+    Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
 
-      final String keyName = keyField.getName();
-      select.where(eq(keyName, id));
-      final ResultSet result = getSession().execute(select);
-      return getMapper().map(result).iterator();
-   }
+    final String keyName = keyField.getName();
+    select.where(eq(keyName, id));
+    final ResultSet result = getSession().execute(select);
+    return getMapper().map(result).iterator();
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public ResultSet findById(final ID id) {
-      Assert.notNull(id, " l'id est requis");
-      final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
-      final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
-      Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ResultSet findById(final ID id) {
+    Assert.notNull(id, " l'id est requis");
+    final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
+    final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
+    Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
 
-      final String keyName = keyField.getName();
-      select.where(eq(keyName, id));
-      final ResultSet result = getSession().execute(select);
-      return result;
-   }
+    final String keyName = keyField.getName();
+    select.where(eq(keyName, id));
+    final ResultSet result = getSession().execute(select);
+    return result;
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public boolean existsById(final ID id) {
-      Assert.notNull(id, " l'id est requis");
-      return findWithMapperById(id).isPresent();
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean existsById(final ID id) {
+    Assert.notNull(id, " l'id est requis");
+    return findWithMapperById(id).isPresent();
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void deleteById(final ID id) {
-      Assert.notNull(id, " l'id est requis");
-      final Delete delete = QueryBuilder.delete().from(ccf.getKeyspace(), getTypeArgumentsName());
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void deleteById(final ID id) {
+    Assert.notNull(id, " l'id est requis");
+    final Delete delete = QueryBuilder.delete().from(ccf.getKeyspace(), getTypeArgumentsName());
 
-      final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
-      Assert.notNull(keyField, "La clé de l'entité à supprimer ne peut être null");
+    final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
+    Assert.notNull(keyField, "La clé de l'entité à supprimer ne peut être null");
 
-      final String keyName = keyField.getName();
-      delete.where(eq(keyName, id));
-      getMapper().map(getSession().execute(delete));
-   }
+    final String keyName = keyField.getName();
+    delete.where(eq(keyName, id));
+    getMapper().map(getSession().execute(delete));
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void delete(final T entity) {
-      Assert.notNull(entity, " l'entity est requis");
-      final Delete delete = QueryBuilder.delete().from(ccf.getKeyspace(), getTypeArgumentsName());
-      QueryUtils.createDeleteQuery(daoType, delete, entity);
-      getSession().execute(delete);
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void delete(final T entity) {
+    Assert.notNull(entity, " l'entity est requis");
+    final Delete delete = QueryBuilder.delete().from(ccf.getKeyspace(), getTypeArgumentsName());
+    QueryUtils.createDeleteQuery(daoType, delete, entity);
+    getSession().execute(delete);
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void deleteAll(final Iterable<T> entities) {
-      Assert.notNull(entities, " les entités a supprimer sont requis");
-      for (final T entity : entities) {
-         delete(entity);
-      }
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void deleteAll(final Iterable<T> entities) {
+    Assert.notNull(entities, " les entités a supprimer sont requis");
+    for (final T entity : entities) {
+      delete(entity);
+    }
+  }
 
-   /**
-    * {@inheritDoc}
-    * 
-    * @Override
-    *           public ResultSet findAllById(final Iterable<ID> ids) {
-    *           Assert.notNull(ids, " les ids des entités a supprimer sont requis");
-    *           final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
-    *           return getSession().execute(select);
-    *           }
-    */
+  /**
+   * {@inheritDoc}
+   * 
+   * @Override
+   *           public ResultSet findAllById(final Iterable<ID> ids) {
+   *           Assert.notNull(ids, " les ids des entités a supprimer sont requis");
+   *           final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
+   *           return getSession().execute(select);
+   *           }
+   */
 
-   /**
-    * @return the logger
-    */
-   @Override
-   public Logger getLogger() {
-      return LOGGER;
-   }
+  /**
+   * @return the logger
+   */
+  @Override
+  public Logger getLogger() {
+    return LOGGER;
+  }
 
 }
--- sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/dao/impl/GenericIndexDAOImpl.java
+++ sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/dao/impl/GenericIndexDAOImpl.java
@@ -16,7 +16,6 @@ import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
 
 import fr.urssaf.image.commons.cassandra.helper.CassandraCQLClientFactory;
-import fr.urssaf.image.commons.cassandra.helper.CassandraServerBeanCql;
 import fr.urssaf.image.sae.commons.dao.IGenericIndexDAO;
 import fr.urssaf.image.sae.commons.utils.ColumnUtil;
 
@@ -25,79 +24,75 @@ import fr.urssaf.image.sae.commons.utils.ColumnUtil;
  */
 public class GenericIndexDAOImpl<T, ID> implements IGenericIndexDAO<T, ID> {
 
-   @Autowired
-   protected CassandraCQLClientFactory ccf;
+  @Autowired
+  protected CassandraCQLClientFactory ccf;
 
-   private MappingManager manager;
+  private MappingManager manager;
 
-   protected Class<? extends T> daoType;
+  protected Class<? extends T> daoType;
 
-   protected Mapper<T> mapper;
+  protected Mapper<T> mapper;
 
-   private static final Logger LOGGER = LoggerFactory.getLogger(GenericIndexDAOImpl.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(GenericIndexDAOImpl.class);
 
-   public GenericIndexDAOImpl() {
-      final Type t = getClass().getGenericSuperclass();
-      final ParameterizedType pt = (ParameterizedType) t;
-      daoType = (Class) pt.getActualTypeArguments()[0];
-   }
+  public GenericIndexDAOImpl() {
+    final Type t = getClass().getGenericSuperclass();
+    final ParameterizedType pt = (ParameterizedType) t;
+    daoType = (Class) pt.getActualTypeArguments()[0];
+  }
 
-   /**
-    * Mapper le type T à la table cassandra
-    *
-    * @return
-    */
-   @Override
-   @SuppressWarnings("unchecked")
-   public Mapper<T> getMapper() {
-      if (mapper == null) {
-         manager = new MappingManager(ccf.getSession());
-         if (ccf.getStartLocal()) {
-            mapper = (Mapper<T>) manager.mapper(daoType, CassandraServerBeanCql.KEYSPACE_TU);
-         } else {
-            mapper = (Mapper<T>) manager.mapper(daoType);
-         }
-      }
-      return mapper;
-   }
+  /**
+   * Mapper le type T à la table cassandra
+   *
+   * @return
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public Mapper<T> getMapper() {
+    if (mapper == null) {
+      manager = new MappingManager(ccf.getSession());
+      mapper = (Mapper<T>) manager.mapper(daoType);
+    }
+    return mapper;
+  }
 
-   /**
-    * @return the daoType
-    */
-   @Override
-   public Class<? extends T> getDaoType() {
-      return daoType;
-   }
+  /**
+   * @return the daoType
+   */
+  @Override
+  public Class<? extends T> getDaoType() {
+    return daoType;
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public CassandraCQLClientFactory getCcf() {
-      return ccf;
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public CassandraCQLClientFactory getCcf() {
+    return ccf;
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public Logger getLogger() {
-      return LOGGER;
-   }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Logger getLogger() {
+    return LOGGER;
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public Iterator<T> findAllWithMapperById(final ID id) {
-      Assert.notNull(id, "L'id ne peut être null");
-      final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
-      final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
-      Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Iterator<T> findAllWithMapperById(final ID id) {
+    Assert.notNull(id, "L'id ne peut être null");
+    final Select select = QueryBuilder.select().from(ccf.getKeyspace(), getTypeArgumentsName());
+    final Field keyField = ColumnUtil.getSimplePartionKeyField(daoType);
+    Assert.notNull(keyField, "La clé de l'entité à chercher ne peut être null");
 
-      final String keyName = keyField.getName();
-      select.where(QueryBuilder.eq(keyName, id));
-      return getMapper().map(getSession().execute(select)).iterator();
-   }
+    final String keyName = keyField.getName();
+    select.where(QueryBuilder.eq(keyName, id));
+    return getMapper().map(getSession().execute(select)).iterator();
+  }
 
 }
