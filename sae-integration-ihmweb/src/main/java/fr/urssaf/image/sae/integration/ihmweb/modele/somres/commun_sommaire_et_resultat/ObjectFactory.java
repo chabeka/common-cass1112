7bbbd815dbd6b5a288755196af02ca5aa404b226
'NICOLAS VANGOUT' <nicolas.vangout-externe@acoss.fr>
2018-12-03 10:14:37 +0000

    Modification pour migration hawai v6
    svn path=/java/branches/merge-hawaiv6/; revision=6643

--- sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/modele/somres/commun_sommaire_et_resultat/ObjectFactory.java
+++ sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/modele/somres/commun_sommaire_et_resultat/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.5-2 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2016.11.17 à 03:49:54 PM CET 
+// Généré le : 2018.11.30 à 10:58:04 AM CET 
 //
 
 
@@ -47,14 +47,6 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link DocumentVirtuelType }
-     * 
-     */
-    public DocumentVirtuelType createDocumentVirtuelType() {
-        return new DocumentVirtuelType();
-    }
-
-    /**
      * Create an instance of {@link NonIntegratedVirtualDocumentType }
      * 
      */
@@ -63,19 +55,19 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link IntegratedDocumentType }
+     * Create an instance of {@link DocumentVirtuelType }
      * 
      */
-    public IntegratedDocumentType createIntegratedDocumentType() {
-        return new IntegratedDocumentType();
+    public DocumentVirtuelType createDocumentVirtuelType() {
+        return new DocumentVirtuelType();
     }
 
     /**
-     * Create an instance of {@link ListeIntegratedDocumentsType }
+     * Create an instance of {@link IntegratedComposantDocumentVirtuelType }
      * 
      */
-    public ListeIntegratedDocumentsType createListeIntegratedDocumentsType() {
-        return new ListeIntegratedDocumentsType();
+    public IntegratedComposantDocumentVirtuelType createIntegratedComposantDocumentVirtuelType() {
+        return new IntegratedComposantDocumentVirtuelType();
     }
 
     /**
@@ -84,6 +76,86 @@ public class ObjectFactory {
      */
     public DocumentType createDocumentType() {
         return new DocumentType();
+    }
+
+    /**
+     * Create an instance of {@link ListeNonIntegratedVirtualDocumentsType }
+     * 
+     */
+    public ListeNonIntegratedVirtualDocumentsType createListeNonIntegratedVirtualDocumentsType() {
+        return new ListeNonIntegratedVirtualDocumentsType();
+    }
+
+    /**
+     * Create an instance of {@link MetadonneeType }
+     * 
+     */
+    public MetadonneeType createMetadonneeType() {
+        return new MetadonneeType();
+    }
+
+    /**
+     * Create an instance of {@link ListeMetadonneeType }
+     * 
+     */
+    public ListeMetadonneeType createListeMetadonneeType() {
+        return new ListeMetadonneeType();
+    }
+
+    /**
+     * Create an instance of {@link ListeIntegratedDocumentsVirtuelsType }
+     * 
+     */
+    public ListeIntegratedDocumentsVirtuelsType createListeIntegratedDocumentsVirtuelsType() {
+        return new ListeIntegratedDocumentsVirtuelsType();
+    }
+
+    /**
+     * Create an instance of {@link ListeErreurType }
+     * 
+     */
+    public ListeErreurType createListeErreurType() {
+        return new ListeErreurType();
+    }
+
+    /**
+     * Create an instance of {@link FichierType }
+     * 
+     */
+    public FichierType createFichierType() {
+        return new FichierType();
+    }
+
+    /**
+     * Create an instance of {@link NonIntegratedDocumentType }
+     * 
+     */
+    public NonIntegratedDocumentType createNonIntegratedDocumentType() {
+        return new NonIntegratedDocumentType();
+    }
+
+    /**
+     * Create an instance of {@link ListeDocumentsVirtuelsType }
+     * 
+     */
+    public ListeDocumentsVirtuelsType createListeDocumentsVirtuelsType() {
+        return new ListeDocumentsVirtuelsType();
+    }
+
+    /**
+     * Create an instance of {@link ErreurType }
+     * 
+     */
+    public ErreurType createErreurType() {
+        return new ErreurType();
+    }
+
+    /**
+     * Create an instance of {@link ListeIntegratedDocumentsType }
+     * 
+     */
+    public ListeIntegratedDocumentsType createListeIntegratedDocumentsType() {
+        return new ListeIntegratedDocumentsType();
     }
 
     /**
@@ -103,75 +175,19 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link FichierType }
-     * 
-     */
-    public FichierType createFichierType() {
-        return new FichierType();
-    }
-
-    /**
-     * Create an instance of {@link ListeDocumentsVirtuelsType }
-     * 
-     */
-    public ListeDocumentsVirtuelsType createListeDocumentsVirtuelsType() {
-        return new ListeDocumentsVirtuelsType();
-    }
-
-    /**
-     * Create an instance of {@link NonIntegratedDocumentType }
-     * 
-     */
-    public NonIntegratedDocumentType createNonIntegratedDocumentType() {
-        return new NonIntegratedDocumentType();
-    }
-
-    /**
-     * Create an instance of {@link ListeIntegratedDocumentsVirtuelsType }
-     * 
-     */
-    public ListeIntegratedDocumentsVirtuelsType createListeIntegratedDocumentsVirtuelsType() {
-        return new ListeIntegratedDocumentsVirtuelsType();
-    }
-
-    /**
-     * Create an instance of {@link MetadonneeType }
-     * 
-     */
-    public MetadonneeType createMetadonneeType() {
-        return new MetadonneeType();
-    }
-
-    /**
-     * Create an instance of {@link ErreurType }
-     * 
-     */
-    public ErreurType createErreurType() {
-        return new ErreurType();
-    }
-
-    /**
-     * Create an instance of {@link ListeMetadonneeType }
-     * 
-     */
-    public ListeMetadonneeType createListeMetadonneeType() {
-        return new ListeMetadonneeType();
-    }
-
-    /**
-     * Create an instance of {@link IntegratedComposantDocumentVirtuelType }
-     * 
-     */
-    public IntegratedComposantDocumentVirtuelType createIntegratedComposantDocumentVirtuelType() {
-        return new IntegratedComposantDocumentVirtuelType();
-    }
-
-    /**
      * Create an instance of {@link ListeDocumentsType }
      * 
      */
     public ListeDocumentsType createListeDocumentsType() {
         return new ListeDocumentsType();
+    }
+
+    /**
+     * Create an instance of {@link IntegratedDocumentType }
+     * 
+     */
+    public IntegratedDocumentType createIntegratedDocumentType() {
+        return new IntegratedDocumentType();
     }
 
     /**
@@ -183,22 +199,6 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link ListeNonIntegratedVirtualDocumentsType }
-     * 
-     */
-    public ListeNonIntegratedVirtualDocumentsType createListeNonIntegratedVirtualDocumentsType() {
-        return new ListeNonIntegratedVirtualDocumentsType();
-    }
-
-    /**
-     * Create an instance of {@link ListeErreurType }
-     * 
-     */
-    public ListeErreurType createListeErreurType() {
-        return new ListeErreurType();
-    }
-
-    /**
      * Create an instance of {@link IntegratedDocumentVirtuelType.Composants }
      * 
      */
@@ -207,19 +207,19 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link DocumentVirtuelType.Composants }
-     * 
-     */
-    public DocumentVirtuelType.Composants createDocumentVirtuelTypeComposants() {
-        return new DocumentVirtuelType.Composants();
-    }
-
-    /**
      * Create an instance of {@link NonIntegratedVirtualDocumentType.Composants }
      * 
      */
     public NonIntegratedVirtualDocumentType.Composants createNonIntegratedVirtualDocumentTypeComposants() {
         return new NonIntegratedVirtualDocumentType.Composants();
+    }
+
+    /**
+     * Create an instance of {@link DocumentVirtuelType.Composants }
+     * 
+     */
+    public DocumentVirtuelType.Composants createDocumentVirtuelTypeComposants() {
+        return new DocumentVirtuelType.Composants();
     }
 
 }
--- sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/modele/somres/sommaire/package-info.java
+++ sae-integration-ihmweb/src/main/java/fr/urssaf/image/sae/integration/ihmweb/modele/somres/sommaire/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.5-2 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2016.11.17 à 03:49:54 PM CET 
+// Généré le : 2018.11.30 à 10:58:04 AM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.cirtil.fr/sae/sommaireXml", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
