73fdb7871376e18ac4026a969cf4451e7dbd3459
'NICOLAS VANGOUT' <nicolas.vangout-externe@acoss.fr>
2019-02-08 13:05:56 +0000

    Upgrade Spring : Erreur build jenkins
    svn path=/java/branches/sae-spring-upgrade/; revision=6807

--- sae-spring-batch-admin/sae-spring-batch-integration/src/test/java/org/springframework/batch/integration/retry/RepeatTransactionalPollingIntegrationTests.java
+++ sae-spring-batch-admin/sae-spring-batch-integration/src/test/java/org/springframework/batch/integration/retry/RepeatTransactionalPollingIntegrationTests.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -71,7 +72,7 @@ public class RepeatTransactionalPollingIntegrationTests implements ApplicationCo
 		logger.debug("Handled: " + message);		
 	}
 
-	@Before
+	@After
 	public void clearLists() {
 		list.clear();
 		handled.clear();
