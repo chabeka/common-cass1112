8ceaf2fd75cd0995ae411f0e84d957582e27ee3e
'NICOLAS VANGOUT' <nicolas.vangout-externe@acoss.fr>
2019-04-10 08:59:54 +0000

    Redmine #364943 : Amélioration des logs GED suite à retour de R7. > Blindage du code de recherche de classe pour la gestion de logs http de la GED pour éviter les StackOverflowError.
    svn path=/java/trunk/; revision=6959

--- sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/utils/SearchObjectClassUtil.java
+++ sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/utils/SearchObjectClassUtil.java
@@ -6,63 +6,77 @@ import java.util.List;
 
 public class SearchObjectClassUtil {
 
-   /**
-    * Méthode utilitaire permettant de retrouver une classe dans un object à
-    * analyser. Cette utilitaire ne gère pas les type primitifs.
-    *
-    * @param objAnalyse
-    *           Object à analyser
-    * @param arrayKlazzSearch
-    *           Classes recherchées
-    * @return Objet trouvé
-    */
-   public static Object searchObjectByClass(final Object objAnalyse,
-                                            final String... arrayKlazzSearch) {
-      final List<String> listeKlazzSearch = Arrays.asList(arrayKlazzSearch);
-      final String lofPrefix = "searchObjectByClass - ";
-      Object objFind = null;
-      if (objAnalyse == null || arrayKlazzSearch == null) {
-         return null;
-      } else {
-         final Class klazz = objAnalyse.getClass();
-         if (!(klazz.isPrimitive() || objAnalyse instanceof String
-               || objAnalyse instanceof Integer
-               || objAnalyse instanceof Double
-               || objAnalyse instanceof Long
-               || objAnalyse instanceof Float
-               || objAnalyse instanceof Boolean
-               || objAnalyse instanceof Byte
-               || objAnalyse instanceof Short
-               || objAnalyse instanceof Void
-               || objAnalyse instanceof Character)) {
-            try {
-               for (final Field field : klazz.getDeclaredFields()) {
-                  field.setAccessible(true);
-                  final Object f = field.get(objAnalyse);
-                  if (f != null) {
-                     field.setAccessible(false);
+  /**
+   * Méthode utilitaire permettant de retrouver une classe dans un object à
+   * analyser. Cette utilitaire ne gère pas les type primitifs.
+   *
+   * @param objAnalyse
+   *          Object à analyser
+   * @param arrayKlazzSearch
+   *          Classes recherchées
+   * @return Objet trouvé
+   */
+  public static Object searchObjectByClass(final Object objAnalyse,
+                                           final String... arrayKlazzSearch) {
+    final List<String> listeKlazzSearch = Arrays.asList(arrayKlazzSearch);
+    Object objFind = null;
+    if (objAnalyse == null || arrayKlazzSearch == null) {
+      return null;
+    } else {
+      // On récupere la class de l'objet à analyser
+      final Class klazz = objAnalyse.getClass();
+      // On ne gére pas les primitif et les types génériques
+      if (!(klazz.isPrimitive() || objAnalyse instanceof String
+          || objAnalyse instanceof Integer
+          || objAnalyse instanceof Double
+          || objAnalyse instanceof Long
+          || objAnalyse instanceof Float
+          || objAnalyse instanceof Boolean
+          || objAnalyse instanceof Byte
+          || objAnalyse instanceof Short
+          || objAnalyse instanceof Void
+          || objAnalyse instanceof Character)) {
+        try {
+          // On boucle sur les champs de la classe
+          for (final Field field : klazz.getDeclaredFields()) {
+            // On rend le champs accessible
+            field.setAccessible(true);
+            // On récupére l'objet representant le champ
+            final Object f = field.get(objAnalyse);
+            if (f != null) {
+              // On supprime l'accessibilité du champ
+              field.setAccessible(false);
 
-                     final Class klazzFind = f.getClass();
-                     if (listeKlazzSearch.contains(klazzFind.toString())) {
-                        return f;
-                     } else {
-                        objFind = searchObjectByClass(f, arrayKlazzSearch);
-                     }
+              // On récupére la classe du champ à analyser
+              final Class klazzFind = f.getClass();
+              // On vérifie que la classe trouvée correspond aux classes recherchées
+              if (listeKlazzSearch.contains(klazzFind.toString())) {
+                return f;
+              } else {
+                // Si on n'a pas trouvé la classe recherché, on parcours le champ. On fait attention pour le cas des classes static et des singleton
+                // car le champ peut être la classe elle même et on risque de boucler à l'infini.
+                if (!klazz.equals(klazzFind) && klazzFind.getDeclaredFields() != null && klazzFind.getDeclaredFields().length > 0) {
+                  objFind = searchObjectByClass(f, arrayKlazzSearch);
+                }
+              }
 
-                     if (objFind != null) {
-                        break;
-                     }
-                  }
-               }
-            } catch (final IllegalAccessException e) {
-               e.printStackTrace();
-            } catch (final IllegalArgumentException e) {
-               e.printStackTrace();
+              // Classe trouvé, on sort
+              if (objFind != null) {
+                break;
+              }
             }
-         }
+          }
+        }
+        catch (final IllegalAccessException e) {
+          e.printStackTrace();
+        }
+        catch (final IllegalArgumentException e) {
+          e.printStackTrace();
+        }
       }
+    }
 
-      return objFind;
-   }
+    return objFind;
+  }
 
 }
