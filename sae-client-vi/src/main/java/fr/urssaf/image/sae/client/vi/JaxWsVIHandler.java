559083735864d2ae4edf3519622e89f8a86fb7e1
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2019-05-20 12:54:17 +0000

    Ajout d'un header vide au soapMessage dans le cas ou le header est null
    svn path=/java/trunk/; revision=7004

--- sae-client-vi/src/main/java/fr/urssaf/image/sae/client/vi/JaxWsVIHandler.java
+++ sae-client-vi/src/main/java/fr/urssaf/image/sae/client/vi/JaxWsVIHandler.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
 import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
@@ -26,110 +29,116 @@ import fr.urssaf.image.sae.client.vi.signature.KeyStoreInterface;
  */
 public class JaxWsVIHandler implements SOAPHandler<SOAPMessageContext> {
 
-   private static final Logger LOGGER = LoggerFactory.getLogger(JaxWsVIHandler.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(JaxWsVIHandler.class);
 
-   private KeyStoreInterface iKeyStore = null;
+  private KeyStoreInterface iKeyStore = null;
 
-   private List<String> pagms = null;
+  private List<String> pagms = null;
 
-   private String issuer = null;
+  private String issuer = null;
 
-   private String login = null;
+  private String login = null;
 
-   /**
-    * Constructeur
-    *
-    * @param iKeyStore
-    *           keystore à utiliser
-    * @param pagms
-    *           liste des pagms
-    * @param issuer
-    *           issuer
-    * @param login
-    *           login de l'utilisateur demandeur du service
-    */
-   public JaxWsVIHandler(final KeyStoreInterface iKeyStore, final List<String> pagms,
-                    final String issuer, final String login) {
-      super();
-      this.iKeyStore = iKeyStore;
-      this.issuer = issuer;
-      this.pagms = pagms;
-      this.login = login;
+  /**
+   * Constructeur
+   *
+   * @param iKeyStore
+   *          keystore à utiliser
+   * @param pagms
+   *          liste des pagms
+   * @param issuer
+   *          issuer
+   * @param login
+   *          login de l'utilisateur demandeur du service
+   */
+  public JaxWsVIHandler(final KeyStoreInterface iKeyStore, final List<String> pagms,
+                        final String issuer, final String login) {
+    super();
+    this.iKeyStore = iKeyStore;
+    this.issuer = issuer;
+    this.pagms = pagms;
+    this.login = login;
 
-      LOGGER.debug("Instanciation d'un VIHandler avec les paramètres suivants : issuer={}, login={}, pagms={}, iKeyStore={}",
-                   issuer,
-                   login,
-                   pagms,
-                   iKeyStore);
-   }
+    LOGGER.debug("Instanciation d'un VIHandler avec les paramètres suivants : issuer={}, login={}, pagms={}, iKeyStore={}",
+                 issuer,
+                 login,
+                 pagms,
+                 iKeyStore);
+  }
 
-   /*
-    * (non-Javadoc)
-    * @see javax.xml.ws.handler.Handler#handleMessage(javax.xml.ws.handler.MessageContext)
-    */
-   @Override
-   public boolean handleMessage(final SOAPMessageContext msgCtx) {
+  /*
+   * (non-Javadoc)
+   * @see javax.xml.ws.handler.Handler#handleMessage(javax.xml.ws.handler.MessageContext)
+   */
+  @Override
+  public boolean handleMessage(final SOAPMessageContext msgCtx) {
 
-      // Ajout de l'en-tête WS-Security chargé depuis un fichier de ressource
-      // XML
-      try {
+    // Ajout de l'en-tête WS-Security chargé depuis un fichier de ressource
+    // XML
+    try {
 
-         // Génération de l'en-tête wsse
-         final Document doc = VIGenerator.getWsseHeader(issuer, login, pagms, iKeyStore.getKeystore(), iKeyStore.getAlias(), iKeyStore.getPassword());
+      // Génération de l'en-tête wsse
+      final Document doc = VIGenerator.getWsseHeader(issuer, login, pagms, iKeyStore.getKeystore(), iKeyStore.getAlias(), iKeyStore.getPassword());
 
-         // Ajout du message dans le header
-         final SOAPMessage soapMsg = msgCtx.getMessage();
-         addXMLDocumentToSoapHeader(doc, soapMsg);
-
+      // Ajout du message dans le header
+      final SOAPMessage soapMsg = msgCtx.getMessage();
+      final SOAPPart soapPart = soapMsg.getSOAPPart();
+      final SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
+      SOAPHeader soapHeader = soapEnvelope.getHeader();
+      if (soapHeader == null) {
+        soapHeader = soapEnvelope.addHeader();
       }
-      catch (final Exception e) {
-         throw new ViSignatureException(e);
-      }
-      return true;
+      addXMLDocumentToSoapHeader(doc, soapMsg);
 
-   }
+    }
+    catch (final Exception e) {
+      throw new ViSignatureException(e);
+    }
+    return true;
 
-   /*
-    * (non-Javadoc)
-    * @see javax.xml.ws.handler.Handler#close(javax.xml.ws.handler.MessageContext)
-    */
-   @Override
-   public void close(final MessageContext arg0) {
-      // do nothing
-   }
+  }
 
-   /*
-    * (non-Javadoc)
-    * @see javax.xml.ws.handler.Handler#handleFault(javax.xml.ws.handler.MessageContext)
-    */
-   @Override
-   public boolean handleFault(final SOAPMessageContext arg0) {
-      // Return true to continue processing
-      return true;
-   }
+  /*
+   * (non-Javadoc)
+   * @see javax.xml.ws.handler.Handler#close(javax.xml.ws.handler.MessageContext)
+   */
+  @Override
+  public void close(final MessageContext arg0) {
+    // do nothing
+  }
 
-   /*
-    * (non-Javadoc)
-    * @see javax.xml.ws.handler.soap.SOAPHandler#getHeaders()
-    */
-   @Override
-   public Set<QName> getHeaders() {
-      // Pas de traitement spécifique sur les headers
-      return null;
-   }
+  /*
+   * (non-Javadoc)
+   * @see javax.xml.ws.handler.Handler#handleFault(javax.xml.ws.handler.MessageContext)
+   */
+  @Override
+  public boolean handleFault(final SOAPMessageContext arg0) {
+    // Return true to continue processing
+    return true;
+  }
 
-   /*
-    * Permet d'ajouter un fragment XML dans les entêtes SOAP
-    */
-   private static void addXMLDocumentToSoapHeader(final Document document, final SOAPMessage soapMessage) throws SOAPException {
-      final DocumentFragment docFrag = document.createDocumentFragment();
-      final Element rootElement = document.getDocumentElement();
-      if (rootElement != null) {
-         docFrag.appendChild(rootElement);
-         final Document ownerDoc = soapMessage.getSOAPHeader().getOwnerDocument();
-         final org.w3c.dom.Node replacingNode = ownerDoc.importNode(docFrag, true);
-         soapMessage.getSOAPHeader().appendChild(replacingNode);
-      }
-   }
+  /*
+   * (non-Javadoc)
+   * @see javax.xml.ws.handler.soap.SOAPHandler#getHeaders()
+   */
+  @Override
+  public Set<QName> getHeaders() {
+    // Pas de traitement spécifique sur les headers
+    return null;
+  }
+
+  /*
+   * Permet d'ajouter un fragment XML dans les entêtes SOAP
+   */
+  private static void addXMLDocumentToSoapHeader(final Document document, final SOAPMessage soapMessage) throws SOAPException {
+    final DocumentFragment docFrag = document.createDocumentFragment();
+    final Element rootElement = document.getDocumentElement();
+    if (rootElement != null) {
+      docFrag.appendChild(rootElement);
+      final Document ownerDoc = soapMessage.getSOAPHeader().getOwnerDocument();
+      final org.w3c.dom.Node replacingNode = ownerDoc.importNode(docFrag, true);
+      soapMessage.getSOAPHeader().appendChild(replacingNode);
+    }
+  }
 
 }
