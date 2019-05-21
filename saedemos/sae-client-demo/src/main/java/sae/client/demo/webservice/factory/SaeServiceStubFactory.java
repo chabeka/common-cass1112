2f202c65d7349a9195d41050deb3c68d7ef2ef3c
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2019-05-21 14:30:19 +0000

    Evolution #328351: la solution proposée par Alexis krier pour contourner le probleme des MTOM
    svn path=/java/trunk/; revision=7008

--- saedemos/sae-client-demo/src/main/java/sae/client/demo/webservice/factory/SaeServiceStubFactory.java
+++ saedemos/sae-client-demo/src/main/java/sae/client/demo/webservice/factory/SaeServiceStubFactory.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import javax.xml.stream.XMLStreamException;
+
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.context.ServiceContext;
+import org.apache.axis2.description.MessageContextListener;
 import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.axis2.engine.Phase;
 
@@ -22,146 +27,169 @@ import sae.client.demo.webservice.security.MyKeyStore;
  */
 public final class SaeServiceStubFactory {
 
-   /**
-    * Nom du fichier properties contenant l'URL du service web SAE
-    */
-   private static final String NOM_FICHIER_PROP = "sae-client-demo.properties";
+  /**
+   * Nom du fichier properties contenant l'URL du service web SAE
+   */
+  private static final String NOM_FICHIER_PROP = "sae-client-demo.properties";
 
-   /**
-    * Clé de la propriété dans le fichier {@link #NOM_FICHIER_PROP} qui contient
-    * l'URL du service web SAE
-    */
-   private static final String CLE_URL_SERVICE = "server.url";
+  /**
+   * Clé de la propriété dans le fichier {@link #NOM_FICHIER_PROP} qui contient
+   * l'URL du service web SAE
+   */
+  private static final String CLE_URL_SERVICE = "server.url";
 
-   /**
-    * Login de l'utilisateur qui lance l'action.
-    */
-   private static final String VI_LOGIN = "CER69XXXXX";
+  /**
+   * Login de l'utilisateur qui lance l'action.
+   */
+  private static final String VI_LOGIN = "CER69XXXXX";
 
-   /**
-    * Identifiant du Contrat de Service à utiliser dans le Vecteur
-    * d'Identification. A Renseigné dans le fichier de properties
-    */
-   private static final String CONTRAT_SERVICE = "contrat.de.service";
+  /**
+   * Identifiant du Contrat de Service à utiliser dans le Vecteur
+   * d'Identification. A Renseigné dans le fichier de properties
+   */
+  private static final String CONTRAT_SERVICE = "contrat.de.service";
 
-   /**
-    * PAGM à utiliser dans le Vecteur d'Identification. A Renseigné dans le fichier de properties
-    */
-   private static final String PAGM = "pagm";
+  /**
+   * PAGM à utiliser dans le Vecteur d'Identification. A Renseigné dans le fichier de properties
+   */
+  private static final String PAGM = "pagm";
 
-   private SaeServiceStubFactory() {
-      // Constructeur privé
-   }
+  private SaeServiceStubFactory() {
+    // Constructeur privé
+  }
 
-   private static Properties getProperties() {
-      final Properties properties = new Properties();
-      try {
-         properties.load(ResourceUtils.loadResource(new SaeServiceStubFactory(),
-                                                    NOM_FICHIER_PROP));
-         return properties;
-      }
-      catch (final IOException e) {
-         throw new DemoRuntimeException(e);
-      }
-   }
+  private static Properties getProperties() {
+    final Properties properties = new Properties();
+    try {
+      properties.load(ResourceUtils.loadResource(new SaeServiceStubFactory(),
+                                                 NOM_FICHIER_PROP));
+      return properties;
+    }
+    catch (final IOException e) {
+      throw new DemoRuntimeException(e);
+    }
+  }
 
-   /**
-    * Lecture dans le fichier properties de l'URL pointant sur le service web
-    * SAE
-    *
-    * @return URL du service web SAE
-    */
-   private static String litUrlSaeService() {
-      return getProperties().getProperty(CLE_URL_SERVICE);
-   }
+  /**
+   * Lecture dans le fichier properties de l'URL pointant sur le service web
+   * SAE
+   *
+   * @return URL du service web SAE
+   */
+  private static String litUrlSaeService() {
+    return getProperties().getProperty(CLE_URL_SERVICE);
+  }
 
-   private static List<String> listPagmSaeService() {
-      final String pagm = getProperties().getProperty(PAGM);
-      return Arrays.asList(pagm.split("\\s*,\\s*"));
-   }
+  private static List<String> listPagmSaeService() {
+    final String pagm = getProperties().getProperty(PAGM);
+    return Arrays.asList(pagm.split("\\s*,\\s*"));
+  }
 
-   /**
-    * Création d'un Stub paramétré avec l'authentification au service web du SAE
-    *
-    * @return le Stub
-    */
-   public static SaeServiceStub createStubAvecAuthentification() {
+  /**
+   * Création d'un Stub paramétré avec l'authentification au service web du SAE
+   *
+   * @return le Stub
+   */
+  public static SaeServiceStub createStubAvecAuthentification() {
 
-      // Lecture du l'URL du service web du SAE depuis le fichier properties
+    // Lecture du l'URL du service web du SAE depuis le fichier properties
+    final String urlSaeService = litUrlSaeService();
+
+    // Création d'une configuration Axis2 par défaut
+    ConfigurationContext configContext;
+    try {
+      configContext = ConfigurationContextFactory
+                                                 .createConfigurationContextFromFileSystem(null, null);
+    }
+    catch (final AxisFault e) {
+      throw new DemoRuntimeException(e);
+    }
+
+    // Création de l'objet Stub
+    SaeServiceStub service;
+    try {
+      service = new SaeServiceStub(configContext, urlSaeService);
+
+      // Pour pallier au problème MTOM Les explication sur l'url ci-dessous
+      // URL : https://issues.apache.org/jira/browse/AXIS2-5202?focusedCommentId=13293399&page=
+      // com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-13293399
+
+      service._getServiceClient().getAxisService().addMessageContextListener(new MessageContextListener() {
+
+        @Override
+        public void attachServiceContextEvent(final ServiceContext sc, final MessageContext mc) {
+
+        }
+
+        @Override
+        public void attachEnvelopeEvent(final MessageContext mc) {
+          try {
+            mc.getEnvelope().cloneOMElement().serialize(System.out);
+          }
+
+          catch (final XMLStreamException e) {
+            throw new DemoRuntimeException(e);
+          }
+        }
+      });
+    }
+    catch (final AxisFault e) {
+      throw new DemoRuntimeException(e);
+    }
+
+    // Récupération de l'objet exposant le PKCS#12
+    // Dans un cas "normal", on récupère l'instance créée au démarrage
+    // de l'application.
+    final KeyStoreInterface keystore = new MyKeyStore();
+
+    // Instancie l'objet de génération du VI
+    final Axis2VIHandler axis2VIHandler = new Axis2VIHandler(keystore,
+                                                             listPagmSaeService(),
+                                                             getProperties().getProperty(CONTRAT_SERVICE),
+                                                             VI_LOGIN);
+    // Ajout d'un Handler lors de la phase "MessageOut" pour insérer le VI
+    final AxisConfiguration axisConfig = configContext.getAxisConfiguration();
+    final List<Phase> outFlowPhases = axisConfig.getOutFlowPhases();
+    final Phase messageOut = findPhaseByName(outFlowPhases, "MessageOut");
+    messageOut.addHandler(axis2VIHandler);
+
+    // Renvoie l'objet Stub
+    return service;
+
+  }
+
+  /**
+   * Création d'un Stub sans authentification
+   *
+   * @return le Stub
+   */
+  public static SaeServiceStub createStubSansAuthentification() {
+    try {
+
       final String urlSaeService = litUrlSaeService();
 
-      // Création d'une configuration Axis2 par défaut
-      ConfigurationContext configContext;
-      try {
-         configContext = ConfigurationContextFactory
-                                                    .createConfigurationContextFromFileSystem(null, null);
+      return new SaeServiceStub(urlSaeService);
+
+    }
+    catch (final AxisFault e) {
+      throw new DemoRuntimeException(e);
+    }
+  }
+
+  private static Phase findPhaseByName(final List<Phase> phases,
+                                       final String nomPhaseRecherchee) {
+
+    Phase result = null;
+
+    for (final Phase phase : phases) {
+      if (phase.getName().equals(nomPhaseRecherchee)) {
+        result = phase;
+        break;
       }
-      catch (final AxisFault e) {
-         throw new DemoRuntimeException(e);
-      }
+    }
 
-      // Création de l'objet Stub
-      SaeServiceStub service;
-      try {
-         service = new SaeServiceStub(configContext, urlSaeService);
-      }
-      catch (final AxisFault e) {
-         throw new DemoRuntimeException(e);
-      }
+    return result;
 
-      // Récupération de l'objet exposant le PKCS#12
-      // Dans un cas "normal", on récupère l'instance créée au démarrage
-      // de l'application.
-      final KeyStoreInterface keystore = new MyKeyStore();
-
-      // Instancie l'objet de génération du VI
-      final Axis2VIHandler axis2VIHandler = new Axis2VIHandler(keystore,
-                                                               listPagmSaeService(),
-                                                               getProperties().getProperty(CONTRAT_SERVICE),
-                                                               VI_LOGIN);
-      // Ajout d'un Handler lors de la phase "MessageOut" pour insérer le VI
-      final AxisConfiguration axisConfig = configContext.getAxisConfiguration();
-      final List<Phase> outFlowPhases = axisConfig.getOutFlowPhases();
-      final Phase messageOut = findPhaseByName(outFlowPhases, "MessageOut");
-      messageOut.addHandler(axis2VIHandler);
-
-      // Renvoie l'objet Stub
-      return service;
-
-   }
-
-   /**
-    * Création d'un Stub sans authentification
-    *
-    * @return le Stub
-    */
-   public static SaeServiceStub createStubSansAuthentification() {
-      try {
-
-         final String urlSaeService = litUrlSaeService();
-
-         return new SaeServiceStub(urlSaeService);
-
-      }
-      catch (final AxisFault e) {
-         throw new DemoRuntimeException(e);
-      }
-   }
-
-   private static Phase findPhaseByName(final List<Phase> phases,
-                                        final String nomPhaseRecherchee) {
-
-      Phase result = null;
-
-      for (final Phase phase : phases) {
-         if (phase.getName().equals(nomPhaseRecherchee)) {
-            result = phase;
-            break;
-         }
-      }
-
-      return result;
-
-   }
+  }
 
 }
