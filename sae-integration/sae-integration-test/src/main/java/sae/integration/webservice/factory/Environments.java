bc0ae2719d1658fe7d98430ef7e09895d52270c6
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2019-07-09 09:16:36 +0000

    Correction url GNS INT INTERNE
    svn path=/java/trunk/; revision=7172

--- sae-integration/sae-integration-test/src/main/java/sae/integration/webservice/factory/Environments.java
+++ sae-integration/sae-integration-test/src/main/java/sae/integration/webservice/factory/Environments.java
@@ -8,7 +8,7 @@ public enum Environments {
                           GNT_INT_CLIENT("http://hwi31intgntv6boweb1.gidn.recouv/ged/services/SaeService/"),
                           GNS_INT_CLIENT("http://hwi31intgnsboweb1.gidn.recouv/ged/services/SaeService/"),
                           GNT_INT_INTERNE("http://hwi31devgntv6boweb1.gidn.recouv/ged/services/SaeService/"),
-                          GNS_INT_INTERNE("hwi31devgnsv6boweb1.gidn.recouv/ged/services/SaeService/"),
+                          GNS_INT_INTERNE("http://hwi31devgnsv6boweb1.gidn.recouv/ged/services/SaeService/"),
                           GNT_PIC("http://hwi31picgntboweb1.gidn.recouv/ged/services/SaeService/"),
                           GNT_INT_NAT_C1("http://hwi31ginc1gntv6boweb1.cer31.recouv/ged/services/SaeService/"),
                           GNT_DEV2("http://hwi31dev2gntboweb1.gidn.recouv/ged/services/SaeService/"),
