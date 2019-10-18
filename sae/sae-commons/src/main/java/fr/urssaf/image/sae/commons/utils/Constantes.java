d1bc285fd60017820918f429f6295afa8bcf9b2f
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2019-10-18 15:24:22 +0000

    sae-commons: Modification constante tracedestinataire
    svn path=/java/branches/sae-20.04/; revision=7407

--- sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/utils/Constantes.java
+++ sae/sae-commons/src/main/java/fr/urssaf/image/sae/commons/utils/Constantes.java
@@ -88,7 +88,7 @@ public final class Constantes {
   // Traces
   public static final String CF_TRACE_JOURNAL_EVT = "tracejournalevt";
 
-  public static final String CF_TRACE_DESTINATAIRE = "tracedestinataire";
+  public static final String CF_TRACE_DESTINATAIRE = "tracedestinatairecql";
 
   public static final String CF_TRACE_REG_TECHNIQUE = "traceregtechnique";
 
@@ -111,5 +111,7 @@ public final class Constantes {
   public static final String CF_DROIT_PAGM = "droitpagm";
 
   public static final String CF_DROIT_PRMD = "droitprmd";
+
+  public static final String CF_TRACE_JOURNAL_EVT_INDEX_DOC = "tracejournalevtindexdoccql";
 
 }
