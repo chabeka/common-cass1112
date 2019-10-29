f3c211ccb04629aca90bb523a37a833a5cf2372e
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2019-10-29 12:48:41 +0000

    Merge avec la branche 19.10 (cf 20.02)
    svn path=/java/trunk/; revision=7456

--- commons-droid/src/main/java/fr/urssaf/image/commons/droid/service/impl/FormatIdentificationServiceImpl.java
+++ commons-droid/src/main/java/fr/urssaf/image/commons/droid/service/impl/FormatIdentificationServiceImpl.java
@@ -1,7 +1,6 @@
 package fr.urssaf.image.commons.droid.service.impl;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,14 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import fr.urssaf.image.commons.droid.exception.FormatIdentificationRuntimeException;
+import fr.urssaf.image.commons.droid.service.FormatIdentificationService;
+import fr.urssaf.image.commons.droid.support.DroidAnalyseSupport;
 import uk.gov.nationalarchives.droid.core.interfaces.IdentificationRequest;
 import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultCollection;
 import uk.gov.nationalarchives.droid.core.interfaces.RequestIdentifier;
 import uk.gov.nationalarchives.droid.core.interfaces.resource.FileSystemIdentificationRequest;
 import uk.gov.nationalarchives.droid.core.interfaces.resource.RequestMetaData;
-import fr.urssaf.image.commons.droid.exception.FormatIdentificationRuntimeException;
-import fr.urssaf.image.commons.droid.service.FormatIdentificationService;
-import fr.urssaf.image.commons.droid.support.DroidAnalyseSupport;
 
 /**
  * Service d'identification de format par DROID.<br>
@@ -34,148 +33,133 @@ import fr.urssaf.image.commons.droid.support.DroidAnalyseSupport;
  * mêmes résultats
  */
 @Service
-public class FormatIdentificationServiceImpl implements
-      FormatIdentificationService {
+public class FormatIdentificationServiceImpl implements FormatIdentificationService {
 
-   private static final Logger LOGGER = LoggerFactory
-         .getLogger(FormatIdentificationServiceImpl.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(FormatIdentificationServiceImpl.class);
 
-   private final DroidAnalyseSupport droidAnalyseSupport;
+  private final DroidAnalyseSupport droidAnalyseSupport;
 
-   /**
-    * Constructeur
-    * 
-    * @param droidAnalyseSupport
-    *           l'objet de support pour l'analyse DROID
-    */
-   @Autowired
-   public FormatIdentificationServiceImpl(
-         DroidAnalyseSupport droidAnalyseSupport) {
+  /**
+   * Constructeur
+   * 
+   * @param droidAnalyseSupport
+   *          l'objet de support pour l'analyse DROID
+   */
+  @Autowired
+  public FormatIdentificationServiceImpl(final DroidAnalyseSupport droidAnalyseSupport) {
 
-      this.droidAnalyseSupport = droidAnalyseSupport;
+    this.droidAnalyseSupport = droidAnalyseSupport;
 
-   }
+  }
 
-   /**
-    * {@inheritDoc}
-    * 
-    * @throws IOException
-    */
-   @Override
-   public final String identifie(File file) throws IOException {
-      return identifie(file, false);
-   }
+  /**
+   * {@inheritDoc}
+   * 
+   * @throws IOException @{@link IOException}
+   */
+  @Override
+  public final String identifie(final File file) throws IOException {
+    return identifie(file, false);
+  }
 
-   private String identifie(File file, boolean analyserContenuArchives)
-         throws IOException {
+  private String identifie(final File file, final boolean analyserContenuArchives) throws IOException {
 
-      // Préparation des données nécessaires au moteur d'identification
-      IdentificationRequest request = prepareIdentification(file);
+    // Préparation des données nécessaires au moteur d'identification
+    final IdentificationRequest request = prepareIdentification(file);
 
-      // L'implémentation est FORTEMENT inspirée de :
-      // uk.gov.nationalarchives.droid.submitter.SubmissionGateway
-      // (droid-results)
-      // uk.gov.nationalarchives.droid.command.ResultPrinter
-      // (droid-command-line)
-      // notamment afin de garder le même algo de détection des formats
-      // et donc d'obtenir exactement les mêmes résultats que Droid
+    // L'implémentation est FORTEMENT inspirée de :
+    // uk.gov.nationalarchives.droid.submitter.SubmissionGateway
+    // (droid-results)
+    // uk.gov.nationalarchives.droid.command.ResultPrinter
+    // (droid-command-line)
+    // notamment afin de garder le même algo de détection des formats
+    // et donc d'obtenir exactement les mêmes résultats que Droid
 
-      // TODO : Voir si on peut travailler sur un InputStream au lieu d'un File
+    // TODO : Voir si on peut travailler sur un InputStream au lieu d'un File
 
-      // Le résultat
-      String idPronom;
+    // Le résultat
+    String idPronom;
 
-      try {
+    try {
 
-         // Identification à l'aide des signatures binaires
-         IdentificationResultCollection results = droidAnalyseSupport
-               .handleSignatures(request);
+      // Identification à l'aide des signatures binaires
+      IdentificationResultCollection results = droidAnalyseSupport.handleSignatures(request);
 
-         // Traitement des formats de type "conteneurs"
-         IdentificationResultCollection containerResults = droidAnalyseSupport
-               .handleContainer(request, results);
+      // Traitement des formats de type "conteneurs"
+      IdentificationResultCollection containerResults = droidAnalyseSupport.handleContainer(request, results);
 
-         // Selon si on a trouvé quelque chose en format "conteneur"
-         // if (containerResults == null) {
-         if ((containerResults == null)
-               || (CollectionUtils.isEmpty(containerResults.getResults()))) {
+      // Selon si on a trouvé quelque chose en format "conteneur"
+      // if (containerResults == null) {
+      if (containerResults == null || CollectionUtils.isEmpty(containerResults.getResults())) {
 
-            // no container results - process the normal results.
-            results = droidAnalyseSupport.handleExtensions(request, results);
+        // no container results - process the normal results.
+        results = droidAnalyseSupport.handleExtensions(request, results);
 
-            // Are we processing archive formats?
-            if (analyserContenuArchives) {
+        // Are we processing archive formats?
+        if (analyserContenuArchives) {
 
-               // handleArchive(request, results);
-               throw new FormatIdentificationRuntimeException(
-                     "L'analyse au sein des archives n'est pas implémentée");
+          // handleArchive(request, results);
+          throw new FormatIdentificationRuntimeException("L'analyse au sein des archives n'est pas implémentée");
 
-            } else { // just process the results so far:
+        } else { // just process the results so far:
 
-               idPronom = droidAnalyseSupport.handleResult(request, results);
+          idPronom = droidAnalyseSupport.handleResult(request, results);
 
-            }
+        }
 
-         } else { // we have possible container formats:
+      } else { // we have possible container formats:
 
-            containerResults = droidAnalyseSupport.handleExtensions(request,
-                  containerResults);
-            idPronom = droidAnalyseSupport.handleResult(request,
-                  containerResults);
+        containerResults = droidAnalyseSupport.handleExtensions(request, containerResults);
+        idPronom = droidAnalyseSupport.handleResult(request, containerResults);
 
-         }
-
-         // Trace
-         LOGGER.debug(
-               "{}, Identification du format par Droid. Format trouvé : {}",
-               request.getFileName(), idPronom);
-      } finally {
-         request.close();
       }
 
-      // Renvoie du format identifié
-      return idPronom;
+      // Trace
+      LOGGER.debug("{}, Identification du format par Droid. Format trouvé : {}", request.getFileName(), idPronom);
+    } finally {
+      request.close();
+    }
 
-   }
+    // Renvoie du format identifié
+    return idPronom;
 
-   private IdentificationRequest prepareIdentification(File file) {
+  }
 
-      String fileName;
-      try {
-         fileName = file.getCanonicalPath();
-      } catch (IOException ex) {
-         throw new FormatIdentificationRuntimeException(ex);
+  private FileSystemIdentificationRequest prepareIdentification(final File file) {
+
+    String fileName;
+    try {
+      fileName = file.getCanonicalPath();
+    } catch (final IOException ex) {
+      throw new FormatIdentificationRuntimeException(ex);
+    }
+
+    final RequestMetaData metaData = new RequestMetaData(file.length(), file.lastModified(), fileName);
+
+    final RequestIdentifier identifier = new RequestIdentifier(file.toURI());
+    identifier.setParentId(1L);
+
+    final FileSystemIdentificationRequest request = new FileSystemIdentificationRequest(metaData, identifier);
+
+    final InputStream inputStream = null;
+    try {
+      request.open(file);
+    } catch (final FileNotFoundException ex) {
+      throw new FormatIdentificationRuntimeException(ex);
+    } catch (final IOException ex) {
+      throw new FormatIdentificationRuntimeException(ex);
+    } finally {
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (final IOException e) {
+          LOGGER.info(e.getMessage());
+        }
       }
+    }
 
-      RequestMetaData metaData = new RequestMetaData(file.length(), file
-            .lastModified(), fileName);
+    return request;
 
-      RequestIdentifier identifier = new RequestIdentifier(file.toURI());
-      identifier.setParentId(1L);
-
-      IdentificationRequest request = new FileSystemIdentificationRequest(
-            metaData, identifier);
-
-      InputStream inputStream = null;
-      try {
-         inputStream = new FileInputStream(file);
-         request.open(inputStream);
-      } catch (FileNotFoundException ex) {
-         throw new FormatIdentificationRuntimeException(ex);
-      } catch (IOException ex) {
-         throw new FormatIdentificationRuntimeException(ex);
-      } finally {
-         if (inputStream != null) {
-            try {
-               inputStream.close();
-            } catch (IOException e) {
-               LOGGER.info(e.getMessage());
-            }
-         }
-      }
-
-      return request;
-
-   }
+  }
 
 }
