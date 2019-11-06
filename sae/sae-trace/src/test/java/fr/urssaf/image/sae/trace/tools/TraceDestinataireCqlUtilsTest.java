4ced31caf42fadaaec0dc14f7004b448bdc42e79
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2019-11-06 07:47:27 +0000

    sae-traces: Ajout logs pour debug test failed with jenkins
    svn path=/java/branches/sae-20.04/; revision=7482

--- sae/sae-trace/src/test/java/fr/urssaf/image/sae/trace/tools/TraceDestinataireCqlUtilsTest.java
+++ sae/sae-trace/src/test/java/fr/urssaf/image/sae/trace/tools/TraceDestinataireCqlUtilsTest.java
@@ -8,23 +8,28 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.urssaf.image.sae.commons.utils.Row;
 import fr.urssaf.image.sae.commons.utils.cql.DataCqlUtils;
 import fr.urssaf.image.sae.trace.dao.model.TraceDestinataire;
 
 /**
- * TODO (AC75095351) Description du type
- *
+ * Test extraction de ressources par fichier xml (AC75095351)
  */
 public class TraceDestinataireCqlUtilsTest {
+  private static final Logger LOGGER = LoggerFactory.getLogger(TraceDestinataireCqlUtilsTest.class);
 
   @Test
   public void test() {
 
     final URL url = this.getClass().getResource("/cassandra-local-dataset-sae-traces.xml");
+    LOGGER.warn("url" + url);
     final List<Row> list = DataCqlUtils.deserializeColumnFamilyToRows(url.getPath(), "TraceDestinataire");
+    LOGGER.warn("sizeList=" + list.size());
     final List<TraceDestinataire> listTraceDestinataire = TraceDestinataireCqlUtils.convertRowsToTraceDestinataires(list);
+    LOGGER.warn("sizeListTraceDestinataire=" + listTraceDestinataire.size());
     Assert.assertTrue(listTraceDestinataire.size() == 20);
   }
 }
