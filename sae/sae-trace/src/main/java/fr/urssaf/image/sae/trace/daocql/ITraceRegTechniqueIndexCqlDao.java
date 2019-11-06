54b20a34233793e0030cc6a405953ff6aca28bef
'AURELIE GARNIER' <aurelie.garnier@acoss.fr>
2019-11-06 11:13:51 +0000

    [maven-release-plugin]  copy for tag commons-pdfbox-1.1.0
    svn path=/java/tags/commons-pdfbox-1.1.0/; revision=7503

--- sae/sae-trace/src/main/java/fr/urssaf/image/sae/trace/daocql/ITraceRegTechniqueIndexCqlDao.java
+++
@@ -1,8 +0,0 @@
-package fr.urssaf.image.sae.trace.daocql;
-
-import fr.urssaf.image.commons.cassandra.cql.dao.IGenericDAO;
-import fr.urssaf.image.sae.trace.dao.modelcql.TraceRegTechniqueIndexCql;
-
-public interface ITraceRegTechniqueIndexCqlDao extends IGenericDAO<TraceRegTechniqueIndexCql, String> {
-
-}
