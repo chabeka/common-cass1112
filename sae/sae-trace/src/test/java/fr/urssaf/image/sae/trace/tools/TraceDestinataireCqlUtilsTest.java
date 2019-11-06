ce3745471ff3ac8036ffc022f3e1b2bbb070ab5c
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2019-11-06 09:56:54 +0000

    sae-traces: debug jenkins error
    svn path=/java/branches/sae-20.04/; revision=7483

--- sae/sae-trace/src/test/java/fr/urssaf/image/sae/trace/tools/TraceDestinataireCqlUtilsTest.java
+++ sae/sae-trace/src/test/java/fr/urssaf/image/sae/trace/tools/TraceDestinataireCqlUtilsTest.java
@@ -3,6 +3,7 @@
  */
 package fr.urssaf.image.sae.trace.tools;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 
@@ -22,11 +23,11 @@ public class TraceDestinataireCqlUtilsTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(TraceDestinataireCqlUtilsTest.class);
 
   @Test
-  public void test() {
+  public void test() throws URISyntaxException {
 
     final URL url = this.getClass().getResource("/cassandra-local-dataset-sae-traces.xml");
     LOGGER.warn("url" + url);
-    final List<Row> list = DataCqlUtils.deserializeColumnFamilyToRows(url.getPath(), "TraceDestinataire");
+    final List<Row> list = DataCqlUtils.deserializeColumnFamilyToRows(url.toURI().getPath(), "TraceDestinataire");
     LOGGER.warn("sizeList=" + list.size());
     final List<TraceDestinataire> listTraceDestinataire = TraceDestinataireCqlUtils.convertRowsToTraceDestinataires(list);
     LOGGER.warn("sizeListTraceDestinataire=" + listTraceDestinataire.size());
