403b48de57326a5ebdaf20c40c99f4f70ca28ea1
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2019-11-20 10:37:01 +0000

    Prise en compte audit de code 20.02
    svn path=/java/trunk/; revision=7589

--- commons/commons-cassandra-spring-batch/src/main/java/fr/urssaf/image/commons/cassandra/spring/batch/dao/cql/ISequencesDaoCql.java
+++ commons/commons-cassandra-spring-batch/src/main/java/fr/urssaf/image/commons/cassandra/spring/batch/dao/cql/ISequencesDaoCql.java
@@ -9,9 +9,9 @@ import fr.urssaf.image.commons.cassandra.spring.batch.cqlmodel.SequencesCql;
 /**
  * Interface DAO de {@link SequencesCql}
  * 
- * @param <SequencesCql>
+ * @param SequencesCql
  *          Type de d'objet contenue dans le registre
- * @param <String>
+ * @param String
  *          le type d'Identifiant de l'objet
  */
 public interface ISequencesDaoCql extends IGenericDAO<SequencesCql, String> {
