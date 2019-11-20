f9e95b6c63701a914d5bd8f7197953347bf1c746
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2019-11-20 16:25:17 +0000

    sae-format:
    Constructeur avec CassandraCQLClientFactory
    
    svn path=/java/branches/sae-20.04/; revision=7600

--- sae/sae-format/src/main/java/fr/urssaf/image/sae/format/referentiel/dao/cql/impl/ReferentielFormatCqlDaoImpl.java
+++ sae/sae-format/src/main/java/fr/urssaf/image/sae/format/referentiel/dao/cql/impl/ReferentielFormatCqlDaoImpl.java
@@ -3,9 +3,11 @@
  */
 package fr.urssaf.image.sae.format.referentiel.dao.cql.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import fr.urssaf.image.commons.cassandra.cql.dao.impl.GenericDAOImpl;
+import fr.urssaf.image.commons.cassandra.helper.CassandraCQLClientFactory;
 import fr.urssaf.image.sae.format.referentiel.dao.cql.IReferentielFormatDaoCql;
 import fr.urssaf.image.sae.format.referentiel.model.FormatFichier;
 
@@ -14,5 +16,13 @@ import fr.urssaf.image.sae.format.referentiel.model.FormatFichier;
  */
 @Repository
 public class ReferentielFormatCqlDaoImpl extends GenericDAOImpl<FormatFichier, String> implements IReferentielFormatDaoCql {
+
+  /**
+   * @param ccf
+   */
+  @Autowired
+  public ReferentielFormatCqlDaoImpl(final CassandraCQLClientFactory ccf) {
+    super(ccf);
+  }
 
 }
