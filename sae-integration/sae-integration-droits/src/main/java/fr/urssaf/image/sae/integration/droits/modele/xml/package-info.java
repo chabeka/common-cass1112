982e3bfa57b5664bc20a77364c827d05d152b11d
'AURELIE GARNIER' <aurelie.garnier@acoss.fr>
2019-11-25 07:07:51 +0000

    [maven-release-plugin]  copy for tag sae-ihm-web-exploit-2.0.1
    svn path=/java/tags/sae-ihm-web-exploit-2.0.1/; revision=7623

--- sae-integration/sae-integration-droits/src/main/java/fr/urssaf/image/sae/integration/droits/modele/xml/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.5-2 
-// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2013.09.17 à 04:50:06 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.cirtil.fr/saeIntegration/droit", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fr.urssaf.image.sae.integration.droits.modele.xml;
