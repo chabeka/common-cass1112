1645a2661682c6763c0cae1df00d57a4f0464fa0
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2019-11-26 13:40:07 +0000

    sae-anais-framework:
    Commentaire pour cleanup
    
    svn path=/java/branches/sae-20.04/; revision=7710

--- saedemos/sae-anais-framework/src/test/java/fr/urssaf/image/sae/anais/framework/util/InitFactory.java
+++ saedemos/sae-anais-framework/src/test/java/fr/urssaf/image/sae/anais/framework/util/InitFactory.java
@@ -11,41 +11,46 @@ import fr.urssaf.image.sae.anais.framework.config.SaeAnaisConfig;
 
 public final class InitFactory {
 
-   private InitFactory() {
+  private InitFactory() {
 
-   }
+  }
 
-   public static SaeAnaisConfig initConfigRechercheDocumentaire() {
+  /**
+   * Initialisation de la configuration Anais
+   * 
+   * @return
+   */
+  public static SaeAnaisConfig initConfigRechercheDocumentaire() {
 
-      SaeAnaisConfig anaisConfig = new SaeAnaisConfig();
+    final SaeAnaisConfig anaisConfig = new SaeAnaisConfig();
 
-      try {
-         AbstractConfiguration.setDefaultListDelimiter("|".charAt(0));
-         Configuration config = new PropertiesConfiguration(
-               "sae-anais-framework-rg-test.properties");
+    try {
+      AbstractConfiguration.setDefaultListDelimiter("|".charAt(0));
+      final Configuration config = new PropertiesConfiguration(
+          "sae-anais-framework-rg-test.properties");
 
-         anaisConfig.setCodeapp(config.getString("anais.code-application"));
-         anaisConfig.setPasswd(config
-               .getString("anais.compte-applicatif-password"));
-         anaisConfig.setCodeenv("PROD");
-         anaisConfig.setAppdn(config.getString("anais.cn"));
-         anaisConfig.setPort(config.getInt("anais.port"));
+      anaisConfig.setCodeapp(config.getString("anais.code-application"));
+      anaisConfig.setPasswd(config
+                            .getString("anais.compte-applicatif-password"));
+      anaisConfig.setCodeenv("PROD");
+      anaisConfig.setAppdn(config.getString("anais.cn"));
+      anaisConfig.setPort(config.getInt("anais.port"));
 
-         anaisConfig.setHostname(config.getStringArray("anais.host")[0]);
-         anaisConfig.setTimeout(config.getString("anais.timeout"));
-         anaisConfig.setUsetls(config.getBoolean("anais.tls"));
+      anaisConfig.setHostname(config.getStringArray("anais.host")[0]);
+      anaisConfig.setTimeout(config.getString("anais.timeout"));
+      anaisConfig.setUsetls(config.getBoolean("anais.tls"));
 
-         anaisConfig.setComptePortail(config.getString("anais.comptePortail"));
-         anaisConfig.setDroitsDirect(config
-               .getBoolean("anais.activerDroitsDirects"));
+      anaisConfig.setComptePortail(config.getString("anais.comptePortail"));
+      anaisConfig.setDroitsDirect(config
+                                  .getBoolean("anais.activerDroitsDirects"));
 
-      } catch (ConfigurationException configException) {
-         throw new IllegalStateException(configException);
-      }
+    } catch (final ConfigurationException configException) {
+      throw new IllegalStateException(configException);
+    }
 
-      return anaisConfig;
+    return anaisConfig;
 
-   }
+  }
 }
 
 // CHECKSTYLE:ON
