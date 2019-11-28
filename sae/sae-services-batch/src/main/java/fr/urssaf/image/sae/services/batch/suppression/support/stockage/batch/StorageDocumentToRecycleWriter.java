6dfbe852312d465bc980fc9e6535876222ee820d
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2019-11-28 18:12:07 +0000

    Anomalie #423242 Transfert de masse avec suppression et modification
    svn path=/java/trunk/; revision=7714

--- sae/sae-services-batch/src/main/java/fr/urssaf/image/sae/services/batch/suppression/support/stockage/batch/StorageDocumentToRecycleWriter.java
+++ sae/sae-services-batch/src/main/java/fr/urssaf/image/sae/services/batch/suppression/support/stockage/batch/StorageDocumentToRecycleWriter.java
@@ -29,103 +29,123 @@ import fr.urssaf.image.sae.storage.services.StorageServiceProvider;
 public class StorageDocumentToRecycleWriter implements
 ItemWriter<StorageDocument> {
 
-   private static final Logger LOGGER = LoggerFactory
-         .getLogger(StorageDocumentToRecycleWriter.class);
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(StorageDocumentToRecycleWriter.class);
 
-   /**
-    * Pool de thread de suppression.
-    */
-   @Autowired
-   private SuppressionPoolThreadExecutor poolExecutor;
+  /**
+   * Pool de thread de suppression.
+   */
+  @Autowired
+  private SuppressionPoolThreadExecutor poolExecutor;
 
-   /**
-    * Provider pour le stockage.
-    */
-   @Autowired
-   @Qualifier("storageServiceProvider")
-   private StorageServiceProvider serviceProvider;
+  /**
+   * Provider pour le stockage.
+   */
+  @Autowired
+  @Qualifier("storageServiceProvider")
+  private StorageServiceProvider serviceProvider;
 
-   private static final String TRC_INSERT = "StorageDocumentToRecycleWriter()";
+  private static final String TRC_INSERT = "StorageDocumentToRecycleWriter()";
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void write(final List<? extends StorageDocument> items) throws Exception {
-      Runnable command;
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void write(final List<? extends StorageDocument> items) throws Exception {
+    Runnable command;
 
-      for (final StorageDocument storageDocument : Utils.nullSafeIterable(items)) {
+    for (final StorageDocument storageDocument : Utils.nullSafeIterable(items)) {
 
-         command = new SuppressionRunnable(storageDocument, this);
+      command = new SuppressionRunnable(storageDocument, this);
 
-         poolExecutor.execute(command);
+      poolExecutor.execute(command);
 
-         LOGGER.debug("{} - nombre de documents en attente dans le pool : {}",
-                      TRC_INSERT,
-                      "Queue : " + poolExecutor.getQueue().size() + " - Total : " + poolExecutor.getTaskCount() + " - Actifs : " + poolExecutor.getActiveCount());
+      LOGGER.debug("{} - nombre de documents en attente dans le pool : {}",
+                   TRC_INSERT,
+                   "Queue : " + poolExecutor.getQueue().size() + " - Total : " + poolExecutor.getTaskCount() + " - Actifs : " + poolExecutor.getActiveCount());
+    }
+  }
+
+  /**
+   * Déplacement du document dans la corbeille
+   * 
+   * @param storageDocument
+   *           document à mettre dans la corbeille
+   * @return le document avec l'uuid renseigné
+   * @throws UpdateServiceEx
+   *            Exception levée lors de la persistance
+   */
+  public final void moveToRecycleBeanStorageDocument(final UUID uuidJob,
+                                                     final StorageDocument storageDocument) throws UpdateServiceEx {
+
+    // constitue la liste des métadonnées à modifier
+    final List<StorageMetadata> modifiedMetadatas = new ArrayList<>();
+
+    StorageMetadata metadataISI = null;
+    StorageMetadata metadataDMC = null;
+    StorageMetadata metadataIFI = null;
+
+    for (final StorageMetadata metadata : storageDocument.getMetadatas()) {
+      // on ne remplit que les dates de mise a la corbeille
+      // et l'id de suppression ou de transfert
+      if (Constantes.CODE_COURT_META_ID_SUPPRESSION.equals(metadata.getShortCode())) {
+        metadataISI = metadata;
+      } else if (Constantes.CODE_COURT_META_DATE_CORBEILLE.equals(metadata.getShortCode())) {
+        metadataDMC = metadata;
+      } else if (StorageTechnicalMetadatas.ID_TRANSFERT_MASSE_INTERNE.getShortCode().equals(metadata.getShortCode())) {
+        metadataIFI = metadata;
       }
-   }
+    }
+    // Si cas transfert de masse, on met pas la meta CODE_COURT_META_ID_SUPPRESSION
+    // car on ne peut avoir un id de transfertMasseInterne et un idSuppressionMasseInterne
+    // à la fois
 
-   /**
-    * Déplacement du document dans la corbeille
-    * 
-    * @param storageDocument
-    *           document à mettre dans la corbeille
-    * @return le document avec l'uuid renseigné
-    * @throws UpdateServiceEx
-    *            Exception levée lors de la persistance
-    */
-   public final void moveToRecycleBeanStorageDocument(final UUID uuidJob,
-                                                      final StorageDocument storageDocument) throws UpdateServiceEx {
+    // si l'idTranfertMasseInterne est non null alors on est dans un transfert de masse
+    if (metadataIFI != null) {
+      modifiedMetadatas.add(metadataIFI);
+      modifiedMetadatas.add(metadataDMC);
+    } else {
+      modifiedMetadatas.add(metadataISI);
+      modifiedMetadatas.add(metadataDMC);
+    }
+    // ETAPE 1 : mise a jour de l'identifiant de suppression ou de transfert (lors de la suppression) et de la date de mise a la corbeille
+    updateDocument(uuidJob, storageDocument, modifiedMetadatas, null);
 
-      // constitue la liste des métadonnées à modifier
-      final List<StorageMetadata> modifiedMetadatas = new ArrayList<>();
-      for (final StorageMetadata metadata : storageDocument.getMetadatas()) {
-         // on ne remplit que les dates de mise a la corbeille
-         // et l'id de suppression ou de transfert
-         if (Constantes.CODE_COURT_META_ID_SUPPRESSION.equals(metadata.getShortCode())
-                 || Constantes.CODE_COURT_META_DATE_CORBEILLE.equals(metadata.getShortCode())
-                 || StorageTechnicalMetadatas.ID_TRANSFERT_MASSE_INTERNE.getShortCode().equals(metadata.getShortCode())) {
-              modifiedMetadatas.add(metadata);
-         }
-      }
-      // ETAPE 1 : mise a jour de l'identifiant de suppression ou de transfert (lors de la suppression) et de la date de mise a la corbeille
-      updateDocument(uuidJob, storageDocument, modifiedMetadatas, null);
+    // ETAPE 2 : mise a la corbeille du document
+    try {
+      serviceProvider.getStorageDocumentService().moveStorageDocumentToRecycleBin(storageDocument.getUuid());
+    }
+    catch (final Exception except) {
+      // quand il y a une erreur de mise a la corbeille, on tente de supprimer les metas ajoutées
+      updateDocument(uuidJob, storageDocument, null, modifiedMetadatas);
+      throw new UpdateServiceEx(except);
 
-      // ETAPE 2 : mise a la corbeille du document
-      try {
-         serviceProvider.getStorageDocumentService().moveStorageDocumentToRecycleBin(storageDocument.getUuid());
-      } catch (final Exception except) {
-         // quand il y a une erreur de mise a la corbeille, on tente de supprimer les metas ajoutées
-         updateDocument(uuidJob, storageDocument, null, modifiedMetadatas);
-         throw new UpdateServiceEx(except);
+    }
+  }
 
-      }
-   }
+  /**
+   * Méthode permettant de modifier le document.
+   * 
+   * @param storageDocument
+   *           document
+   * @param modifiedMetadatas
+   *           liste des metas modifiées
+   * @param deletedMetadatas
+   *           liste des metas supprimées
+   * @throws UpdateServiceEx
+   *            Exception levée lors de la persistance
+   */
+  private void updateDocument(final UUID uuidJob, final StorageDocument storageDocument,
+                              final List<StorageMetadata> modifiedMetadatas,
+                              final List<StorageMetadata> deletedMetadatas) throws UpdateServiceEx {
+    try {
+      serviceProvider.getStorageDocumentService().updateStorageDocument(uuidJob, 
+                                                                        storageDocument.getUuid(), modifiedMetadatas, deletedMetadatas);
 
-   /**
-    * Méthode permettant de modifier le document.
-    * 
-    * @param storageDocument
-    *           document
-    * @param modifiedMetadatas
-    *           liste des metas modifiées
-    * @param deletedMetadatas
-    *           liste des metas supprimées
-    * @throws UpdateServiceEx
-    *            Exception levée lors de la persistance
-    */
-   private void updateDocument(final UUID uuidJob, final StorageDocument storageDocument,
-                               final List<StorageMetadata> modifiedMetadatas,
-                               final List<StorageMetadata> deletedMetadatas) throws UpdateServiceEx {
-      try {
-         serviceProvider.getStorageDocumentService().updateStorageDocument(uuidJob, 
-                                                                           storageDocument.getUuid(), modifiedMetadatas, deletedMetadatas);
+    } catch (final Exception except) {
 
-      } catch (final Exception except) {
+      throw new UpdateServiceEx(except);
 
-         throw new UpdateServiceEx(except);
-
-      }
-   }
+    }
+  }
 }
