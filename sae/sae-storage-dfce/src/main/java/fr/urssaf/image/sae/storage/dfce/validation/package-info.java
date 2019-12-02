f88e0b04cf007e21860918b9b8feebc91c26476b
'AURELIE GARNIER' <aurelie.garnier@acoss.fr>
2019-12-02 06:30:41 +0000

    [maven-release-plugin]  copy for tag sae-ihm-web-exploit-livrable-1.0.33
    svn path=/java/tags/sae-ihm-web-exploit-livrable-1.0.33/; revision=7729

--- sae/sae-storage-dfce/src/main/java/fr/urssaf/image/sae/storage/dfce/validation/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Ce package contient les aspects permettant de valider les 
- * paramètres d'entrées des différents services.
- * 
- * 
- */
-package fr.urssaf.image.sae.storage.dfce.validation;
