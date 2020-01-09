51b7de940c2c55ec4f13583286e736d573770101
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2020-01-09 13:10:20 +0000

    Evolution #232945Evolution des filtres de la recherche par itérateur - Mise à jour tests Unitaires
    svn path=/java/branches/sae-20.04/; revision=7894

--- sae/sae-services/src/test/java/fr/urssaf/image/sae/services/search/cql/SAESearchServiceImplDatasCqlTest.java
+++ sae/sae-services/src/test/java/fr/urssaf/image/sae/services/search/cql/SAESearchServiceImplDatasCqlTest.java
@@ -752,6 +752,7 @@ public class SAESearchServiceImplDatasCqlTest extends AbstractServiceCqlTest {
   }
 
   /**
+   * Ne plus faire de test de doublons sur les equalFilter : Evolution #232945
    * Test de la recherche par itérateur
    *
    * @throws DoublonFiltresMetadataEx
@@ -780,8 +781,7 @@ public class SAESearchServiceImplDatasCqlTest extends AbstractServiceCqlTest {
                                                                           "20120101",
         "20120101");
     final List<AbstractMetadata> filters = new ArrayList<>();
-    final UntypedMetadata metaFiltre = new UntypedMetadata("Titre",
-        "titre_a_chercher");
+    final UntypedRangeMetadata metaFiltre = new UntypedRangeMetadata("DateArchivage", "201901010000000", "201901122356999");
 
     // Ajout du filtre en double
     filters.add(metaFiltre);
@@ -795,8 +795,8 @@ public class SAESearchServiceImplDatasCqlTest extends AbstractServiceCqlTest {
       final PaginatedUntypedDocuments documents = saeSearchService
           .searchPaginated(fixedMetadatas,
                            varyingMetadata,
-                           filters,
                            new ArrayList<AbstractMetadata>(),
+                           filters,
                            nbDocumentsParPage,
                            pageId,
                            listeDesiredMetadata,
@@ -808,7 +808,7 @@ public class SAESearchServiceImplDatasCqlTest extends AbstractServiceCqlTest {
       Assert
       .assertEquals(
                     "Le message attendu est incorrect",
-                    "La ou les métadonnées suivantes, utilisées dans le filtre de la requête de recherche par itérateur, sont en doublon : Titre",
+                    "La ou les métadonnées suivantes, utilisées dans le filtre de la requête de recherche par itérateur, sont en doublon : DateArchivage",
                     e.getMessage());
     }
 
