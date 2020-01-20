e1f9bfad3995c205d9e5b5714556fd90681cdc37
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2020-01-20 06:57:38 +0000

    sae-format:  suppression LOGGER inutile
    svn path=/java/branches/sae-20.04/; revision=7927

--- sae/sae-format/src/main/java/fr/urssaf/image/sae/format/referentiel/model/FormatFichier.java
+++ sae/sae-format/src/main/java/fr/urssaf/image/sae/format/referentiel/model/FormatFichier.java
@@ -1,8 +1,6 @@
 package fr.urssaf.image.sae.format.referentiel.model;
 
 import org.javers.core.metamodel.annotation.Id;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.PartitionKey;
