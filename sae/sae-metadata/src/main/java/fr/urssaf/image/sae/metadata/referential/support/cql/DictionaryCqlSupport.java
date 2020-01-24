d7b124a18c487a9a624c541a6d3cbf16a39eabe1
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2020-01-24 15:15:36 +0000

    sae-metadata:
    ajout au dictionary sans clock
    
    svn path=/java/branches/sae-20.04/; revision=7948

--- sae/sae-metadata/src/main/java/fr/urssaf/image/sae/metadata/referential/support/cql/DictionaryCqlSupport.java
+++ sae/sae-metadata/src/main/java/fr/urssaf/image/sae/metadata/referential/support/cql/DictionaryCqlSupport.java
@@ -21,89 +21,89 @@ import fr.urssaf.image.sae.metadata.referential.model.Dictionary;
 @Component
 public class DictionaryCqlSupport {
 
-	@Autowired
-	IDictionaryDaoCql dictionaryDaoCql;
+  @Autowired
+  IDictionaryDaoCql dictionaryDaoCql;
 
-	public DictionaryCqlSupport(final IDictionaryDaoCql dictionaryDaoCql) {
-		this.dictionaryDaoCql = dictionaryDaoCql;
-	}
+  public DictionaryCqlSupport(final IDictionaryDaoCql dictionaryDaoCql) {
+    this.dictionaryDaoCql = dictionaryDaoCql;
+  }
 
-	/**
-	 * Ajout d'une entrée au dictionnaire, le créé s'il n'existe pas
-	 * 
-	 * @param identifiant identifiant du dictionnaire
-	 * @param value       valeur de l'entée
-	 */
+  /**
+   * Ajout d'une entrée au dictionnaire, le créé s'il n'existe pas
+   * 
+   * @param identifiant identifiant du dictionnaire
+   * @param value       valeur de l'entée
+   */
 
-	public final void addElement(final String identifiant, final String value) {
-		
-		final Optional<Dictionary> dictionaryOpt = dictionaryDaoCql.findWithMapperById(identifiant);
-		if (dictionaryOpt.isPresent()) {
-			if (!dictionaryOpt.get().getEntries().contains(value)) {
-				dictionaryOpt.get().getEntries().add(value);
-				dictionaryDaoCql.saveWithMapper(dictionaryOpt.get());
-			}
-		} else {
-			final String tab[] = { value };
-			final Dictionary dictionary = new Dictionary();
-			dictionary.setIdentifiant(identifiant);
-			dictionary.setEntries(Arrays.asList(tab));
-			dictionaryDaoCql.saveWithMapper(dictionary);
-		}
-	}
+  public final void addElement(final String identifiant, final String value) {
 
-	/**
-	 * Supprime une entrée du dictionnaire
-	 * 
-	 * @param identifiant identifiant du dictionnaire
-	 * @param value       Valeur de l'entrée à supprimer
-	 */
-	public final void deleteElement(final String identifiant, final String value) {
-		final Optional<Dictionary> dictionaryOpt = dictionaryDaoCql.findWithMapperById(identifiant);
-		if (dictionaryOpt.isPresent()) {
-			final Dictionary dictionaryFromBD = dictionaryOpt.get();
-			if (dictionaryFromBD.getEntries().remove(value)) {
-				dictionaryDaoCql.saveWithMapper(dictionaryFromBD);
-			}
-		}
-	}
+    final Optional<Dictionary> dictionaryOpt = dictionaryDaoCql.findWithMapperById(identifiant);
+    if (dictionaryOpt.isPresent()) {
+      if (!dictionaryOpt.get().getEntries().contains(value)) {
+        dictionaryOpt.get().getEntries().add(value);
+        dictionaryDaoCql.saveWithMapper(dictionaryOpt.get());
+      }
+    } else {
+      final String tab[] = { value };
+      final Dictionary dictionary = new Dictionary();
+      dictionary.setIdentifiant(identifiant);
+      dictionary.setEntries(Arrays.asList(tab));
+      dictionaryDaoCql.saveWithMapper(dictionary);
+    }
+  }
 
-	/**
-	 * Supprime une entrée du dictionnaire
-	 * 
-	 * @param identifiant identifiant du dictionnaire
-	 * @param value       Valeur de l'entrée à supprimer
-	 */
-	public final void deleteElement(final String identifiant, final String value, final long clock) {
-		final Optional<Dictionary> dictionaryOpt = dictionaryDaoCql.findWithMapperById(identifiant);
-		if (dictionaryOpt.isPresent()) {
-			final Dictionary dictionaryFromBD = dictionaryOpt.get();
-			if (dictionaryFromBD.getEntries().remove(value)) {
-				dictionaryDaoCql.saveWithMapper(dictionaryFromBD,clock);
-			}
-		}
-	}
+  /**
+   * Supprime une entrée du dictionnaire
+   * 
+   * @param identifiant identifiant du dictionnaire
+   * @param value       Valeur de l'entrée à supprimer
+   */
+  public final void deleteElement(final String identifiant, final String value) {
+    final Optional<Dictionary> dictionaryOpt = dictionaryDaoCql.findWithMapperById(identifiant);
+    if (dictionaryOpt.isPresent()) {
+      final Dictionary dictionaryFromBD = dictionaryOpt.get();
+      if (dictionaryFromBD.getEntries().remove(value)) {
+        dictionaryDaoCql.saveWithMapper(dictionaryFromBD);
+      }
+    }
+  }
 
-	
-	/**
-	 * Retourne un dictionary
-	 */
-	public Dictionary find(final String identifiant) {
-		Assert.notNull(identifiant, "l'identifiant ne peut etre null");
-		return dictionaryDaoCql.findWithMapperById(identifiant).orElse(null);
+  /**
+   * Supprime une entrée du dictionnaire
+   * 
+   * @param identifiant identifiant du dictionnaire
+   * @param value       Valeur de l'entrée à supprimer
+   */
+  public final void deleteElement(final String identifiant, final String value, final long clock) {
+    final Optional<Dictionary> dictionaryOpt = dictionaryDaoCql.findWithMapperById(identifiant);
+    if (dictionaryOpt.isPresent()) {
+      final Dictionary dictionaryFromBD = dictionaryOpt.get();
+      if (dictionaryFromBD.getEntries().remove(value)) {
+        dictionaryDaoCql.saveWithMapper(dictionaryFromBD);
+      }
+    }
+  }
 
-	}
 
-	/**
-	 * Retourne la liste de toutes les dictionary
-	 */
-	public List<Dictionary> findAll() {
-		final Iterator<Dictionary> it = dictionaryDaoCql.findAllWithMapper();
-		final List<Dictionary> list = new ArrayList<>();
-		while (it.hasNext()) {
-			list.add(it.next());
-		}
-		return list;
-	}
+  /**
+   * Retourne un dictionary
+   */
+  public Dictionary find(final String identifiant) {
+    Assert.notNull(identifiant, "l'identifiant ne peut etre null");
+    return dictionaryDaoCql.findWithMapperById(identifiant).orElse(null);
+
+  }
+
+  /**
+   * Retourne la liste de toutes les dictionary
+   */
+  public List<Dictionary> findAll() {
+    final Iterator<Dictionary> it = dictionaryDaoCql.findAllWithMapper();
+    final List<Dictionary> list = new ArrayList<>();
+    while (it.hasNext()) {
+      list.add(it.next());
+    }
+    return list;
+  }
 
 }
