cb59357e6377146eaad4d3642155a63e135d08cf
'MAMADOU DIALLO' <	mamadou.diallo-externe@acoss.fr>
2020-03-24 13:26:51 +0000

    Anomalie #443725: Pas de message lors de l'arrêt de tomcat sans reprise à l'occasion d'un transfert
    svn path=/java/trunk/; revision=8152

--- sae/sae-services-batch/src/main/java/fr/urssaf/image/sae/services/batch/transfert/support/stockage/batch/TransfertListener.java
+++ sae/sae-services-batch/src/main/java/fr/urssaf/image/sae/services/batch/transfert/support/stockage/batch/TransfertListener.java
@@ -1,5 +1,8 @@
 package fr.urssaf.image.sae.services.batch.transfert.support.stockage.batch;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -19,10 +22,13 @@ import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.Lists;
+
 import fr.urssaf.image.sae.bo.model.untyped.UntypedDocument;
 import fr.urssaf.image.sae.services.batch.capturemasse.listener.AbstractListener;
 import fr.urssaf.image.sae.services.batch.capturemasse.model.TraitementMasseIntegratedDocument;
 import fr.urssaf.image.sae.services.batch.capturemasse.support.stockage.exception.AbstractInsertionMasseRuntimeException;
+import fr.urssaf.image.sae.services.batch.capturemasse.support.stockage.interruption.InterruptionTraitementMasseSupport;
 import fr.urssaf.image.sae.services.batch.capturemasse.support.stockage.interruption.exception.InterruptionTraitementException;
 import fr.urssaf.image.sae.services.batch.common.Constantes;
 import fr.urssaf.image.sae.services.batch.transfert.support.stockage.multithreading.TransfertPoolThreadExecutor;
@@ -47,6 +53,11 @@ public class TransfertListener extends AbstractListener {
    @Autowired
    private TransfertPoolThreadExecutor executor;
 
+   /**
+    * class controlant les interruptions serveur
+    */
+   @Autowired
+   private InterruptionTraitementMasseSupport interruptionTraitementMasseSupport;
    /**
     * Action exécutée avant chaque process
     * 
@@ -183,8 +194,9 @@ public class TransfertListener extends AbstractListener {
       executor.shutdown();
       executor.waitFinishInsertion();
 
+      addErrorMsgOnNonIntegratedDocument(jobExecution);
+      
       final ConcurrentLinkedQueue<UUID> list = getIntegratedDocuments();
-
       jobExecution.getExecutionContext()
       .put(Constantes.NB_INTEG_DOCS,
             executor.getIntegratedDocuments().size());
@@ -203,6 +215,57 @@ public class TransfertListener extends AbstractListener {
 
       return status;
    }
+
+   /**
+    * Ajout d'un message d'erreur sur tous  les documents qui n'ont pas été traité
+    * par les thread et qui n'ont donc pas de message d'erreur
+    * @param jobExecution
+    */
+	private void addErrorMsgOnNonIntegratedDocument(final JobExecution jobExecution) {
+		
+	      // nombre de doc total du sommaire
+	      int nbDocCount = (int) jobExecution.getExecutionContext().get(Constantes.DOC_COUNT);
+	      int nbDocIntegDoc = executor.getIntegratedDocuments().size();
+	      
+	      // nombre de doc non intrégré
+	      int nbDocNonIntegDoc = nbDocCount - nbDocIntegDoc;
+	      // nombre de doc avec message d'erreur non vide
+	      int nbDocWithError = getIndexErreurListe().size();
+	      int nbDocWithoutError = nbDocNonIntegDoc - nbDocWithError;
+	      if (nbDocWithoutError > 0) {
+	    	  // code et message par default
+	          String mgsToAdd = "Une erreur interne à l'application est survenue lors du transfert. Transfert impossible : ";
+	    	  String codeError = Constantes.ERR_BUL002;
+	          
+	    	  // index du dernier document traité
+	    	  List<Integer> listIndex = Lists.newArrayList(getIndexErreurListe().iterator());
+	    	  Integer max = Collections.max(listIndex);
+	    	  
+	    	  // on verifie que c'est pas un probleme de connection
+	          String interrupMsg = interruptionTraitementMasseSupport.getConnectionResultExceptionMessage();
+	          if(!interrupMsg.isEmpty()) {
+	        	  codeError = Constantes.ERR_BUL002;
+	        	  mgsToAdd = interrupMsg;
+	        	  
+	          } else if (!listIndex.isEmpty()) {
+	        	  
+	        	  // on recupere le message et le code du dernier document traité
+	        	  List<String> listCodes = Lists.newArrayList(getCodesErreurListe().iterator());
+		    	  List<String> listMsg = Lists.newArrayList(getErrorMessageList().iterator()); 		    	  
+		    	  codeError = listCodes.get(listCodes.size() -1);
+		    	  mgsToAdd = listMsg.get(listMsg.size() -1);
+	          } else {
+	        	  // keep default code and error message
+	          }
+	          
+	          // ajout des erreus au documents
+	    	  for(int i = max + 1; i <= nbDocCount; i++) {
+	    		  getCodesErreurListe().add(codeError);
+	    		  getIndexErreurListe().add(i);
+	    		  getErrorMessageList().add(mgsToAdd);
+	    	  }
+	      }
+	}
 
    private ExitStatus gestionException(
          final AbstractInsertionMasseRuntimeException exception) {
