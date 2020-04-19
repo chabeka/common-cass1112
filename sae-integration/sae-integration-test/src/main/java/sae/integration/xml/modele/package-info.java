ebb3d15294f6bf96a5fd1a968e0a7998ccb4e610
'SAMUEL CARRIERE' <samuel.carriere@acoss.fr>
2020-04-19 19:39:16 +0000

    [maven-release-plugin]  copy for tag sae-20.4.2
    svn path=/java/tags/sae-20.4.2/; revision=8247

--- sae-integration/sae-integration-test/src/main/java/sae/integration/xml/modele/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.3.2 
-// Voir <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2019.09.06 à 10:57:31 AM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.cirtil.fr/sae/commun_sommaire_et_resultat", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package sae.integration.xml.modele;
