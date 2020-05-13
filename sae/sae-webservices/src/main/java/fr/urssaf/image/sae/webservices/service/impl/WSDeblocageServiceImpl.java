2a3881ceeef232c1082d7146a0563e57541730ab
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2020-05-13 07:50:13 +0000

    sae-webservices : trace pour déblocage (ticket 434310) modification de la classe WSDeblocageServiceImpl refactoring pour état ajout de trace avec info état avant déblocage et informations d'authentification
    svn path=/java/trunk/; revision=8275

--- sae/sae-webservices/src/main/java/fr/urssaf/image/sae/webservices/service/impl/WSDeblocageServiceImpl.java
+++ sae/sae-webservices/src/main/java/fr/urssaf/image/sae/webservices/service/impl/WSDeblocageServiceImpl.java
@@ -3,10 +3,12 @@ package fr.urssaf.image.sae.webservices.service.impl;
 import java.util.Date;
 import java.util.UUID;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import fr.cirtil.www.saeservice.Deblocage;
@@ -20,6 +22,7 @@ import fr.urssaf.image.sae.pile.travaux.service.JobLectureService;
 import fr.urssaf.image.sae.pile.travaux.service.JobQueueService;
 import fr.urssaf.image.sae.trace.model.TraceToCreate;
 import fr.urssaf.image.sae.trace.service.DispatcheurService;
+import fr.urssaf.image.sae.vi.modele.VIContenuExtrait;
 import fr.urssaf.image.sae.webservices.exception.DeblocageAxisFault;
 import fr.urssaf.image.sae.webservices.impl.factory.ObjectStorageResponseFactory;
 import fr.urssaf.image.sae.webservices.service.WSDeblocageService;
@@ -84,8 +87,12 @@ public class WSDeblocageServiceImpl implements WSDeblocageService {
             .get(
                  Constantes.CODE_TRAITEMENT);
       }
+      String stateRequest = "";
+      if (jobRequest.getState() != null) {
+        stateRequest = jobRequest.getState().toString();
+      }
 
-      if (JobState.FAILURE.name().equals(jobRequest.getState().toString())) {
+      if (JobState.FAILURE.name().equals(stateRequest)) {
         // Si déblocage modification de masse
         if (StringUtils.isNotBlank(codeTraitement)) {
           jobQueueService.deleteJobAndSemaphoreFromJobsQueues(uuidJob,
@@ -98,18 +105,14 @@ public class WSDeblocageServiceImpl implements WSDeblocageService {
                                                 endingDate,
                                                 null);
           // Ajouter une trace DEBLOCAGE|OK en mettant l'id du traitement de masse dans les infos
-          ajouterTraceDeblocage(uuidJob, endingDate);
+          ajouterTraceDeblocage(uuidJob, endingDate, stateRequest);
         } else {
           LOG.warn("{} - échec de déblocage du job {} - ce job ne correspond pas à un traitement de modification de masse", prefixeTrc, uuid);
           throw new DeblocageAxisFault("ErreurInterneDeblocage",
               "Le job ne correspond pas à un traitement de modification de masse");
         }
-      } else if (JobState.RESERVED.name()
-          .equals(
-                  jobRequest.getState().toString())
-          || JobState.STARTING.name()
-          .equals(
-                  jobRequest.getState().toString())) {
+      } else if (JobState.RESERVED.name().equals(stateRequest)
+          || JobState.STARTING.name().equals(stateRequest)) {
         jobQueueService.deleteJobFromJobsQueues(uuidJob);
         // Passer le job à l'état FAILURE
         final Date dateFailure = new Date();
@@ -118,7 +121,7 @@ public class WSDeblocageServiceImpl implements WSDeblocageService {
                                               dateFailure,
                                               null);
         // Ajouter une trace DEBLOCAGE|OK en mettant l'id du traitement de masse dans les infos
-        ajouterTraceDeblocage(uuidJob, dateFailure);
+        ajouterTraceDeblocage(uuidJob, dateFailure, stateRequest);
       } else {
         LOG.warn("{} - échec de déblocage du job {} - ce job ne peut pas être débloqué à cause de son état", prefixeTrc, uuid);
         throw new DeblocageAxisFault("ErreurInterneDeblocage",
@@ -150,7 +153,7 @@ public class WSDeblocageServiceImpl implements WSDeblocageService {
    * @param name
    * @param endingDate
    */
-  private void ajouterTraceDeblocage(final UUID uuidJob, final Date endingDate) {
+  private void ajouterTraceDeblocage(final UUID uuidJob, final Date endingDate, final String stateBefore) {
     // Instantiation de l'objet TraceToCreate
     final TraceToCreate traceToCreate = new TraceToCreate();
 
@@ -171,10 +174,29 @@ public class WSDeblocageServiceImpl implements WSDeblocageService {
     traceToCreate.getInfos().put("saeServeurHostname",
                                  HostnameUtil.getHostname());
     traceToCreate.getInfos().put("saeServeurIP", HostnameUtil.getIP());
-
+    traceToCreate.getInfos().put("stateBeforeUnlock", stateBefore);
+    setInfosAuth(traceToCreate);
     // Appel du dispatcheur
     dispatcheurService.ajouterTrace(traceToCreate);
 
   }
+  private void setInfosAuth(final TraceToCreate traceToCreate) {
 
+    if (SecurityContextHolder.getContext() != null
+        && SecurityContextHolder.getContext().getAuthentication() != null
+        && SecurityContextHolder.getContext().getAuthentication()
+        .getPrincipal() != null) {
+
+      final VIContenuExtrait extrait = (VIContenuExtrait) SecurityContextHolder
+          .getContext().getAuthentication().getPrincipal();
+      // Le code du Contrat de Service
+      traceToCreate.setContrat(extrait.getCodeAppli());
+      // Le ou les PAGM
+      if (CollectionUtils.isNotEmpty(extrait.getPagms())) {
+        traceToCreate.getPagms().addAll(extrait.getPagms());
+      }
+      // Le login utilisateur
+      traceToCreate.setLogin(extrait.getIdUtilisateur());
+    }
+  }
 }
