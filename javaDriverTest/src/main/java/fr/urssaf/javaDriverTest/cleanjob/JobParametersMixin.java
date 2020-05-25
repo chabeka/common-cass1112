a58b95d59004769cb89bd23b3e631382bfb3422f
'AURELIE GARNIER' <aurelie.garnier@acoss.fr>
2020-05-25 12:30:25 +0000

    [maven-release-plugin]  copy for tag sae-migration-donnees-20.4.1
    svn path=/java/tags/sae-migration-donnees-20.4.1/; revision=8280

--- javaDriverTest/src/main/java/fr/urssaf/javaDriverTest/cleanjob/JobParametersMixin.java
+++
@@ -1,9 +0,0 @@
-package fr.urssaf.javaDriverTest.cleanjob;
-
-import org.codehaus.jackson.annotate.JsonAutoDetect;
-
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY,
-                getterVisibility = JsonAutoDetect.Visibility.NONE,
-                isGetterVisibility = JsonAutoDetect.Visibility.NONE)
-public abstract class JobParametersMixin {
-}
