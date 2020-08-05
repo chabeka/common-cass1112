4493c89ff884e8066b7dba1b383ede536a0eb22c
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2020-08-05 15:58:37 +0000

    sae-lotinstallmaj: Sonar/Corrections: entryset, log stacktrace
    svn path=/java/trunk/; revision=8371

--- sae/sae-lotinstallmaj/src/main/java/fr/urssaf/image/sae/lotinstallmaj/service/utils/cql/CQLDataFileLoader.java
+++ sae/sae-lotinstallmaj/src/main/java/fr/urssaf/image/sae/lotinstallmaj/service/utils/cql/CQLDataFileLoader.java
@@ -3,6 +3,7 @@ package fr.urssaf.image.sae.lotinstallmaj.service.utils.cql;
 
 
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,41 +15,40 @@ import com.datastax.driver.core.Session;
  */
 public class CQLDataFileLoader {
 
-    private static final Logger log = LoggerFactory.getLogger(CQLDataFileLoader.class);
+  private static final Logger log = LoggerFactory.getLogger(CQLDataFileLoader.class);
 
-    private final Session session;
-    
-    public Session getSession() {
-        return session;
-    }
+  private final Session session;
 
-    public CQLDataFileLoader(Session session) {
-        this.session = session;
-    }
+  public Session getSession() {
+    return session;
+  }
 
-    public void load(CQLDataFileSet dataSet) {
-        log.debug("loading data");
-        
-        for (String query : dataSet.getCQLStatements()) {
-            log.debug("executing : " + query);
-            try {
-            	session.execute(query);
-            	log.info("Query ===>  " + query);
-            } catch (Exception e) {
-            	e.printStackTrace();
-				log.error("Problème d'execution de la requete : " + query);
-				//return;
-			}
-        }
-    }
+  public CQLDataFileLoader(final Session session) {
+    this.session = session;
+  }
 
-    public String createBachStatement(CQLDataFileSet dataSet) {
-    	
-    	StringBuffer statement = new StringBuffer("BEGIN BATCH ");
-    	for (String query : dataSet.getCQLStatements()) {
-            statement.append(query );
-        }
-    	statement.append(" APPLY BATCH;");
-		return statement.toString();
+  public void load(final CQLDataFileSet dataSet) {
+    log.debug("loading data");
+
+    for (final String query : dataSet.getCQLStatements()) {
+      log.debug("executing : " + query);
+      try {
+        session.execute(query);
+        log.info("Query ===>  " + query);
+      } catch (final Exception e) {
+        log.error(ExceptionUtils.getFullStackTrace(e));
+        log.error("Problème d'execution de la requete : " + query);
+      }
     }
+  }
+
+  public String createBachStatement(final CQLDataFileSet dataSet) {
+
+    final StringBuffer statement = new StringBuffer("BEGIN BATCH ");
+    for (final String query : dataSet.getCQLStatements()) {
+      statement.append(query );
+    }
+    statement.append(" APPLY BATCH;");
+    return statement.toString();
+  }
 }
