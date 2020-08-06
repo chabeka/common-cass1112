fa9b7f6092de2383d4c350843c2d0f34834cc080
'SAMUEL CARRIERE' <samuel.carriere@acoss.fr>
2020-08-06 14:39:06 +0000

    oops
    svn path=/java/trunk/; revision=8382

--- sae-bnd/src/main/java/fr/urssaf/image/parser_opencsv/application/configuration/SaeConfiguration.java
+++ sae-bnd/src/main/java/fr/urssaf/image/parser_opencsv/application/configuration/SaeConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.FileSystemResource;
 
 import fr.urssaf.image.parser_opencsv.application.exception.BNDScriptRuntimeException;
 import fr.urssaf.image.parser_opencsv.webservice.factory.AddViHeaderHandlerResolver;
