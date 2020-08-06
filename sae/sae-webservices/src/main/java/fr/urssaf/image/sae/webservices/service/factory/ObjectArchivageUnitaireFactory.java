1b523671f4dd5f4f6bfe72d64f6eadbfb44fe1a0
'EMMANUEL COIGNARD' <emmanuel.coignard-externe@acoss.fr>
2020-08-06 15:30:08 +0000

    sae-webservices:  @SuppressWarnings("pmd:MethodReturnsInternalArray") sur SaeServiceStub  dans sae-service pour toutes les listes de sae-service (classes générées auto): ajout   "pmd:MethodReturnsInternalArray" dans les @SuppressWarnings  ObjectArchivageUnitaireFactory : suppression du check null
    svn path=/java/trunk/; revision=8387

--- sae/sae-webservices/src/main/java/fr/urssaf/image/sae/webservices/service/factory/ObjectArchivageUnitaireFactory.java
+++ sae/sae-webservices/src/main/java/fr/urssaf/image/sae/webservices/service/factory/ObjectArchivageUnitaireFactory.java
@@ -22,176 +22,164 @@ import fr.urssaf.image.sae.webservices.factory.ObjectTypeFactory;
  * 
  */
 public final class ObjectArchivageUnitaireFactory {
-   private static final Logger LOG = LoggerFactory
-         .getLogger(ObjectArchivageUnitaireFactory.class);
+  private static final Logger LOG = LoggerFactory
+      .getLogger(ObjectArchivageUnitaireFactory.class);
 
-   private ObjectArchivageUnitaireFactory() {
+  private ObjectArchivageUnitaireFactory() {
 
-   }
+  }
 
-   /**
-    * instanciation de {@link ArchivageUnitaireResponse}.<br>
-    * Implementation de {@link ArchivageUnitaireResponseType}
-    * 
-    * <pre>
-    * &lt;xsd:complexType name="archivageUnitaireResponseType">
-    *    ...     
-    *    &lt;xsd:sequence>
-    *       &lt;xsd:element name="idArchive" type="sae:uuidType">
-    *       ...      
-    *       &lt;/xsd:element>
-    *    &lt;/xsd:sequence>
-    * &lt;/xsd:complexType>
-    * </pre>
-    * 
-    * @param idArchive
-    *           valeur de <code>uuidType</code>
-    * @return instance de {@link ArchivageUnitaireResponse}
-    */
-   public static ArchivageUnitaireResponse createArchivageUnitaireResponse(
-         UUID idArchive) {
-      String prefixeTrc = "createArchivageUnitaireResponse()";
-      LOG.debug("{} - Début", prefixeTrc);
-      ArchivageUnitaireResponse response = createArchivageUnitaireResponse();
-      final ArchivageUnitaireResponseType responseType = response
-            .getArchivageUnitaireResponse();
+  /**
+   * instanciation de {@link ArchivageUnitaireResponse}.<br>
+   * Implementation de {@link ArchivageUnitaireResponseType}
+   * 
+   * <pre>
+   * &lt;xsd:complexType name="archivageUnitaireResponseType">
+   *    ...     
+   *    &lt;xsd:sequence>
+   *       &lt;xsd:element name="idArchive" type="sae:uuidType">
+   *       ...      
+   *       &lt;/xsd:element>
+   *    &lt;/xsd:sequence>
+   * &lt;/xsd:complexType>
+   * </pre>
+   * 
+   * @param idArchive
+   *           valeur de <code>uuidType</code>
+   * @return instance de {@link ArchivageUnitaireResponse}
+   */
+  public static ArchivageUnitaireResponse createArchivageUnitaireResponse(
+                                                                          final UUID idArchive) {
+    final String prefixeTrc = "createArchivageUnitaireResponse()";
+    LOG.debug("{} - Début", prefixeTrc);
+    final ArchivageUnitaireResponse response = createArchivageUnitaireResponse();
+    final ArchivageUnitaireResponseType responseType = response
+        .getArchivageUnitaireResponse();
 
-      responseType.setIdArchive(ObjectTypeFactory.createUuidType(idArchive));
-      if (response == null) {
-         LOG.debug("{} - Valeur de retour : null", prefixeTrc);
-      } else {
-         LOG.debug("{} - Valeur de retour archiveId: \"{}\"", prefixeTrc, response
-               .getArchivageUnitaireResponse().getIdArchive());
-      }
-      LOG.debug("{} - Sortie", prefixeTrc);
-      // Fin des traces debug - sortie méthode
-      return response;
-   }
+    responseType.setIdArchive(ObjectTypeFactory.createUuidType(idArchive));
+    LOG.debug("{} - Valeur de retour archiveId: \"{}\"", prefixeTrc, response
+              .getArchivageUnitaireResponse().getIdArchive());
+    LOG.debug("{} - Sortie", prefixeTrc);
+    // Fin des traces debug - sortie méthode
+    return response;
+  }
 
-   /**
-    * instanciation de {@link ArchivageUnitaireResponse} vide.<br>
-    * 
-    * @return instance de {@link ArchivageUnitaireResponse}
-    */
-   private static ArchivageUnitaireResponse createArchivageUnitaireResponse() {
+  /**
+   * instanciation de {@link ArchivageUnitaireResponse} vide.<br>
+   * 
+   * @return instance de {@link ArchivageUnitaireResponse}
+   */
+  private static ArchivageUnitaireResponse createArchivageUnitaireResponse() {
 
-      ArchivageUnitaireResponse response = new ArchivageUnitaireResponse();
-      ArchivageUnitaireResponseType responseType = new ArchivageUnitaireResponseType();
-      response.setArchivageUnitaireResponse(responseType);
+    final ArchivageUnitaireResponse response = new ArchivageUnitaireResponse();
+    final ArchivageUnitaireResponseType responseType = new ArchivageUnitaireResponseType();
+    response.setArchivageUnitaireResponse(responseType);
 
-      return response;
-   }
-   
-   
-   
-   /**
-    * instanciation de {@link ArchivageUnitairePJResponse}.<br>
-    * Implementation de {@link ArchivageUnitairePJResponseType}
-    * 
-    * <pre>
-    * &lt;xsd:complexType name="archivageUnitairePJResponseType">
-    *    ...     
-    *    &lt;xsd:sequence>
-    *       &lt;xsd:element name="idArchive" type="sae:uuidType">
-    *       ...      
-    *       &lt;/xsd:element>
-    *    &lt;/xsd:sequence>
-    * &lt;/xsd:complexType>
-    * </pre>
-    * 
-    * @param idArchive
-    *           valeur de <code>uuidType</code>
-    * @return instance de {@link ArchivageUnitaireResponse}
-    */
-   public static ArchivageUnitairePJResponse createArchivageUnitairePJResponse(
-         UUID idArchive) {
-      
-      String prefixeTrc = "createArchivageUnitairePJResponse()";
-      LOG.debug("{} - Début", prefixeTrc);
-      
-      ArchivageUnitairePJResponse response = createArchivageUnitairePJResponse();
-      ArchivageUnitairePJResponseType responseType = response
-            .getArchivageUnitairePJResponse();
+    return response;
+  }
 
-      responseType.setIdArchive(ObjectTypeFactory.createUuidType(idArchive));
-      if (response == null) {
-         LOG.debug("{} - Valeur de retour : null", prefixeTrc);
-      } else {
-         LOG.debug("{} - Valeur de retour archiveId: \"{}\"", prefixeTrc, response
-               .getArchivageUnitairePJResponse().getIdArchive());
-      }
-      LOG.debug("{} - Sortie", prefixeTrc);
-      // Fin des traces debug - sortie méthode
-      return response;
-   }
-   
-   /**
-    * instanciation de {@link ArchivageUnitairePJResponse} vide.<br>
-    * 
-    * @return instance de {@link ArchivageUnitairePJResponse}
-    */
-   private static ArchivageUnitairePJResponse createArchivageUnitairePJResponse() {
 
-      ArchivageUnitairePJResponse response = new ArchivageUnitairePJResponse();
-      ArchivageUnitairePJResponseType responseType = new ArchivageUnitairePJResponseType();
-      response.setArchivageUnitairePJResponse(responseType);
 
-      return response;
-   }
-   
-   
-   /**
-    * instanciation de {@link ArchivageUnitairePJResponse}.<br>
-    * Implementation de {@link ArchivageUnitairePJResponseType}
-    * 
-    * <pre>
-    * &lt;xsd:complexType name="archivageUnitairePJResponseType">
-    *    ...     
-    *    &lt;xsd:sequence>
-    *       &lt;xsd:element name="idArchive" type="sae:uuidType">
-    *       ...      
-    *       &lt;/xsd:element>
-    *    &lt;/xsd:sequence>
-    * &lt;/xsd:complexType>
-    * </pre>
-    * 
-    * @param idArchive
-    *           valeur de <code>uuidType</code>
-    * @return instance de {@link ArchivageUnitaireResponse}
-    */
-   public static StockageUnitaireResponse createStockageUnitaireResponse(
-         UUID idArchive) {
-      
-      String prefixeTrc = "createStockageUnitaireResponse()";
-      LOG.debug("{} - Début", prefixeTrc);
-      
-      StockageUnitaireResponse response = createStockageUnitaireResponse();
-            
-            StockageUnitaireResponseType responseType = response.getStockageUnitaireResponse();
-            
-      responseType.setIdGed(ObjectTypeFactory.createUuidType(idArchive));
-      if (response == null) {
-         LOG.debug("{} - Valeur de retour : null", prefixeTrc);
-      } else {
-         LOG.debug("{} - Valeur de retour archiveId: \"{}\"", prefixeTrc, response.getStockageUnitaireResponse().getIdGed());
-      }
-      LOG.debug("{} - Sortie", prefixeTrc);
-      // Fin des traces debug - sortie méthode
-      return response;
-   }
-   
-   /**
-    * instanciation de {@link StockageUnitaireResponse} vide.<br>
-    * 
-    * @return instance de {@link StockageUnitaireResponse}
-    */
-   private static StockageUnitaireResponse createStockageUnitaireResponse() {
+  /**
+   * instanciation de {@link ArchivageUnitairePJResponse}.<br>
+   * Implementation de {@link ArchivageUnitairePJResponseType}
+   * 
+   * <pre>
+   * &lt;xsd:complexType name="archivageUnitairePJResponseType">
+   *    ...     
+   *    &lt;xsd:sequence>
+   *       &lt;xsd:element name="idArchive" type="sae:uuidType">
+   *       ...      
+   *       &lt;/xsd:element>
+   *    &lt;/xsd:sequence>
+   * &lt;/xsd:complexType>
+   * </pre>
+   * 
+   * @param idArchive
+   *           valeur de <code>uuidType</code>
+   * @return instance de {@link ArchivageUnitaireResponse}
+   */
+  public static ArchivageUnitairePJResponse createArchivageUnitairePJResponse(
+                                                                              final UUID idArchive) {
 
-      StockageUnitaireResponse response = new StockageUnitaireResponse();
-      StockageUnitaireResponseType responseType = new StockageUnitaireResponseType();
-      response.setStockageUnitaireResponse(responseType);
+    final String prefixeTrc = "createArchivageUnitairePJResponse()";
+    LOG.debug("{} - Début", prefixeTrc);
 
-      return response;
-   }
+    final ArchivageUnitairePJResponse response = createArchivageUnitairePJResponse();
+    final ArchivageUnitairePJResponseType responseType = response
+        .getArchivageUnitairePJResponse();
+
+    responseType.setIdArchive(ObjectTypeFactory.createUuidType(idArchive));
+    LOG.debug("{} - Valeur de retour archiveId: \"{}\"", prefixeTrc, response
+              .getArchivageUnitairePJResponse().getIdArchive());
+    LOG.debug("{} - Sortie", prefixeTrc);
+    // Fin des traces debug - sortie méthode
+    return response;
+  }
+
+  /**
+   * instanciation de {@link ArchivageUnitairePJResponse} vide.<br>
+   * 
+   * @return instance de {@link ArchivageUnitairePJResponse}
+   */
+  private static ArchivageUnitairePJResponse createArchivageUnitairePJResponse() {
+
+    final ArchivageUnitairePJResponse response = new ArchivageUnitairePJResponse();
+    final ArchivageUnitairePJResponseType responseType = new ArchivageUnitairePJResponseType();
+    response.setArchivageUnitairePJResponse(responseType);
+
+    return response;
+  }
+
+
+  /**
+   * instanciation de {@link ArchivageUnitairePJResponse}.<br>
+   * Implementation de {@link ArchivageUnitairePJResponseType}
+   * 
+   * <pre>
+   * &lt;xsd:complexType name="archivageUnitairePJResponseType">
+   *    ...     
+   *    &lt;xsd:sequence>
+   *       &lt;xsd:element name="idArchive" type="sae:uuidType">
+   *       ...      
+   *       &lt;/xsd:element>
+   *    &lt;/xsd:sequence>
+   * &lt;/xsd:complexType>
+   * </pre>
+   * 
+   * @param idArchive
+   *           valeur de <code>uuidType</code>
+   * @return instance de {@link ArchivageUnitaireResponse}
+   */
+  public static StockageUnitaireResponse createStockageUnitaireResponse(
+                                                                        final UUID idArchive) {
+
+    final String prefixeTrc = "createStockageUnitaireResponse()";
+    LOG.debug("{} - Début", prefixeTrc);
+
+    final StockageUnitaireResponse response = createStockageUnitaireResponse();
+
+    final StockageUnitaireResponseType responseType = response.getStockageUnitaireResponse();
+
+    responseType.setIdGed(ObjectTypeFactory.createUuidType(idArchive));
+    LOG.debug("{} - Valeur de retour archiveId: \"{}\"", prefixeTrc, response.getStockageUnitaireResponse().getIdGed());
+    LOG.debug("{} - Sortie", prefixeTrc);
+    // Fin des traces debug - sortie méthode
+    return response;
+  }
+
+  /**
+   * instanciation de {@link StockageUnitaireResponse} vide.<br>
+   * 
+   * @return instance de {@link StockageUnitaireResponse}
+   */
+  private static StockageUnitaireResponse createStockageUnitaireResponse() {
+
+    final StockageUnitaireResponse response = new StockageUnitaireResponse();
+    final StockageUnitaireResponseType responseType = new StockageUnitaireResponseType();
+    response.setStockageUnitaireResponse(responseType);
+
+    return response;
+  }
 
 }
