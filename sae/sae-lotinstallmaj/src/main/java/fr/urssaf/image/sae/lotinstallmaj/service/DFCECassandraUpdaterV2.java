6b5af0d95f6fe5113f90ab7e7891d3c430be7f94
'SAMUEL CARRIERE' <samuel.carriere@acoss.fr>
2020-08-28 09:24:07 +0000

    Correction update DFCE, et nettoyage
    svn path=/java/trunk/; revision=8440

--- sae/sae-lotinstallmaj/src/main/java/fr/urssaf/image/sae/lotinstallmaj/service/DFCECassandraUpdaterV2.java
+++ sae/sae-lotinstallmaj/src/main/java/fr/urssaf/image/sae/lotinstallmaj/service/DFCECassandraUpdaterV2.java
@@ -20,249 +20,253 @@ import fr.urssaf.image.sae.lotinstallmaj.service.utils.cql.CQLDataFileSet;
  */
 public abstract class DFCECassandraUpdaterV2 {
 
-  private static final Logger LOG = LoggerFactory.getLogger(DFCECassandraUpdaterV2.class);
+   private static final Logger LOG = LoggerFactory.getLogger(DFCECassandraUpdaterV2.class);
 
-  private static final String MESSAGE_DFCE_EST_A_JOUR = "La base de données DFCE est déja en version {} de nom : {}";
+   private static final String MESSAGE_DFCE_EST_A_JOUR = "La base de données DFCE est déja en version {} de nom : {}";
 
-  private static final String MESSAGE_DEBUT_MISE_A_JOUR = "Début de la mise à jour de la base en version {}";
+   private static final String MESSAGE_DEBUT_MISE_A_JOUR = "Début de la mise à jour de la base en version {}";
 
-  private static final String MESSAGE_FIN_MISE_A_JOUR = "Fin de l'installation de la mise à jour {}";
+   private static final String MESSAGE_FIN_MISE_A_JOUR = "Fin de l'installation de la mise à jour {}";
 
-  private static final String MESSAGE_CHANGE_VERSION_DFCE = "Passer la version en Base de DFCE à {}";
+   private static final String MESSAGE_CHANGE_VERSION_DFCE = "Passer la version en Base de DFCE à {}";
 
-  @Autowired
-  private DFCEConnexionComponent dfceConnexionTester;
+   @Autowired
+   private DFCEConnexionComponent dfceConnexionTester;
 
-  public static final int LAST_DFCE_AVAILABLE_VERSION = 3;
+   public static final int LAST_DFCE_AVAILABLE_VERSION = 3;
 
-  public enum VersionDFCE {
+   public enum VersionDFCE {
 
-    DFCE_192_TO_200_SCHEMA("/cql/dfce-1.9.2-TO-2.0.0-schema.cql", 1, "v2.0.0"),
-    DFCE_200_TO_210_SCHEMA("/cql/dfce-2.0.0-TO-2.1.0-schema.cql", 2, "v2.1.0"),
-    DFCE_210_TO_230_SCHEMA("/cql/dfce-2.1.0-TO-2.3.0-schema.cql", 3, "v2.3.0"),
+      DFCE_192_TO_200_SCHEMA("/cql/dfce-1.9.2-TO-2.0.0-schema.cql", 1, "v2.0.0"),
+      DFCE_200_TO_210_SCHEMA("/cql/dfce-2.0.0-TO-2.1.0-schema.cql", 2, "v2.1.0"),
+      DFCE_210_TO_230_SCHEMA("/cql/dfce-2.1.0-TO-2.3.0-schema.cql", 3, "v2.3.0"),
 
-    DFCE_230_TO_192_SCHEMA("/cql/dfce-2.3.0-TO-1.9.2-schema.cql", "v1.9.2");
+      DFCE_230_TO_192_SCHEMA("/cql/dfce-2.3.0-TO-1.9.2-schema.cql", "v1.9.2");
 
-    private final String cqlRelativeClasspath;
+      private final String cqlRelativeClasspath;
 
-    private final int numVersion;
+      private final int numVersion;
 
-    private final String versionName;
+      private final String versionName;
 
-    private static final Map<Integer, String> maps = new HashMap<>();
+      private static final Map<Integer, String> maps = new HashMap<>();
 
-    private VersionDFCE(final String cqlRelativeClasspath, final String versionName) {
-      this.cqlRelativeClasspath = cqlRelativeClasspath;
-      this.versionName = versionName;
-      numVersion = 0;
-    }
-
-    private VersionDFCE(final String cqlRelativeClasspath, final int numVersion, final String versionName) {
-      this.cqlRelativeClasspath = cqlRelativeClasspath;
-      this.numVersion = numVersion;
-      this.versionName = versionName;
-    }
-
-    /**
-     * @return the cqlRelativeClasspath
-     */
-    public String getCqlRelativeClasspath() {
-      return cqlRelativeClasspath;
-    }
-
-    /**
-     * @return the numVersion
-     */
-    public int getNumVersion() {
-      return numVersion;
-    }
-
-    /**
-     * @return the versionName
-     */
-    public String getVersionName() {
-      return versionName;
-    }
-
-    static {
-      for (final VersionDFCE version : VersionDFCE.values()) {
-        maps.put(version.getNumVersion(), version.getVersionName());
+      private VersionDFCE(final String cqlRelativeClasspath, final String versionName) {
+         this.cqlRelativeClasspath = cqlRelativeClasspath;
+         this.versionName = versionName;
+         numVersion = 0;
       }
-    }
 
-    public static final String getVersionNameById(final int version) {
-      return maps.get(version);
-    }
+      private VersionDFCE(final String cqlRelativeClasspath, final int numVersion, final String versionName) {
+         this.cqlRelativeClasspath = cqlRelativeClasspath;
+         this.numVersion = numVersion;
+         this.versionName = versionName;
+      }
 
-  }
+      /**
+       * @return the cqlRelativeClasspath
+       */
+      public String getCqlRelativeClasspath() {
+         return cqlRelativeClasspath;
+      }
 
-  @Autowired
-  protected DFCEKeyspaceConnecter dfcecf;
+      /**
+       * @return the numVersion
+       */
+      public int getNumVersion() {
+         return numVersion;
+      }
 
-  /**
-   * Mise à jour vers la version 200
-   */
-  public final void update192ToVersion200() {
-    final long currentVersion = getDatabaseVersionDFCE();
-    final String currentVersionName = VersionDFCE.getVersionNameById((int) currentVersion);
+      /**
+       * @return the versionName
+       */
+      public String getVersionName() {
+         return versionName;
+      }
 
-    if (currentVersion >= VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion()) {
-      LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, currentVersionName);
-      return;
-    }
-    LOG.info(MESSAGE_DEBUT_MISE_A_JOUR, VersionDFCE.DFCE_192_TO_200_SCHEMA);
-    CQLDataFileSet cqlData;
-    cqlData = new CQLDataFileSet(VersionDFCE.DFCE_192_TO_200_SCHEMA.getCqlRelativeClasspath());
-    final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
-    dataLoader.load(cqlData);	
-    LOG.info(MESSAGE_FIN_MISE_A_JOUR, VersionDFCE.DFCE_192_TO_200_SCHEMA);
+      static {
+         for (final VersionDFCE version : VersionDFCE.values()) {
+            maps.put(version.getNumVersion(), version.getVersionName());
+         }
+      }
 
-    // Met à jour la version de la base DFCE
-    LOG.info(MESSAGE_CHANGE_VERSION_DFCE, VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion());
-    try {
-      setDatabaseVersionDFCE(VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion());
-      System.out.println("passé");
-    }
-    catch (final Exception e) {
-      throw new MajLotRuntimeException(e);
-    }
-  }
-  /**
-   * Mise à jour vers la version 210
-   */
-  public final void update200ToVersion210() {
-    final long currentVersion = getDatabaseVersionDFCE();
-    final String currentVersionName = VersionDFCE.getVersionNameById((int) currentVersion);
+      public static final String getVersionNameById(final int version) {
+         return maps.get(version);
+      }
 
-    if (currentVersion >= VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion()) {
-      LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, currentVersionName);
-      return;
-    }
+   }
 
-    if (VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion() - currentVersion > 1) {
-      update192ToVersion200();
-    }
+   @Autowired
+   protected DFCEKeyspaceConnecter dfcecf;
 
-    LOG.info(MESSAGE_DEBUT_MISE_A_JOUR, VersionDFCE.DFCE_200_TO_210_SCHEMA);
-    CQLDataFileSet cqlData;
-    cqlData = new CQLDataFileSet(VersionDFCE.DFCE_200_TO_210_SCHEMA.cqlRelativeClasspath);
-    final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
-    dataLoader.load(cqlData);
-    LOG.info(MESSAGE_FIN_MISE_A_JOUR, VersionDFCE.DFCE_200_TO_210_SCHEMA);
+   /**
+    * Mise à jour vers la version 200
+    */
+   public final void update192ToVersion200() {
+      final long currentVersion = getDatabaseVersionDFCE();
+      final String currentVersionName = VersionDFCE.getVersionNameById((int) currentVersion);
 
-    // Met à jour la version de la base DFCE
-    LOG.info(MESSAGE_CHANGE_VERSION_DFCE, VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion());
-    setDatabaseVersionDFCE(VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion());
-  }
+      if (currentVersion >= VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion()) {
+         LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, currentVersionName);
+         return;
+      }
+      LOG.info(MESSAGE_DEBUT_MISE_A_JOUR, VersionDFCE.DFCE_192_TO_200_SCHEMA);
+      CQLDataFileSet cqlData;
+      cqlData = new CQLDataFileSet(VersionDFCE.DFCE_192_TO_200_SCHEMA.getCqlRelativeClasspath());
+      final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
+      dataLoader.load(cqlData);	
+      LOG.info(MESSAGE_FIN_MISE_A_JOUR, VersionDFCE.DFCE_192_TO_200_SCHEMA);
 
-  /**
-   * Mise à jour en la version 230
-   */
-  public final void update210ToVersion230() {
-    final long currentVersion = getDatabaseVersionDFCE();
-    final String currentVersionName = VersionDFCE.getVersionNameById((int) currentVersion);
+      // Met à jour la version de la base DFCE
+      LOG.info(MESSAGE_CHANGE_VERSION_DFCE, VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion());
+      try {
+         setDatabaseVersionDFCE(VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion());
+      }
+      catch (final Exception e) {
+         throw new MajLotRuntimeException(e);
+      }
+   }
+   /**
+    * Mise à jour vers la version 210
+    */
+   public final void update200ToVersion210() {
+      final long currentVersion = getDatabaseVersionDFCE();
+      final String currentVersionName = VersionDFCE.getVersionNameById((int) currentVersion);
 
-    if (currentVersion >= VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion()) {
-      LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, currentVersionName);
-      return;
-    }
+      if (currentVersion >= VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion()) {
+         LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, currentVersionName);
+         return;
+      }
 
-    if (VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion() - currentVersion > 1) {
-      update200ToVersion210();
-    }
+      if (VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion() - currentVersion > 1) {
+         update192ToVersion200();
+      }
 
-    LOG.info(MESSAGE_DEBUT_MISE_A_JOUR, VersionDFCE.DFCE_210_TO_230_SCHEMA);
-    CQLDataFileSet cqlData;
-    cqlData = new CQLDataFileSet(VersionDFCE.DFCE_210_TO_230_SCHEMA.cqlRelativeClasspath);
-    final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
-    dataLoader.load(cqlData);
-    LOG.info(MESSAGE_FIN_MISE_A_JOUR, VersionDFCE.DFCE_210_TO_230_SCHEMA);
+      LOG.info(MESSAGE_DEBUT_MISE_A_JOUR, VersionDFCE.DFCE_200_TO_210_SCHEMA);
+      CQLDataFileSet cqlData;
+      cqlData = new CQLDataFileSet(VersionDFCE.DFCE_200_TO_210_SCHEMA.cqlRelativeClasspath);
+      final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
+      dataLoader.load(cqlData);
+      LOG.info(MESSAGE_FIN_MISE_A_JOUR, VersionDFCE.DFCE_200_TO_210_SCHEMA);
 
-    // Met à jour la version de la base DFCE
-    LOG.info(MESSAGE_CHANGE_VERSION_DFCE, VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion());
-    setDatabaseVersionDFCE(VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion());
-  }
+      // Met à jour la version de la base DFCE
+      LOG.info(MESSAGE_CHANGE_VERSION_DFCE, VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion());
+      setDatabaseVersionDFCE(VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion());
+   }
 
-  /**
-   * Retour arrière de la version 2.3.1 à la version 1.9.2
-   */
-  public final void update230ToVersion192() {
+   /**
+    * Mise à jour en la version 230
+    */
+   public final void update210ToVersion230() {
+      final long currentVersion = getDatabaseVersionDFCE();
+      final String currentVersionName = VersionDFCE.getVersionNameById((int) currentVersion);
 
-    final int VERSION_INITIAL = 0;
-    final long currentVersion = getDatabaseVersionDFCE();
+      if (currentVersion >= VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion()) {
+         LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, currentVersionName);
+         return;
+      }
 
-    if (currentVersion == VERSION_INITIAL) {
-      LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, "Initial");
-      return;
-    }
+      if (VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion() - currentVersion > 1) {
+         update200ToVersion210();
+      }
 
-    LOG.info("Retour à la version [initial] de la base DFCE");
-    CQLDataFileSet cqlData;
-    cqlData = new CQLDataFileSet(VersionDFCE.DFCE_230_TO_192_SCHEMA.cqlRelativeClasspath);
-    final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
-    dataLoader.load(cqlData);
-    LOG.info("Fin du Rollback en version [initial] de la base DFCE");
+      LOG.info(MESSAGE_DEBUT_MISE_A_JOUR, VersionDFCE.DFCE_210_TO_230_SCHEMA);
+      CQLDataFileSet cqlData;
+      cqlData = new CQLDataFileSet(VersionDFCE.DFCE_210_TO_230_SCHEMA.cqlRelativeClasspath);
+      final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
+      dataLoader.load(cqlData);
+      LOG.info(MESSAGE_FIN_MISE_A_JOUR, VersionDFCE.DFCE_210_TO_230_SCHEMA);
 
-    // Met à jour la version de la base DFCE
-    setDatabaseVersionDFCE(VERSION_INITIAL);
-  }
+      // Met à jour la version de la base DFCE
+      LOG.info(MESSAGE_CHANGE_VERSION_DFCE, VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion());
+      setDatabaseVersionDFCE(VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion());
+   }
 
-  /**
-   * Installation de la dernière version disponible de la base DFCE
-   * 
-   * @return true si la base à été mise à jour et false si elle était à jour
-   * @throws MajLotGeneralException
-   */
-  public final boolean isReadyForUpdate() throws MajLotUnknownDFCEVersion {
+   /**
+    * Retour arrière de la version 2.3.1 à la version 1.9.2
+    */
+   public final void update230ToVersion192() {
 
-    boolean isReady = false;
+      final int VERSION_INITIAL = 0;
+      final long currentVersion = getDatabaseVersionDFCE();
 
-    final boolean isBaseSAE = isKeyspaceSAE();
-    final long saeBdVersion = getDatabaseVersion();
+      if (currentVersion == VERSION_INITIAL) {
+         LOG.info(MESSAGE_DFCE_EST_A_JOUR, currentVersion, "Initial");
+         return;
+      }
 
-    // Si base SAE créé et la version de la base est >= 33 ==> version dfce = 3
-    if (isBaseSAE && saeBdVersion >= 33) {
-      setDatabaseVersionDFCE(3);
-    } else {
-      //
-    }
+      LOG.info("Retour à la version [initial] de la base DFCE");
+      CQLDataFileSet cqlData;
+      cqlData = new CQLDataFileSet(VersionDFCE.DFCE_230_TO_192_SCHEMA.cqlRelativeClasspath);
+      final CQLDataFileLoader dataLoader = new CQLDataFileLoader(dfcecf.getSession());
+      dataLoader.load(cqlData);
+      LOG.info("Fin du Rollback en version [initial] de la base DFCE");
 
-    // S'il existe une nouvelle mise à jour de la base DFCE
-    final long dfceBdVersion = getDatabaseVersionDFCE();
-    if (LAST_DFCE_AVAILABLE_VERSION > dfceBdVersion) {
-      if (LAST_DFCE_AVAILABLE_VERSION == VersionDFCE.DFCE_192_TO_200_SCHEMA.getNumVersion()) {
-        update192ToVersion200();
-      } else if (LAST_DFCE_AVAILABLE_VERSION == VersionDFCE.DFCE_200_TO_210_SCHEMA.getNumVersion()) {
-        update200ToVersion210();
-      } else if (LAST_DFCE_AVAILABLE_VERSION == VersionDFCE.DFCE_210_TO_230_SCHEMA.getNumVersion()) {
-        update210ToVersion230();
+      // Met à jour la version de la base DFCE
+      setDatabaseVersionDFCE(VERSION_INITIAL);
+   }
+
+   /**
+    * Installation de la dernière version disponible de la base DFCE
+    * 
+    * @return true si la base à été mise à jour et false si elle était à jour
+    * @throws MajLotGeneralException
+    */
+   public final boolean isReadyForUpdate() throws MajLotUnknownDFCEVersion {
+
+      boolean isReady = false;
+
+      // Récupération de la version actuelle de DFCE
+      long dfceBdVersion = getDatabaseVersionDFCE();
+      if (dfceBdVersion == 0) {
+         // Si base SAE créée et la version de la base est >= 33 ==> version dfce = 3, mais la version
+         // de DFCE n'est pas encore inscrite en base de données
+         final boolean isBaseSAE = isKeyspaceSAE();
+         final long saeBdVersion = getDatabaseVersion();
+         if (isBaseSAE && saeBdVersion >= 33) {
+            setDatabaseVersionDFCE(3);
+            dfceBdVersion = 3;
+         }
+      }
+
+      // S'il existe une nouvelle mise à jour de la base DFCE
+      if (LAST_DFCE_AVAILABLE_VERSION > dfceBdVersion) {
+         if (dfceBdVersion == 0) {
+            update192ToVersion200();
+            dfceBdVersion = 1;
+         }
+         if (dfceBdVersion == 1) {
+            update200ToVersion210();
+            dfceBdVersion = 2;
+         }
+         if (dfceBdVersion == 2) {
+            update210ToVersion230();
+            dfceBdVersion = 3;
+         }
       } else {
-        throw new MajLotUnknownDFCEVersion(LAST_DFCE_AVAILABLE_VERSION);
+         LOG.info("DFCE est déjà à jour...");
       }
-    } else {
-      LOG.info("DFCE est déjà à jour...");
-    }
 
-    // Tester la connexion à dfce
-    try {
-      isReady = dfceConnexionTester.testerWithServiceProvider();
-    }
-    catch (final Exception e) {
-      LOG.info("Erreur de connexion à la webapp DFCE, Détails : {}", e.getMessage());
-    }
-    LOG.info("Test de connexion : {}", isReady ? "OK" : "KO");
+      // Tester la connexion à dfce
+      try {
+         isReady = dfceConnexionTester.testerWithServiceProvider();
+      }
+      catch (final Exception e) {
+         LOG.info("Erreur de connexion à la webapp DFCE, Détails : {}", e.getMessage());
+      }
+      LOG.info("Test de connexion : {}", isReady ? "OK" : "KO");
 
-    return isReady;
-  }
+      return isReady;
+   }
 
-  protected abstract long getDatabaseVersion();
+   protected abstract long getDatabaseVersion();
 
-  protected abstract void setDatabaseVersion(int version);
+   protected abstract void setDatabaseVersion(int version);
 
-  protected abstract long getDatabaseVersionDFCE();
+   protected abstract long getDatabaseVersionDFCE();
 
-  protected abstract void setDatabaseVersionDFCE(int version);
+   protected abstract void setDatabaseVersionDFCE(int version);
 
-  protected abstract boolean isKeyspaceSAE();
+   protected abstract boolean isKeyspaceSAE();
 
 }
